5f383ae823b30b01012c425c12909aaeb3ab9abc
John McIntosh <johnmci@smalltalkconsulting.com>
2001-12-18 21:34:16 +0000

    3.2.1 cleanup for apple project builder
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@24 fa1542d4-bde8-0310-ad64-8ed1123d492a

--- platforms/Mac OS/vm/sqConfig.h
+++ platforms/Mac OS/vm/sqConfig.h
@@ -1,18 +1,18 @@
 /* sqConfig.h -- platform identification and configuration */
 
-#if defined(__MWERKS__) && !defined(macintosh)  && !defined(BEOS_SQUEAK)
+#if defined(__MWERKS__) && !defined(macintoshSqueak)  && !defined(BEOS_SQUEAK)
   /* CodeWarrior 8 neglects to define "macintosh" */
-# define macintosh
+# define macintoshSqueak
 #endif
 
 #if defined (__APPLE__) && defined(__MACH__)
   /* For Apple's OS X, ppc version of darwin */
-#define macintosh
 #include <mactypes.h>
+#define macintoshSqueak
 #endif
 
 
-#if defined(macintosh)
+#if defined(macintoshSqueak)
 # if defined(SQ_CONFIG_DONE)
 #   error configuration conflict
 # endif
