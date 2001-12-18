5e4e7fd49745273bef24fedc38f4525649ab07ee
John McIntosh <johnmci@smalltalkconsulting.com>
2001-12-18 21:42:48 +0000

    3.2.1 rewrote for long file name support
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@27 fa1542d4-bde8-0310-ad64-8ed1123d492a

--- platforms/Mac OS/vm/sqMacFileLogic.h
+++ platforms/Mac OS/vm/sqMacFileLogic.h
@@ -1,46 +1,27 @@
-#include <Navigation.h>
-#include <StandardFile.h>
-#if !defined( __MPW__ ) &&  !defined ( __APPLE__ ) && !defined ( __MACH__ )
- #include <path2fss.h>
+#if TARGET_API_MAC_CARBON
+    #include <Carbon/Carbon.h>
 #else
-#include <Aliases.h>
-#include <Gestalt.h>
+	#include <Navigation.h>
+	#include <StandardFile.h>
+	#if defined(__MWERKS__)
+		#include <path2fss.h>
+	#endif
+	#include <Aliases.h>
+	#include <Gestalt.h>
+	#include <string.h>	
 #endif
 
-pascal	OSErr	FSMakeFSSpecCompat(short vRefNum,
-								   long dirID,
-								   ConstStr255Param fileName,
-								   FSSpec *spec);
-OSErr FSpLocationFromFullPath(short fullPathLength,
-									 const void *fullPath,
-									 FSSpec *spec);
-									 
-pascal	OSErr	GetFullPath(short vRefNum,
-							long dirID,
-							ConstStr255Param name,
-							short *fullPathLength,
-							Handle *fullPath);
-
-pascal	OSErr	FSpGetFullPath(const FSSpec *spec,
-							   short *fullPathLength,
-							   Handle *fullPath);
+OSErr	FSMakeFSSpecCompat(short vRefNum, long dirID, ConstStr255Param fileName,  FSSpec *spec);
+OSStatus GetApplicationDirectory(FSSpec *workingDirectory);
+int PathToDir(char *pathName, int pathNameMax, FSSpec *workingDirectory);
+int PathToFile(char *pathName, int pathNameMax, FSSpec *workingDirectory);
+OSErr makeFSSpec(char *pathString, int pathStringLength, FSSpec *spec);
 							   
 OSErr squeakFindImage(const FSSpecPtr defaultLocationfssPtr,FSSpecPtr documentfsSpec);
-pascal void findImageEventProc(NavEventCallbackMessage callBackSelector, 
-                        NavCBRecPtr callBackParms, 
-                        NavCallBackUserData callBackUD);
-pascal Boolean findImageFilterProc(AEDesc* theItem, void* info, 
-                            NavCallBackUserData callBackUD,
-                            NavFilterModes filterMode);                        						
-pascal short DialogHook(short item, DialogPtr theDialog, 
-	void *userData);
-	
-OSStatus GetApplicationDirectory(short *vRefNum, long *dirID);
-int PathToWorkingDir(char *pathName, int pathNameMax, short volumeNumber,long directoryID);
-int PrefixPathWith(char *pathName, int pathNameSize, int pathNameMax, char *prefix);
-void StoreFullPathForLocalNameInto(char *shortName, char *fullName, int length, short VolumeNumber,long directoryID);
-OSErr makeFSSpec(char *pathString, int pathStringLength,FSSpec *spec);
-int fetchFileInfo(CInfoPBRec *pb,int dirIndex,FSSpec *spec,unsigned char *name, Boolean doAlias, Boolean *isFolder);
-int doItTheHardWay(unsigned char *pathString,FSSpec *spec,CInfoPBRec *pb,Boolean noDrillDown);
+pascal void findImageEventProc(NavEventCallbackMessage callBackSelector,   NavCBRecPtr callBackParms,  NavCallBackUserData callBackUD);
+pascal Boolean findImageFilterProc(AEDesc* theItem, void* info, NavCallBackUserData callBackUD, NavFilterModes filterMode);                       
+pascal short DialogHook(short item, DialogPtr theDialog,  void *userData);
+int fetchFileInfo(int dirIndex,FSSpec *spec,unsigned char *name,Boolean doAlias,long *parentDirectory, int *isFolder,int *createDateStorage,int *modificationDateStorage,squeakInt64 *sizeOfFile,Str255 *longFileName);
+int doItTheHardWay(unsigned char *pathString,FSSpec *spec,Boolean noDrillDown);
 int lookupPath(char *pathString, int pathStringLength, FSSpec *spec,Boolean noDrillDown);
-void makePascalStringFromSqName(char *pathString, int pathStringLength,unsigned char *name);
+void makeOSXPath(char * dst, int src, int num,Boolean resolveAlias);Boolean isVmPathVolumeHFSPlus(void);
