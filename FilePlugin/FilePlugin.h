5ab38b90d8bd162e7f0db0dcf8c0728535502951
John McIntosh <johnmci@smalltalkconsulting.com>
2002-01-22 19:25:28 +0000

    3.2.x changed off_t to squeakFileOffsetType
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@123 fa1542d4-bde8-0310-ad64-8ed1123d492a

--- FilePlugin/FilePlugin.h
+++ FilePlugin/FilePlugin.h
@@ -1,3 +1,15 @@
+/****************************************************************************
+*   PROJECT: Common include
+*   FILE:    FilePlugin.h
+*   CONTENT: 
+*
+*   AUTHOR:  
+*   ADDRESS: 
+*   EMAIL:   
+*   RCSID:   $Id: FilePlugin.h,v 1.3 2002/01/22 19:25:28 johnmci Exp $
+*
+*	01/22/2002 JMM change off_t to squeakOffsetFileType
+*/
 /* File support definitions */
 
 /* squeak file record; see sqFilePrims.c for details */
@@ -5,7 +17,7 @@ typedef struct {
 	FILE	*file;
 	int		sessionID;
 	int		writable;
-	off_t		fileSize;
+	squeakFileOffsetType		fileSize;
 	int		lastOp;  /* 0 = uncommitted, 1 = read, 2 = write */
 } SQFile;
 
@@ -13,18 +25,18 @@ typedef struct {
 int sqFileAtEnd(SQFile *f);
 int sqFileClose(SQFile *f);
 int sqFileDeleteNameSize(int sqFileNameIndex, int sqFileNameSize);
-off_t sqFileGetPosition(SQFile *f);
+squeakFileOffsetType sqFileGetPosition(SQFile *f);
 int sqFileInit(void);
 int sqFileShutdown(void);
 int sqFileOpen(SQFile *f, int sqFileNameIndex, int sqFileNameSize, int writeFlag);
 size_t sqFileReadIntoAt(SQFile *f, size_t count, int byteArrayIndex, size_t startIndex);
 int sqFileRenameOldSizeNewSize(int oldNameIndex, int oldNameSize, int newNameIndex, int newNameSize);
-int sqFileSetPosition(SQFile *f, off_t position);
-off_t sqFileSize(SQFile *f);
+int sqFileSetPosition(SQFile *f, squeakFileOffsetType position);
+squeakFileOffsetType sqFileSize(SQFile *f);
 int sqFileValid(SQFile *f);
 size_t sqFileWriteFromAt(SQFile *f, size_t count, int byteArrayIndex, size_t startIndex);
 int sqFileFlush(SQFile *f);
-int sqFileTruncate(SQFile *f,off_t offset);
+int sqFileTruncate(SQFile *f,squeakFileOffsetType offset);
 int sqFileThisSession(void);
 
 /* directories */
@@ -34,7 +46,7 @@ int dir_Delimitor(void);
 int dir_Lookup(char *pathString, int pathStringLength, int index,
 	/* outputs: */
 	char *name, int *nameLength, int *creationDate, int *modificationDate,
-	int *isDirectory, off_t *sizeIfFile);
+	int *isDirectory, squeakFileOffsetType *sizeIfFile);
 int dir_PathToWorkingDir(char *pathName, int pathNameMax);
 int dir_SetMacFileTypeAndCreator(char *filename, int filenameSize, char *fType, char *fCreator);
 int dir_GetMacFileTypeAndCreator(char *filename, int filenameSize, char *fType, char *fCreator);
