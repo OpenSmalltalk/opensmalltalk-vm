b13dc092ad7e8cf20ca6f710649cd12618bc4f5e
John McIntosh <johnmci@smalltalkconsulting.com>
2002-02-05 20:05:57 +0000

    3.2.3 update as part of windows port.
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@181 fa1542d4-bde8-0310-ad64-8ed1123d492a

--- platforms/Mac OS/plugins/UUIDPlugin/sqMacUUID.c
+++ platforms/Mac OS/plugins/UUIDPlugin/sqMacUUID.c
@@ -1,17 +1,32 @@
+/****************************************************************************
+*   PROJECT: UUID support for the mac
+*   FILE:    sqMacUUID.c
+*   CONTENT: 
+*
+*   AUTHOR:  John McIntosh.
+*   ADDRESS: 
+*   EMAIL:   johnmci@smalltalkconsulting.com
+*   RCSID:   $Id: sqMacUUID.c,v 1.3 2002/02/05 20:05:57 johnmci Exp $
+*
+*   Feb 5 2002, JMM cleanup for windows port
+*
+*/
+
 #include "UUIDPlugin.h"
 #include "sq.h"
 
 extern struct VirtualMachine *interpreterProxy;
 
 #if TARGET_API_MAC_CARBON
+#include <CFUUID.h>
 
-int MakeUUID(UUIDD * location) {
+int MakeUUID(sqUUID location) {
     CFUUIDRef theUUID;
     CFUUIDBytes theBytes;
 
     theUUID =  CFUUIDCreate (null);
     theBytes = CFUUIDGetUUIDBytes(theUUID);
-    memcpy((char *) location,(char *) &theBytes,sizeof(UUIDD));
+    memcpy((char *) location,(char *) &theBytes,sizeof(sqUUID));
     CFRelease(theUUID);
 }
 
@@ -19,14 +34,21 @@ int sqUUIDInit() {
     return 1;
 }
 
+int sqUUIDShutdown() {
+    return 1;
+}
 #else
 static Boolean gInit= false;
 int sqUUIDInit() {
     return 0;
 }
 
-MakeUUID(UUIDD * location) {
+MakeUUID(sqUUID location) {
     interpreterProxy->success(false);
+}
+
+int sqUUIDShutdown() {
+    return 0;
 }
 #endif
 
