6cf6f38445e13eb3902151206a0b0f08bc061aa9
John McIntosh <johnmci@smalltalkconsulting.com>
2002-02-12 18:26:09 +0000

    3.2.4 Mac Printing (real printing) Support using the classic print interface, and the new carbon print manger
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@198 fa1542d4-bde8-0310-ad64-8ed1123d492a

---
+++ platforms/Mac OS/plugins/PrintJobPlugin/sqMacPrinting.h
@@ -0,0 +1,61 @@
+/****************************************************************************
+*   PROJECT: Mac printing
+*   FILE:    sqMacPrinting.h
+*   CONTENT: 
+*
+*   AUTHOR:  John McIntosh.
+*   ADDRESS: 
+*   EMAIL:   johnmci@smalltalkconsulting.com
+*   RCSID:   $Id: sqMacPrinting.h,v 1.1 2002/02/12 18:26:09 johnmci Exp $
+*
+*   NOTES: 
+*
+*****************************************************************************/
+#if defined(__MWERKS__)
+	#include <PMApplication.h>
+#else
+	#if TARGET_API_MAC_CARBON	
+#include <Carbon/Carbon.h>
+	#else
+	#endif
+#endif
+
+typedef struct {
+	UInt32	numberOfPages;
+        UInt32	currentPage;
+        UInt32	firstPage;
+        UInt32	lastPage;
+        Boolean	allowPostscript;
+        void 	*formBitMap;
+        long	width;
+        long	height;
+        long	depth;
+        float	scaleW;
+        float	scaleH;
+        void	*postscript;
+        UInt32	postscriptLength;
+#if TARGET_API_MAC_CARBON	
+        Handle 	flatFormatHandle;
+        PMPrintSession printSession;
+        PMPageFormat	pageFormat;
+        PMPrintSettings printSettings;
+#else
+	    THPrint		thePrRecHdl;
+		TPPrPort	thePrPort;
+#endif
+}  PrintingLogic, *PrintingLogicPtr;
+
+
+int ioPrintSetup(PrintingLogicPtr  *printJob);
+int ioPrintPreProcessing(PrintingLogicPtr printJob,int numberOfPages);
+int ioPrint(PrintingLogicPtr printJob);
+int ioPrintPostProcessing(PrintingLogicPtr printJob);
+int ioPrintCleanup(PrintingLogicPtr *printJob);
+int ioInitPrintJob(void);
+int ioShutdownPrintJob(void);
+int ioPagePreProcessing(PrintingLogicPtr printJob);
+int ioPagePostProcessing(PrintingLogicPtr printJob);
+int ioPagePostscript(PrintingLogicPtr printJob,char *postscript,int postscriptLength);
+int ioPageForm(PrintingLogicPtr printJob, char *aBitMap,int w,int h,int d,float sh,float sw);
+int ioPrintGetFirstPageNumber(PrintingLogicPtr printJob);
+int ioPrintGetLastPageNumber(PrintingLogicPtr printJob);
