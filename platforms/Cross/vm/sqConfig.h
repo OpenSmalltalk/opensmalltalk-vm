39541ffa4217e780d9daa35816f3b101083ad215
Tim Rowledge <tim@rowledge.org>
2002-02-12 20:57:06 +0000

    remove default version to avoid confusing compilers. See platform specific directories
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@203 fa1542d4-bde8-0310-ad64-8ed1123d492a

--- platforms/Cross/vm/sqConfig.h
+++
@@ -1,8 +0,0 @@
-/* Default sqConfig.h -- platform identification and configuration
-   See platform specific directories for other versins of this
-   default file */
-
-
-#if !defined(SQ_CONFIG_DONE)
-# error test for, and describe, your architecture here.
-#endif
