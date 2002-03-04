d93d824b35211882f87a1732230c6ab36b2741b6
John McIntosh <johnmci@smalltalkconsulting.com>
2002-03-04 00:34:03 +0000

    3.2.5b6 more consolidation of memory logic from sqMacMain.c requires header changes
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@250 fa1542d4-bde8-0310-ad64-8ed1123d492a

--- platforms/Mac OS/vm/sqMacMemory.h
+++ platforms/Mac OS/vm/sqMacMemory.h
@@ -6,13 +6,16 @@
 *   AUTHOR:  John Maloney, John McIntosh, and others.
 *   ADDRESS: 
 *   EMAIL:   johnmci@smalltalkconsulting.com
-*   RCSID:   $Id: sqMacMemory.h,v 1.1 2002/02/23 10:47:44 johnmci Exp $
+*   RCSID:   $Id: sqMacMemory.h,v 1.2 2002/03/04 00:34:03 johnmci Exp $
 *
 *   NOTES: 
 *  Feb 22nd, 2002, JMM moved code into 10 other files, see sqMacMain.c for comments
+*  Mar 1st, 2002, JMM cleanup a bit. 
 ****************************************************************************/
 
 #if TARGET_API_MAC_CARBON
     #include <Carbon/Carbon.h>
 #else
 #endif
+UInt32	sqGetAvailableMemory(void);
+void sqMacMemoryFree();
