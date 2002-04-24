9f13c61a3aa18872732cfb9c9484ce514d791782
Tim Rowledge <tim@rowledge.org>
2002-04-24 17:56:36 +0000

    added for new plugin structure support
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@316 fa1542d4-bde8-0310-ad64-8ed1123d492a

---
+++ platforms/RiscOS/vm/osExports.c
@@ -0,0 +1,14 @@
+/* note: this file is only a backward compatible wrapper
+   for the old-style "platform.exports" definitions.
+   If your platform has migrated to the new exports
+   style you may as well insert the exports right here */
+#include <stdio.h>
+/* duh ... this is ugly */
+#define XFN(export) {"", #export, (void*)export},
+#define XFN2(plugin, export) {#plugin, #export, (void*)plugin##_##export}
+extern void  setSocketPollFunction(int spf );
+void *os_exports[][3] = {
+//	#include "platform.exports"
+   XFN(setSocketPollFunction)
+	{NULL, NULL, NULL}
+};
