306f7a3cbd7e46889c50ca4e496b091fd7433c80
John McIntosh <johnmci@smalltalkconsulting.com>
2002-04-27 18:56:16 +0000

    3.2.7b3 Added function def, housekeeping
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@325 fa1542d4-bde8-0310-ad64-8ed1123d492a

--- platforms/Mac OS/vm/sqMacMain.h
+++ platforms/Mac OS/vm/sqMacMain.h
@@ -6,10 +6,11 @@
 *   AUTHOR:  John Maloney, John McIntosh, and others.
 *   ADDRESS: 
 *   EMAIL:   johnmci@smalltalkconsulting.com
-*   RCSID:   $Id: sqMacMain.h,v 1.1 2002/02/23 10:47:37 johnmci Exp $
+*   RCSID:   $Id: sqMacMain.h,v 1.2 2002/04/27 18:56:16 johnmci Exp $
 *
 *   NOTES: 
 *  Feb 22nd, 2002, JMM moved code into 10 other files, see sqMacMain.c for comments
+*  Apr 17th, 2002, JMM os 9 check
 ****************************************************************************/
 
 #if TARGET_API_MAC_CARBON
@@ -18,4 +19,5 @@
 #endif
 
 Boolean RunningOnCarbonX(void);
+Boolean isSystem9_0_or_better(void);
 
