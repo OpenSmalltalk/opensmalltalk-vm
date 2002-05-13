0efa3d4b93907b2598e0e2b2b5eea7b61067fc17
John McIntosh <johnmci@smalltalkconsulting.com>
2002-05-13 19:48:44 +0000

    3.2.7b5 added api for open file with security flag
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@390 fa1542d4-bde8-0310-ad64-8ed1123d492a

--- DropPlugin/DropPlugin.h
+++ DropPlugin/DropPlugin.h
@@ -7,6 +7,6 @@ int dropShutdown(void);
 char* dropRequestFileName(int dropIndex); /* return name of file or NULL if error */
 /* note: dropRequestFileHandle needs to bypass plugin security checks when implemented */
 int dropRequestFileHandle(int dropIndex); /* return READ-ONLY file handle OOP or nilObject if error */
-int sqSetFileAccessCallback(void *);
+int sqSecFileAccessCallback(void *);
 void sqSetNumberOfDropFiles(int numberOfFiles);
 void sqSetFileInformation(int dropIndex, void *dropFile);
