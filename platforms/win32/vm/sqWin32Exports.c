0b93ad283d5ce3bbb0ae18f493a96901eab2e787
Andreas Raab <andreas.raab@gmx.de>
2002-06-17 12:35:05 +0000

    added printf to exports
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@419 fa1542d4-bde8-0310-ad64-8ed1123d492a

--- platforms/win32/vm/sqWin32Exports.c
+++ platforms/win32/vm/sqWin32Exports.c
@@ -29,6 +29,7 @@ void *os_exports[][3] = {
   {"","primitivePluginRequestFileHandle", primitivePluginRequestFileHandle},
   {"","primitivePluginDestroyRequest", primitivePluginDestroyRequest},
   {"","primitivePluginRequestState", primitivePluginRequestState},
+  ("", "printf", printf),
   {"","stWindow", &stWindow},
   {"","firstMessageHook", &firstMessageHook},
   {"","preMessageHook", &preMessageHook},
