e08d1a8e8767f27583c0573c0d3fe216b49b859a
John McIntosh <johnmci@smalltalkconsulting.com>
2003-06-20 01:56:32 +0000

    3.5.1b4 Added procedure def for fix for crash with virtual desktops
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@586 fa1542d4-bde8-0310-ad64-8ed1123d492a

--- platforms/Mac OS/vm/sqMacWindow.h
+++ platforms/Mac OS/vm/sqMacWindow.h
@@ -6,15 +6,17 @@
 *   AUTHOR:  John Maloney, John McIntosh, and others.
 *   ADDRESS: 
 *   EMAIL:   johnmci@smalltalkconsulting.com
-*   RCSID:   $Id: sqMacWindow.h,v 1.2 2002/05/09 19:26:05 johnmci Exp $
+*   RCSID:   $Id: sqMacWindow.h,v 1.3 2003/06/20 01:56:32 johnmci Exp $
 *
 *   NOTES: 
 *  Feb 22nd, 2002, JMM moved code into 10 other files, see sqMacMain.c for comments
 *  May 5th, 2002, JMM build as NS plugin
+*  Jun 7th, 2003, JMM fix up full screen threading issue
 ****************************************************************************/
 
 #if TARGET_API_MAC_CARBON
     #include <Carbon/Carbon.h>
+int ioSetFullScreenActual(int fullScreen);
 #else
 #endif
 
