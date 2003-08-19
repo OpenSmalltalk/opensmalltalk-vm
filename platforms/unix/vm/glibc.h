963915c7895d2b7371f5daa0bb6277b70c21475e
Ian Piumarta <piumarta@gmail.com>
2003-08-19 22:32:32 +0000

    Check __GLIBC_PREREQ defined before trying to use it.
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@685 fa1542d4-bde8-0310-ad64-8ed1123d492a

--- platforms/unix/vm/glibc.h
+++ platforms/unix/vm/glibc.h
@@ -1,14 +1,16 @@
 #include <features.h>
-#if defined(__GNUC__) && __GLIBC_PREREQ(2,3)
-  /* squash __ctype_to{upper,lower}_loc */
-# if defined(__USE_EXTERN_INLINES)
-#   undef __USE_EXTERN_INLINES
-#   include <ctype.h>
-#   undef toupper
-#   undef tolower
-#   define __USE_EXTERN_INLINES 1
+#if defined(__GNUC__) && defined(__GLIBC_PREREQ)
+# if __GLIBC_PREREQ(2,3)
+    /* squash __ctype_to{upper,lower}_loc */
+#   if defined(__USE_EXTERN_INLINES)
+#     undef __USE_EXTERN_INLINES
+#     include <ctype.h>
+#     undef toupper
+#     undef tolower
+#     define __USE_EXTERN_INLINES 1
+#   endif
+    /* squash realpath@GLIBC_2.3 */
+#   include <stdlib.h>
+    asm (".symver realpath, realpath@GLIBC_2.0");
 # endif
-  /* squash realpath@GLIBC_2.3 */
-# include <stdlib.h>
-  asm (".symver realpath, realpath@GLIBC_2.0");
 #endif
