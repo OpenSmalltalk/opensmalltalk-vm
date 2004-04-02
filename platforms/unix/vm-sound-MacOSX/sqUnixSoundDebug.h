4c7d2b868b37621a56531c6c72040ca0dc416d6c
Ian Piumarta <piumarta@gmail.com>
2004-04-02 01:15:08 +0000

    Disable sound debugging.
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@847 fa1542d4-bde8-0310-ad64-8ed1123d492a

--- platforms/unix/vm-sound-MacOSX/sqUnixSoundDebug.h
+++ platforms/unix/vm-sound-MacOSX/sqUnixSoundDebug.h
@@ -1,9 +1,15 @@
-#if 1
+#if 0
 
 # define startSpy();
 # define stopSpy();
 
 #else
+
+#define RED	0xff0000
+#define GREEN	0x00ff00
+#define BLUE	0x0000ff
+#define WHITE	0xffffff
+#define BLACK	0x000000
 
 #include <pthread.h>
 
