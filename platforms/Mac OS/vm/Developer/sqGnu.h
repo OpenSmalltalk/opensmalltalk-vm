713a70649719fcf40d9c90d4bdf66271bce20a7b
John McIntosh <johnmci@smalltalkconsulting.com>
2005-05-31 05:55:29 +0000

    mac 3.8.8b3 GCC 4.x does the jumptable need to be static? Evaluate further
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@1203 fa1542d4-bde8-0310-ad64-8ed1123d492a

--- platforms/Mac OS/vm/Developer/sqGnu.h
+++ platforms/Mac OS/vm/Developer/sqGnu.h
@@ -52,7 +52,7 @@
 #endif
 #define PRIM_DISPATCH	goto *jumpTable[foo->primitiveIndex]
 #define JUMP_TABLE \
-  static void *jumpTable[256]= { \
+   void *jumpTable[256]= { \
       &&_0,   &&_1,   &&_2,   &&_3,   &&_4,   &&_5,   &&_6,   &&_7,   &&_8,   &&_9, \
      &&_10,  &&_11,  &&_12,  &&_13,  &&_14,  &&_15,  &&_16,  &&_17,  &&_18,  &&_19, \
      &&_20,  &&_21,  &&_22,  &&_23,  &&_24,  &&_25,  &&_26,  &&_27,  &&_28,  &&_29, \
