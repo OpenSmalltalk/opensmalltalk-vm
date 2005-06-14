b1a6ace18b9e1a70a4d77ff685441ce41df54eef
John McIntosh <johnmci@smalltalkconsulting.com>
2005-06-14 06:26:12 +0000

    mac LocalePlugin 1.0.0 add for os-x carbon and os-9
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@1219 fa1542d4-bde8-0310-ad64-8ed1123d492a

---
+++ platforms/Mac OS/plugins/LocalePlugin/sqMacLocaleCarbon.h
@@ -0,0 +1,13 @@
+/*
+ *  sqMacLocaleCarbon.h
+ *  SqueakLocale
+ *
+ *  Created by John M McIntosh on 6/9/05.
+ *
+ */
+
+#include <Carbon/Carbon.h>
+#include "sqMemoryAccess.h"
+#include <locale.h>
+#include "sq.h"
+
