a0f37079ae48bd3868e8163ae0b5dfa7d4e2c6fd
Bernd Eckardt <bernd@impara.de>
2005-06-22 07:31:28 +0000

    Win32 changes: - some types adapted from int to sqInt - evt->utf32code changed - config.h added
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@1224 fa1542d4-bde8-0310-ad64-8ed1123d492a

--- SocketPlugin/sqWin32NewNet.c
+++ SocketPlugin/sqWin32NewNet.c
@@ -6,7 +6,7 @@
 *   AUTHOR:  Andreas Raab (ar)
 *   ADDRESS: University of Magdeburg, Germany
 *   EMAIL:   raab@isg.cs.uni-magdeburg.de
-*   RCSID:   $Id: sqWin32NewNet.c,v 1.9 2004/01/19 22:25:51 andreasraab Exp $
+*   RCSID:   $Id$
 *
 *   NOTES:
 *	1) TCP & UDP are now fully supported.
@@ -26,7 +26,7 @@
 #ifndef NO_NETWORK
 
 #ifndef NO_RCSID
-  static char RCSID[]="$Id: sqWin32NewNet.c,v 1.9 2004/01/19 22:25:51 andreasraab Exp $";
+  static char RCSID[]="$Id$";
 #endif
 
 #if 0
@@ -252,7 +252,7 @@ static int removeFromList(privateSocketStruct *pss) {
   }
 }
 
-/* cleanupSocket: 
+/* cleanupSocket:
    Clean up the private socket structure and associated elements.
    The function is called from the watcher threads when a socket
    is being destroyed.
@@ -336,7 +336,7 @@ static int acceptHandler(privateSocketStruct *pss)
   int addrSize = sizeof(struct sockaddr_in);
   struct sockaddr_in addr;
   acceptedSocketStruct *accepted = NULL;
-  
+
   /* accept incoming connections */
   result = accept(pss->s, (struct sockaddr*) &pss->peer, &addrSize);
   if(result != INVALID_SOCKET) {
@@ -481,7 +481,7 @@ static DWORD WINAPI readWatcherThread(privateSocketStruct *pss)
   int n, doWait, sema;
 
   while(1) {
-    doWait = 1; 
+    doWait = 1;
     /* Do we have a task to perform?! */
     if(pss->readWatcherOp) {
       /* Determine state of the socket */
@@ -490,7 +490,7 @@ static DWORD WINAPI readWatcherThread(privateSocketStruct *pss)
       pss->readSelect = 1;
       n = select(1, &fds, NULL, NULL, &tv);
       pss->readSelect = 0;
-      /* Note: select will return 
+      /* Note: select will return
 	 0 - if it timed out (unlikely but possible)
 	 1 - if the socket is readable
 	 SOCKET_ERROR - if the socket has been closed
@@ -544,11 +544,11 @@ static DWORD WINAPI readWatcherThread(privateSocketStruct *pss)
 	}
       }
     }
-    
+
     /* Wait until we have something to do */
     if(doWait && !pss->closePending)
       WaitForSingleObject(pss->hReadWatcherEvent, INFINITE);
-    
+
     /* Check if we need to close the socket */
     if(pss->closePending) {
       cleanupSocket(pss);
@@ -575,7 +575,7 @@ static DWORD WINAPI writeWatcherThread(privateSocketStruct *pss)
       pss->writeSelect = 1;
       n = select(1, NULL, &fds, &err, &tv);
       pss->writeSelect = 0;
-      /* Note: select will return 
+      /* Note: select will return
 	 0 - if it timed out (unlikely but possible)
 	 1 - if the socket is writable or an error occured
 	 SOCKET_ERROR - if the socket has been closed
@@ -637,7 +637,7 @@ static DWORD WINAPI writeWatcherThread(privateSocketStruct *pss)
     if(doWait && !pss->closePending) {
       WaitForSingleObject(pss->hWriteWatcherEvent, INFINITE);
     }
-    
+
     /* Check if we need to close the socket */
     if(pss->closePending) {
       cleanupSocket(pss);
@@ -646,7 +646,7 @@ static DWORD WINAPI writeWatcherThread(privateSocketStruct *pss)
     }
   }
 }
- 
+
 /*****************************************************************************
  *****************************************************************************
  *****************************************************************************/
@@ -684,7 +684,7 @@ static int createWatcherThreads(privateSocketStruct *pss)
   pss->hWriteWatcherEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
 
   /* Create the read watcher */
-  hThread = 
+  hThread =
     CreateThread(NULL,			   /* No security descriptor */
 		 pageSize,                 /* default stack size     */
 		 (LPTHREAD_START_ROUTINE) readWatcherThread, /* what to do */
@@ -704,9 +704,9 @@ static int createWatcherThreads(privateSocketStruct *pss)
     printLastError(TEXT("SetThreadPriority() failed"));
   if(!ResumeThread(hThread))
     printLastError(TEXT("ResumeThread() failed"));
-  
+
   /* Create the write watcher */
-  hThread = 
+  hThread =
     CreateThread(NULL,			   /* No security descriptor */
 		 pageSize,                 /* default stack size     */
 		 (LPTHREAD_START_ROUTINE) writeWatcherThread,/* what to do */
@@ -726,7 +726,7 @@ static int createWatcherThreads(privateSocketStruct *pss)
     printLastError(TEXT("SetThreadPriority() failed"));
   if(!ResumeThread(hThread))
     printLastError(TEXT("ResumeThread() failed"));
-  
+
   return 1;
 }
 
@@ -745,19 +745,19 @@ int sqNetworkInit(int resolverSemaIndex)
   if (thisNetSession != 0) return 0;  /* noop if network is already initialized */
 
   err = WSAStartup( MAKEWORD(2,0), &wsaData );
-  if ( err != 0 ) 
+  if ( err != 0 )
     return -1;
 
   /* Confirm that the Windows Sockets DLL supports 1.1 or greater */
   if(HIBYTE(wsaData.wVersion < 1) || HIBYTE(wsaData.wVersion) > 2) {
     WSACleanup();
-    return -1; 
+    return -1;
   }
 
   gethostname(localHostName,MAXHOSTNAMELEN);
   thisNetSession = GetTickCount();
   if (thisNetSession == 0) thisNetSession = 1;  /* don't use 0 */
-  
+
   /* install resolver semaphore */
   resolverSemaphoreIndex = resolverSemaIndex;
 
@@ -936,7 +936,7 @@ void sqSocketConnectToPort(SocketPtr s, int addr, int port)
 }
 
 /*****************************************************************************
-  sqSocketListenOnPort: 
+  sqSocketListenOnPort:
 	TCP => start listening for incoming connections.
 	UDP => associate the local port number with the socket.
 *****************************************************************************/
@@ -945,14 +945,14 @@ void sqSocketListenOnPort(SocketPtr s, int port)
   int result;
   struct sockaddr_in addr;
   privateSocketStruct *pss = PSP(s);
-	
+
   if (!SocketValid(s)) return;
   /* bind the socket */
   ZeroMemory(&addr,sizeof(struct sockaddr_in));
   addr.sin_family = AF_INET;
   addr.sin_port = htons((short)port);
   addr.sin_addr.s_addr = localHostAddress;
-  
+
   result = bind( SOCKET(s), (struct sockaddr*) &addr, sizeof(struct sockaddr_in));
   if(result == SOCKET_ERROR) {
     pss->sockError = WSAGetLastError();
@@ -976,7 +976,7 @@ void sqSocketListenOnPort(SocketPtr s, int port)
 }
 
 /*****************************************************************************
-  sqSocketListenOnPortBacklogSize: 
+  sqSocketListenOnPortBacklogSize:
 	TCP => start listening for incoming connections.
 	UDP => Just call sqListenOnPort
 *****************************************************************************/
@@ -1026,7 +1026,7 @@ void sqSocketListenOnPortBacklogSizeInterface(SocketPtr s, int port, int backlog
 }
 
 /*****************************************************************************
-  sqSocketDestroy: Release the resources associated with this socket. 
+  sqSocketDestroy: Release the resources associated with this socket.
                    If a connection is open, it is aborted
 *****************************************************************************/
 void sqSocketDestroy(SocketPtr s)
@@ -1078,7 +1078,7 @@ int sqSocketReceiveDataAvailable(SocketPtr s)
   Receive data into the given buffer. Do not exceed bufSize.
   Return the number of bytes actually read.
 *****************************************************************************/
-int sqSocketReceiveDataBufCount(SocketPtr s, int buf, int bufSize)
+sqInt sqSocketReceiveDataBufCount(SocketPtr s, char *buf, sqInt bufSize)
 {
   privateSocketStruct *pss = PSP(s);
   int result;
@@ -1090,10 +1090,10 @@ int sqSocketReceiveDataBufCount(SocketPtr s, int buf, int bufSize)
   /* read incoming data */
   if(UDPSocketType == pss->sockType) { /* UDP */
     addrSize = sizeof(pss->peer);
-    result = recvfrom(pss->s, (void*)buf, bufSize, 0, 
+    result = recvfrom(pss->s, buf, bufSize, 0,
 		      (struct sockaddr*) &pss->peer, &addrSize);
   } else { /* TCP */
-    result = recv(pss->s,(void*)buf, bufSize, 0);
+    result = recv(pss->s,buf, bufSize, 0);
   }
 
 /* printf("Data read (%d) WSAGetLastError (%d)\n", result, WSAGetLastError()); */
@@ -1139,7 +1139,7 @@ int sqSocketReceiveDataBufCount(SocketPtr s, int buf, int bufSize)
 /*****************************************************************************
   sqSocketSendDone: Return non-zero if all data has been sent.
 *****************************************************************************/
-int sqSocketSendDone(SocketPtr s)
+sqInt sqSocketSendDone(SocketPtr s)
 {
   int sockState;
 
@@ -1152,10 +1152,10 @@ int sqSocketSendDone(SocketPtr s)
 
 /*****************************************************************************
   sqSocketSendDataBufCount:
-  Send bufSize bytes from the data pointed to by buf. 
+  Send bufSize bytes from the data pointed to by buf.
   Return the number of bytes sent.
 *****************************************************************************/
-int sqSocketSendDataBufCount(SocketPtr s, int buf, int bufSize)
+sqInt sqSocketSendDataBufCount(SocketPtr s, char *buf, sqInt bufSize)
 {
   privateSocketStruct *pss = PSP(s);
   int result;
@@ -1173,10 +1173,10 @@ int sqSocketSendDataBufCount(SocketPtr s, int buf, int bufSize)
   /* send actual data */
   if(UDPSocketType == pss->sockType) { /* UDP */
     addrSize = sizeof(pss->peer);
-    result = sendto(pss->s, (void*)buf, bufSize, 0, 
+    result = sendto(pss->s, buf, bufSize, 0,
 		    (struct sockaddr*) &pss->peer, addrSize);
   } else {
-    result = send(pss->s, (void*)buf, bufSize, 0);
+    result = send(pss->s, buf, bufSize, 0);
   }
 /* printf("Data sent (%d) WSAGetLastError (%d)\n", result, WSAGetLastError()); */
 
@@ -1362,11 +1362,11 @@ void	sqSocketCreateNetTypeSocketTypeRecvBytesSendBytesSemaIDReadSemaIDWriteSemaI
   /* Create a new mutex object for synchronized access */
   pss->mutex = CreateMutex(NULL, 0,NULL);
   if(!pss->mutex) { FAIL(); return; }
-  
+
   /* Install the socket into the socket list */
   pss->next = firstSocket;
   firstSocket = pss;
-  
+
   /* Setup the watchers */
   if(UDPSocketType == socketType) {
     /* Since UDP sockets are always connected */
@@ -1407,7 +1407,7 @@ void sqSocketAcceptFromRecvBytesSendBytesSemaIDReadSemaIDWriteSemaID(SocketPtr s
     pss->accepted = accepted->next;
     if(!pss->accepted) {
       /* No more connections; go back to waiting state and start watcher */
-      pss->sockState = WaitingForConnection; 
+      pss->sockState = WaitingForConnection;
       pss->readWatcherOp = WatchAccept;
       SetEvent(pss->hReadWatcherEvent);
     }
@@ -1432,12 +1432,12 @@ void sqSocketAcceptFromRecvBytesSendBytesSemaIDReadSemaIDWriteSemaID(SocketPtr s
   pss->sockState= Connected | SOCK_DATA_WRITABLE;
   pss->sockError= 0;
   MoveMemory(&pss->peer, &accepted->peer, sizeof(struct sockaddr_in));
-  
+
   /* fill the SQSocket */
   s->sessionID = thisNetSession;
   s->socketType = pss->sockType;
   s->privateSocketPtr = pss;
-	
+
   /* Disable TCP delays */
   setsockopt(SOCKET(s), IPPROTO_TCP, TCP_NODELAY, (char*) &one, sizeof(one));
   /* Make the socket non-blocking */
@@ -1464,10 +1464,10 @@ void sqSocketAcceptFromRecvBytesSendBytesSemaIDReadSemaIDWriteSemaID(SocketPtr s
   GlobalFree(GlobalHandle(accepted));
 }
 
-int sqSocketReceiveUDPDataBufCountaddressportmoreFlag(SocketPtr s, int buf, int bufSize,  int *address,  int *port, int *moreFlag)
+sqInt sqSocketReceiveUDPDataBufCountaddressportmoreFlag(SocketPtr s, char *buf, sqInt bufSize,  sqInt *address,  sqInt *port, sqInt *moreFlag)
 {
   int nRead;
-  if(UDPSocketType != s->socketType) 
+  if(UDPSocketType != s->socketType)
     return interpreterProxy->primitiveFail();
   /* bind UDP socket*/
   sqSocketConnectToPort(s, *address, *port);
@@ -1481,9 +1481,9 @@ int sqSocketReceiveUDPDataBufCountaddressportmoreFlag(SocketPtr s, int buf, int 
   return nRead;
 }
 
-int	sqSockettoHostportSendDataBufCount(SocketPtr s, int address, int port, int buf, int bufSize)
+sqInt	sqSockettoHostportSendDataBufCount(SocketPtr s, sqInt address, sqInt port, char *buf, sqInt bufSize)
 {
-  if(UDPSocketType != s->socketType) 
+  if(UDPSocketType != s->socketType)
     return interpreterProxy->primitiveFail();
   /* bind UDP socket */
   sqSocketConnectToPort(s, address, port);
@@ -1574,21 +1574,21 @@ static socketOption *findOption(char *name, size_t nameSize) {
 /*
   set the given option for the socket.
 */
-int sqSocketSetOptionsoptionNameStartoptionNameSizeoptionValueStartoptionValueSizereturnedValue
-    (SocketPtr s,int optionName, int optionNameSize,
-     int optionValueIndex, int optionValueSize, int *result)
+sqInt sqSocketSetOptionsoptionNameStartoptionNameSizeoptionValueStartoptionValueSizereturnedValue
+    (SocketPtr s,char *optionName, sqInt optionNameSize,
+     char *optionValueIndex, sqInt optionValueSize, sqInt *result)
 {
   char optionValue[256];
   size_t bufSize;
   unsigned char   buf[256];
 
   if (SocketValid(s)) {
-    socketOption *opt= findOption((char *)optionName, (size_t)optionNameSize);
+    socketOption *opt= findOption(optionName, (size_t)optionNameSize);
 
     if (opt == 0) goto barf;
     if(optionValueSize >= sizeof(optionValue)) goto barf;
 
-    memcpy(optionValue, (void*)optionValueIndex, optionValueSize);
+    memcpy(optionValue, optionValueIndex, optionValueSize);
     optionValue[optionValueSize] = 0;
 
     if(opt->optType == 1) {
@@ -1644,21 +1644,21 @@ int sqSocketSetOptionsoptionNameStartoptionNameSizeoptionValueStartoptionValueSi
 
 
 /* query the socket for the given option.  */
-int sqSocketGetOptionsoptionNameStartoptionNameSizereturnedValue
-    (SocketPtr s,int optionName, int optionNameSize, int *result)
+sqInt sqSocketGetOptionsoptionNameStartoptionNameSizereturnedValue
+    (SocketPtr s,char *optionName, sqInt optionNameSize, sqInt *result)
 {
   int optval;
   size_t len;
   socketOption *opt;
   if (!SocketValid(s)) goto barf;
-  opt= findOption((char *)optionName, (size_t)optionNameSize);
+  opt= findOption(optionName, (size_t)optionNameSize);
   if (opt == 0) {
     /* printf("option not found\n"); */
     goto barf;
   }
   if (opt->optType == 1) {
     len= sizeof(optval);
-    if ((getsockopt(SOCKET(s), opt->optLevel, opt->optName, 
+    if ((getsockopt(SOCKET(s), opt->optLevel, opt->optName,
 		    (void*)&optval,&len)) < 0)
       {
 	/* printf("getsockopt() returned < 0\n"); */
@@ -1678,14 +1678,14 @@ int sqSocketGetOptionsoptionNameStartoptionNameSizereturnedValue
     struct sockaddr_in addr;
     /* WSAIoctl() */
     if(opt->optName != SIO_GET_BROADCAST_ADDRESS) goto barf;
-    err = WSAIoctl(SOCKET(s), 
-		   SIO_GET_BROADCAST_ADDRESS, 
-		   NULL, 0, 
-		   &addr, sizeof(addr), 
-		   &sz, 
+    err = WSAIoctl(SOCKET(s),
+		   SIO_GET_BROADCAST_ADDRESS,
+		   NULL, 0,
+		   &addr, sizeof(addr),
+		   &sz,
 		   NULL, NULL);
     if(err) {
-      printf("WSAIoctl error: %d (WSAGetLastError=%d)\n", 
+      printf("WSAIoctl error: %d (WSAGetLastError=%d)\n",
 	     err, WSAGetLastError());
       goto barf;
     }
@@ -1832,8 +1832,8 @@ void sqResolverStartNameLookup(char *hostName, int nameSize)
 
   if(asyncLookupHandle) return; /* lookup in progress */
   len = nameSize < MAXHOSTNAMELEN ? nameSize : MAXHOSTNAMELEN;
-  if((lastError == 0) && 
-     (strlen(lastName) == len) && 
+  if((lastError == 0) &&
+     (strlen(lastName) == len) &&
      (strncmp(hostName, lastName, len) == 0)) {
 	  /* same as last, no point in looking it up */
 	  SIGNAL(resolverSemaphoreIndex);
@@ -1886,7 +1886,7 @@ DWORD WINAPI sqGetHostByAddr(int netAddress)
 
   nAddr = htonl(netAddress);
   he = gethostbyaddr((char*)&nAddr, 4, PF_INET);
-  if(he) 
+  if(he)
     {
       strcpy(lastName,he->h_name);
       lastAddr = ntohl(*(long*)(he->h_addr_list[0]));
@@ -1908,7 +1908,7 @@ DWORD WINAPI sqGetHostByName(char *hostName)
 { struct hostent *he;
 
   he = gethostbyname(hostName);
-  if(he) 
+  if(he)
     {
       strcpy(lastName,he->h_name);
       lastAddr = ntohl(*(long*)(he->h_addr_list[0]));
