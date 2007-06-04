6cd452110ef7f2364b6a74891340c0f149d21eb7
Andreas Raab <andreas.raab@gmx.de>
2007-06-04 05:20:33 +0000

    Unicodification of AsyncFile primitives.
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@1700 fa1542d4-bde8-0310-ad64-8ed1123d492a

--- AsynchFilePlugin/sqWin32AsyncFilePrims.c
+++ AsynchFilePlugin/sqWin32AsyncFilePrims.c
@@ -6,7 +6,7 @@
 *   AUTHOR:  Andreas Raab (ar)
 *   ADDRESS: University of Magdeburg, Germany
 *   EMAIL:   raab@isg.cs.uni-magdeburg.de
-*   RCSID:   $Id: sqWin32AsyncFilePrims.c,v 1.2 2002/05/04 23:20:27 andreasraab Exp $
+*   RCSID:   $Id$
 *
 *   NOTES:
 *     1) Instead of the async io functions (e.g., ReadFileEx/WriteFileEx)
@@ -55,21 +55,21 @@
 */
 
 typedef struct {
-	/* Win stuff */
-	HANDLE hFile;		/* The file handle we're operating on */
-	HANDLE hThread;		/* The thread running async IO */
-	HANDLE hEvent;		/* Event for communicating with the thread */
-	DWORD  dwPosition;	/* File position to start reading */
-	DWORD  dwSize;		/* Number of bytes to transfer */
-	BOOL   rFlag;		/* Do we read or write?! */
+  /* Win stuff */
+  HANDLE hFile;		/* The file handle we're operating on */
+  HANDLE hThread;		/* The thread running async IO */
+  HANDLE hEvent;		/* Event for communicating with the thread */
+  DWORD  dwPosition;	/* File position to start reading */
+  DWORD  dwSize;		/* Number of bytes to transfer */
+  BOOL   rFlag;		/* Do we read or write?! */
 
-	/* Squeak stuff */
-	int    writable;	 /* Was the file opened for write? */
-	int    semaIndex;	 /* The semaphore to signal upon completion */
-	int    status;	 /* State of affairs */
-	DWORD  bytesTransferred; /* The number of bytes actually transferred */
-	int    bufferSize; /* the size of the buffer */
-	char 	*bufferPtr;	 /* the data buffer */
+  /* Squeak stuff */
+  int    writable;	 /* Was the file opened for write? */
+  int    semaIndex;	 /* The semaphore to signal upon completion */
+  int    status;	 /* State of affairs */
+  DWORD  bytesTransferred; /* The number of bytes actually transferred */
+  int    bufferSize; /* the size of the buffer */
+  char 	*bufferPtr;	 /* the data buffer */
 } AsyncFileState;
 
 /*** Status Values ***/
@@ -96,34 +96,34 @@ int asyncFileWriteStart(AsyncFile *f, int fPosition, int bufferPtr, int bufferSi
 *****************************************************************************/
 DWORD WINAPI sqAsyncFileThread(AsyncFileState *state)
 {
-	BOOL ok;
+  BOOL ok;
 
-	while(state->hFile != INVALID_HANDLE_VALUE) {
-		WaitForSingleObject(state->hEvent, INFINITE);
-		if(state->hFile == INVALID_HANDLE_VALUE) break;
-		if(!state->bufferPtr) {
-			state->status = LAST_OP_FAILED;
-			continue;
-		}
-		/* Seek to r/w position */
-		if(SetFilePointer(state->hFile, state->dwPosition, NULL, FILE_BEGIN) == (DWORD)-1) {
-			state->status = LAST_OP_FAILED;
-			continue;
-		}
-		if(state->rFlag) {
-			ok = ReadFile(state->hFile, state->bufferPtr, state->dwSize, &state->bytesTransferred, NULL);
-		} else {
-			ok = WriteFile(state->hFile, state->bufferPtr, state->dwSize, &state->bytesTransferred, NULL);
-		}
-		if(ok)
-			state->status = IDLE;
-		else
-			state->status = LAST_OP_FAILED;
-		synchronizedSignalSemaphoreWithIndex(state->semaIndex);
-	}
-	state->hThread = NULL;
-	ExitThread(0);
-	return 1;
+  while(state->hFile != INVALID_HANDLE_VALUE) {
+    WaitForSingleObject(state->hEvent, INFINITE);
+    if(state->hFile == INVALID_HANDLE_VALUE) break;
+    if(!state->bufferPtr) {
+      state->status = LAST_OP_FAILED;
+      continue;
+    }
+    /* Seek to r/w position */
+    if(SetFilePointer(state->hFile, state->dwPosition, NULL, FILE_BEGIN) == (DWORD)-1) {
+      state->status = LAST_OP_FAILED;
+      continue;
+    }
+    if(state->rFlag) {
+      ok = ReadFile(state->hFile, state->bufferPtr, state->dwSize, &state->bytesTransferred, NULL);
+    } else {
+      ok = WriteFile(state->hFile, state->bufferPtr, state->dwSize, &state->bytesTransferred, NULL);
+    }
+    if(ok)
+      state->status = IDLE;
+    else
+      state->status = LAST_OP_FAILED;
+    synchronizedSignalSemaphoreWithIndex(state->semaIndex);
+  }
+  state->hThread = NULL;
+  ExitThread(0);
+  return 1;
 }
 
 
@@ -132,43 +132,43 @@ DWORD WINAPI sqAsyncFileThread(AsyncFileState *state)
 *****************************************************************************/
 
 void *asyncAlloc(DWORD size) {
-	return GlobalLock(GlobalAlloc(GMEM_MOVEABLE, size));
+  return GlobalLock(GlobalAlloc(GMEM_MOVEABLE, size));
 }
 
 void asyncFree(void *ptr) {
-	GlobalFree(GlobalHandle(ptr));
+  GlobalFree(GlobalHandle(ptr));
 }
 
 int asyncFileAllocateBuffer(AsyncFileState *state, int byteCount) {
   /* Allocate a new buffer of the given size if necessary. If the current buffer
-	 is already allocated and of the desired size, do nothing. */
+     is already allocated and of the desired size, do nothing. */
 
-	if ((state->bufferPtr != NULL) && (state->bufferSize >= byteCount)) {
-		return 0;  /* buffer is already allocated and of (at least) the desired size */
-	}
+  if ((state->bufferPtr != NULL) && (state->bufferSize >= byteCount)) {
+    return 0;  /* buffer is already allocated and of (at least) the desired size */
+  }
 
-	/* free old buffer, if any */
-	if (state->bufferPtr != NULL) {
-		asyncFree(state->bufferPtr);
-		state->bufferSize = 0;
-		state->bufferPtr = NULL;
-	}
-	/* allocate new buffer */
-	state->bufferPtr = asyncAlloc(byteCount);
-	if (state->bufferPtr == NULL) {
-		state->bufferSize = 0;
-		return success(false);  /* could not allocate a buffer of size count */
-	}
-	state->bufferSize = byteCount;
-	return 1;
+  /* free old buffer, if any */
+  if (state->bufferPtr != NULL) {
+    asyncFree(state->bufferPtr);
+    state->bufferSize = 0;
+    state->bufferPtr = NULL;
+  }
+  /* allocate new buffer */
+  state->bufferPtr = asyncAlloc(byteCount);
+  if (state->bufferPtr == NULL) {
+    state->bufferSize = 0;
+    return success(false);  /* could not allocate a buffer of size count */
+  }
+  state->bufferSize = byteCount;
+  return 1;
 }
 
 int asyncFileValid(AsyncFile *f) {
-	return (
-		(f != NULL) &&
-		(f->sessionID == thisSession) &&
-		(f->state != NULL) &&
-		(((AsyncFileState *) f->state)->hFile != INVALID_HANDLE_VALUE));
+  return (
+	  (f != NULL) &&
+	  (f->sessionID == thisSession) &&
+	  (f->state != NULL) &&
+	  (((AsyncFileState *) f->state)->hFile != INVALID_HANDLE_VALUE));
 }
 
 /*****************************************************************************
@@ -176,196 +176,202 @@ int asyncFileValid(AsyncFile *f) {
 *****************************************************************************/
 
 int asyncFileClose(AsyncFile *f) {
-	/* Close the given asynchronous file. */
+  /* Close the given asynchronous file. */
 
-	AsyncFileState *state;
+  AsyncFileState *state;
 
-	if (!asyncFileValid(f)) return 0;  /* already closed */
-	state = (AsyncFileState*) f->state;
-	if(!CloseHandle(state->hFile)) {
-		printLastError(TEXT("AsyncFileClose failed"));
-		success(false);
-    }
-	state->hFile = INVALID_HANDLE_VALUE;
-	SetEvent(state->hEvent);
-	WaitForSingleObject(state->hThread, 5000);
-	if(state->hThread) {
-		warnPrintf(TEXT("Terminating async thread"));
-		TerminateThread(state->hThread,0);
-	}
-	CloseHandle(state->hEvent);
-	if (state->bufferPtr != NULL) asyncFree(state->bufferPtr);
-	free((void *) f->state);
-	f->state = NULL;
-	f->sessionID = 0;
-	return 1;
+  if (!asyncFileValid(f)) return 0;  /* already closed */
+  state = (AsyncFileState*) f->state;
+  if(!CloseHandle(state->hFile)) {
+    printLastError(TEXT("AsyncFileClose failed"));
+    success(false);
+  }
+  state->hFile = INVALID_HANDLE_VALUE;
+  SetEvent(state->hEvent);
+  WaitForSingleObject(state->hThread, 5000);
+  if(state->hThread) {
+    warnPrintf(TEXT("Terminating async thread"));
+    TerminateThread(state->hThread,0);
+  }
+  CloseHandle(state->hEvent);
+  if (state->bufferPtr != NULL) asyncFree(state->bufferPtr);
+  free((void *) f->state);
+  f->state = NULL;
+  f->sessionID = 0;
+  return 1;
 }
 
-int asyncFileOpen(AsyncFile *f, int fileNamePtr, int fileNameSize, int writeFlag, int semaIndex) {
+int asyncFileOpen(AsyncFile *f, int fileNamePtr, int fileNameSize, 
+		  int writeFlag, int semaIndex) {
   /* Opens the given file using the supplied AsyncFile structure to record
-	 its state. Fails with no side effects if f is already open. Files are
-	 always opened in binary mode. */
+     its state. Fails with no side effects if f is already open. Files are
+     always opened in binary mode. */
 
-	int i;
-	TCHAR cFileName[256];
-	AsyncFileState *state;
-	HANDLE hFile;
-	DWORD id;
+  int i;
+  char  cFileName[256];
+  WCHAR wFileName[256];
+  AsyncFileState *state;
+  HANDLE hFile;
+  DWORD id;
 
-	/* don''t open an already open file */
-	if (asyncFileValid(f)) return success(false);
+  /* don''t open an already open file */
+  if (asyncFileValid(f)) return success(false);
 
-	/* copy the file name into a null-terminated C string */
-	if (fileNameSize > 255) return success(false);
-	for (i = 0; i < fileNameSize; i++) {
-		cFileName[i] = *((char *) (fileNamePtr + i));
-	}
-	cFileName[fileNameSize] = 0;
+  /* copy the file name into a null-terminated C string */
+  if (fileNameSize > 255) return success(false);
+  for (i = 0; i < fileNameSize; i++) {
+    cFileName[i] = *((char *) (fileNamePtr + i));
+  }
+  cFileName[fileNameSize] = 0;
 
-	f->sessionID = 0;
-	hFile = CreateFile(cFileName,
-						writeFlag ? (GENERIC_READ | GENERIC_WRITE) : GENERIC_READ,
-						writeFlag ? FILE_SHARE_READ : (FILE_SHARE_READ | FILE_SHARE_WRITE),
-						NULL, /* No security descriptor */
-						writeFlag ? OPEN_ALWAYS : OPEN_EXISTING,
-						FILE_ATTRIBUTE_NORMAL,
-						NULL /* No template */);
-	if(hFile == INVALID_HANDLE_VALUE)
-		return success(false);
-	f->state = (AsyncFileState *) calloc(1,sizeof(AsyncFileState));	/* allocate state record */
-	if (f->state == NULL) {
-		CloseHandle(hFile);
-		return success(false);
-	}
-	f->sessionID = thisSession;
-	state = (AsyncFileState *) f->state;
-	state->hFile = hFile;
-	state->writable = writeFlag;
-	state->semaIndex = semaIndex;
-	state->status = IDLE;
-	state->bytesTransferred = 0;
-	state->bufferSize = 0;
-	state->bufferPtr = NULL;
-	state->hEvent = CreateEvent(NULL, 0, 0, NULL);
-	state->hThread =
-		CreateThread(NULL,                    /* No security descriptor */
-					0,                       /* default stack size     */
-					(LPTHREAD_START_ROUTINE) &sqAsyncFileThread, /* what to do */
-					(LPVOID) state,       /* parameter for thread   */
-					CREATE_SUSPENDED,        /* creation parameter -- create suspended so we can check the return value */
-					&id);                    /* return value for thread id */
-	if(!state->hThread) {
-		printLastError(TEXT("CreateThread() failed"));
-		return success(false);
-	}
-	/* file operations run with high priority */
-	if(!SetThreadPriority(state->hThread, THREAD_PRIORITY_HIGHEST))
-		printLastError(TEXT("SetThreadPriority() failed"));
-	if(!ResumeThread(state->hThread)) {
-		printLastError(TEXT("ResumeThread() failed"));
-		return success(false);
-	}
-	return 1;
+  /* Convert to Unicode */
+  MultiByteToWideChar(CP_UTF8, 0, cFileName, -1, wFileName, 255);
+
+  f->sessionID = 0;
+  hFile = CreateFileW(wFileName,
+	    writeFlag ? (GENERIC_READ | GENERIC_WRITE) : GENERIC_READ,
+	    writeFlag ? FILE_SHARE_READ : (FILE_SHARE_READ | FILE_SHARE_WRITE),
+	    NULL, /* No security descriptor */
+	    writeFlag ? OPEN_ALWAYS : OPEN_EXISTING,
+	    FILE_ATTRIBUTE_NORMAL,
+	    NULL /* No template */);
+  if(hFile == INVALID_HANDLE_VALUE)
+    return success(false);
+  f->state = (AsyncFileState *) 
+    calloc(1,sizeof(AsyncFileState));	/* allocate state record */
+  if (f->state == NULL) {
+    CloseHandle(hFile);
+    return success(false);
+  }
+  f->sessionID = thisSession;
+  state = (AsyncFileState *) f->state;
+  state->hFile = hFile;
+  state->writable = writeFlag;
+  state->semaIndex = semaIndex;
+  state->status = IDLE;
+  state->bytesTransferred = 0;
+  state->bufferSize = 0;
+  state->bufferPtr = NULL;
+  state->hEvent = CreateEvent(NULL, 0, 0, NULL);
+  state->hThread =
+    CreateThread(NULL,                    /* No security descriptor */
+		 0,                       /* default stack size     */
+		 (LPTHREAD_START_ROUTINE) &sqAsyncFileThread, /* what to do */
+		 (LPVOID) state,       /* parameter for thread   */
+		 CREATE_SUSPENDED,        /* create suspended */
+		 &id);                    /* return value for thread id */
+  if(!state->hThread) {
+    printLastError(TEXT("CreateThread() failed"));
+    return success(false);
+  }
+  /* file operations run with high priority */
+  if(!SetThreadPriority(state->hThread, THREAD_PRIORITY_HIGHEST))
+    printLastError(TEXT("SetThreadPriority() failed"));
+  if(!ResumeThread(state->hThread)) {
+    printLastError(TEXT("ResumeThread() failed"));
+    return success(false);
+  }
+  return 1;
 }
 
 int asyncFileReadResult(AsyncFile *f, int bufferPtr, int bufferSize) {
   /* Copy up to bufferSize bytes from the buffer of the last read operation
-	 into the given Squeak buffer, and return the number of bytes copied.
-	 Negative values indicate:
-		-1    -- busy; the last operation has not finished yet
-		-2    -- error; the last operation failed
-	Note that a read operation may read fewer bytes than requested if, for
-	example, there are fewer than the requested number of bytes between the
-	starting file position of the read operation and the end-of-file. */
+     into the given Squeak buffer, and return the number of bytes copied.
+     Negative values indicate:
+     -1    -- busy; the last operation has not finished yet
+     -2    -- error; the last operation failed
+     Note that a read operation may read fewer bytes than requested if, for
+     example, there are fewer than the requested number of bytes between the
+     starting file position of the read operation and the end-of-file. */
 
-	AsyncFileState *state = (AsyncFileState *) f->state;
-	int bytesRead;
+  AsyncFileState *state = (AsyncFileState *) f->state;
+  int bytesRead;
 
-	if (!asyncFileValid(f)) return success(false);
-	if (state->status == BUSY) return -1;
-	if (state->status == LAST_OP_FAILED) return -2;
-
-	/* copy the file buffer into the squeak buffer */
-	bytesRead = (bufferSize < state->bytesTransferred) ? bufferSize : state->bytesTransferred;
-	MoveMemory((void *) bufferPtr, (void *)state->bufferPtr, bytesRead);
-	return bytesRead;
+  if (!asyncFileValid(f)) return success(false);
+  if (state->status == BUSY) return -1;
+  if (state->status == LAST_OP_FAILED) return -2;
+  
+  /* copy the file buffer into the squeak buffer */
+  bytesRead = (bufferSize < state->bytesTransferred) ? bufferSize : state->bytesTransferred;
+  MoveMemory((void *) bufferPtr, (void *)state->bufferPtr, bytesRead);
+  return bytesRead;
 }
 
 int asyncFileReadStart(AsyncFile *f, int fPosition, int count) {
   /* Start an asynchronous operation to read count bytes from the given file
-	 starting at the given file position. The file''s semaphore will be signalled when
-	 the operation is complete. The client may then use asyncFileReadResult() to
-	 find out if the operation succeeded and to get the data that was read. */
-	AsyncFileState *state = (AsyncFileState *) f->state;
+     starting at the given file position. The file''s semaphore will be signalled when
+     the operation is complete. The client may then use asyncFileReadResult() to
+     find out if the operation succeeded and to get the data that was read. */
+  AsyncFileState *state = (AsyncFileState *) f->state;
+  
+  if (!asyncFileValid(f)) return success(false);
+  if (state->status == BUSY) return success(false);  /* operation in progress */
+  
+  /* allocate a new buffer if necessary */
+  asyncFileAllocateBuffer(state, count);
+  if (state->bufferPtr == NULL) return success(false);  /* could not allocate buffer */
 
-	if (!asyncFileValid(f)) return success(false);
-	if (state->status == BUSY) return success(false);  /* operation in progress */
-
-	/* allocate a new buffer if necessary */
-	asyncFileAllocateBuffer(state, count);
-	if (state->bufferPtr == NULL) return success(false);  /* could not allocate buffer */
-
-	state->dwPosition = fPosition;
-	state->dwSize = count;
-	state->status = BUSY;
-	state->rFlag = TRUE;
-	SetEvent(state->hEvent);
-	return 1;
+  state->dwPosition = fPosition;
+  state->dwSize = count;
+  state->status = BUSY;
+  state->rFlag = TRUE;
+  SetEvent(state->hEvent);
+  return 1;
 }
 
 int asyncFileRecordSize() {
-	return sizeof(AsyncFile);
+  return sizeof(AsyncFile);
 }
 
 int asyncFileWriteResult(AsyncFile *f) {
   /* Return the number of bytes copied by the last write operation.
-	 Negative values indicate:
-		-1    -- busy; the last operation has not finished yet
-		-2    -- error; the last operation failed */
+     Negative values indicate:
+     -1    -- busy; the last operation has not finished yet
+     -2    -- error; the last operation failed */
 
-	AsyncFileState *state = (AsyncFileState *) f->state;
-
-	if (!asyncFileValid(f)) return success(false);
-	if (state->status == BUSY) return -1;
-	if (state->status == LAST_OP_FAILED) return -2;
-	return state->bytesTransferred;
+  AsyncFileState *state = (AsyncFileState *) f->state;
+  
+  if (!asyncFileValid(f)) return success(false);
+  if (state->status == BUSY) return -1;
+  if (state->status == LAST_OP_FAILED) return -2;
+  return state->bytesTransferred;
 }
 
 int asyncFileWriteStart(AsyncFile *f, int fPosition, int bufferPtr, int bufferSize) {
   /* Start an asynchronous operation to write bufferSize bytes to the given file
-	 starting at the given file position. The file''s semaphore will be signalled when
-	 the operation is complete. The client may then use asyncFileWriteResult() to
-	 find out if the operation succeeded and how many bytes were actually written. */
-	AsyncFileState *state = (AsyncFileState *) f->state;
+     starting at the given file position. The file''s semaphore will be signalled when
+     the operation is complete. The client may then use asyncFileWriteResult() to
+     find out if the operation succeeded and how many bytes were actually written. */
+  AsyncFileState *state = (AsyncFileState *) f->state;
 
-	if (!asyncFileValid(f)) return success(false);
-	if (state->status == BUSY) return success(false);  /* operation in progress */
-	if (!state->writable) return success(false);
+  if (!asyncFileValid(f)) return success(false);
+  if (state->status == BUSY) return success(false);  /* operation in progress */
+  if (!state->writable) return success(false);
 
-	/* allocate a new buffer if necessary */
-	asyncFileAllocateBuffer(state, bufferSize);
-	if (state->bufferPtr == NULL) return success(false);  /* could not allocate buffer */
+  /* allocate a new buffer if necessary */
+  asyncFileAllocateBuffer(state, bufferSize);
+  if (state->bufferPtr == NULL) return success(false);  /* could not allocate buffer */
 
-	/* copy the squeak buffer into the file buffer */
-	MoveMemory((void*)state->bufferPtr, (void*) bufferPtr, bufferSize);
+  /* copy the squeak buffer into the file buffer */
+  MoveMemory((void*)state->bufferPtr, (void*) bufferPtr, bufferSize);
 
-	state->dwPosition = fPosition;
-	state->dwSize = bufferSize;
-	state->status = BUSY;
-	state->rFlag = FALSE;
-	SetEvent(state->hEvent);
-	return 1;
+  state->dwPosition = fPosition;
+  state->dwSize = bufferSize;
+  state->status = BUSY;
+  state->rFlag = FALSE;
+  SetEvent(state->hEvent);
+  return 1;
 }
 
 
 int asyncFileInit(void)
 {
-	return 1;
+  return 1;
 }
 
 int asyncFileShutdown(void)
 {
-	return 1;
+  return 1;
 }
 
 #endif /* NO_ASYNC_FILES */
