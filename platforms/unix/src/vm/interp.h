e6e6285e9aa264298cc018c722ce49f68e2b3eef
Ian Piumarta <piumarta@gmail.com>
2008-06-25 01:07:49 +0000

    fix missing newline at end of interp.h
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@1897 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: 03037eb77df657206a2b7ec191c31cdd8dfb47ab

--- platforms/unix/src/vm/interp.h
+++ platforms/unix/src/vm/interp.h
@@ -1,1 +1,5 @@
-/* Automatically generated from Squeak on an Array(9 May 2008 11:23:58 am)by VMMaker 3.8b6 */#define SQ_VI_BYTES_PER_WORD 4
+/* Automatically generated from Squeak on an Array(9 May 2008 11:23:58 am)
+by VMMaker 3.8b6
+ */
+
+#define SQ_VI_BYTES_PER_WORD 4
