f0297be485737a4b433c4a2ef669666c83a70c06
Bernd Eckardt <bernd@impara.de>
2008-09-05 08:43:17 +0000

    Win32BrowserPlugin IE: - added changes for Vista done by Korakurider - additional registry key "UserImage" can be used for absolute image path setting, if necessary
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@1917 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: 41a4ba78bf67d19a46305a38dc97b8c8e4f5f45c

--- platforms/win32/vm/Plugin/sqWin32Plugin.c
+++ platforms/win32/vm/Plugin/sqWin32Plugin.c
@@ -29,9 +29,40 @@
 *		4) I HATE browsers. Did I mention this already?!
 *
 *****************************************************************************/
+/*
+ * Retrofit for Windows Vista (IE7/Protected mode)
+ *
+ * AUTHOR: Korakurider (kr)
+ *
+ * CHANGE NOTES:
+ *      1) If this is activated in protected mode, VM is invoked with "-lowRights" option, 
+ *         and as a low integrity process.
+ *         The updated VM capable of the option is needed.
+ *         Writable place for ActiveX in protected mode is only under %USERPROFILE%\AppData\LocalLow.
+ *         That VM is needed to switch "untrustedUserDirectory" if that option is specified.
+ *      
+ *      2) If there isn't private registry setting for current user (HKCU\Software\Squeak),
+ *         try shared setting (HKLM\Software\Squeak).
+ *
+ *      3) registry setting "Image" is now treated as full path name for the image.
+ */
+ /* 
+ *  AUTHOR: Bernd Eckardt (be)    
+ *  CHANGES:
+ *    1)  registry setting "Image" reverted to former usage (relative) 
+ *    2)  registry setting "UserImage" added to handel a absolute path 
+ *        if this is defined in registry this is used,  otherwise fallback to "Inage"  
+ *
+ */
+ 
+ 
+ 
+#define VISTA_SECURITY
+
 #include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sddl.h> /* for Vista */
 
 #include "sqWin32Plugin.h"
 
@@ -41,6 +72,8 @@ const char* gInstanceLookupString = "hWnd->squeak";
 #ifndef HKEY_SQUEAK_ROOT
 #define HKEY_SQUEAK_ROOT "SOFTWARE\\Squeak"
 #endif
+
+
 
 /* are we running IE?! */
 int ieMode = 0;
@@ -56,6 +89,18 @@ DWORD g_WM_BROWSER_PIPE = 0;
 DWORD g_WM_CLIENT_PIPE = 0;
 
 HANDLE hCurrentProcess = NULL;
+
+#ifdef VISTA_SECURITY
+/* For Integrity Level on Vista */
+#define SID_INTEGRITYLEVEL_LOW     "S-1-16-4096"
+#define SID_INTEGRITYLEVEL_MEDIUM  "S-1-16-8192"
+#define SID_INTEGRITYLEVEL_HIGH    "S-1-16-12288"
+
+#define INTEGRITYLEVEL_UNKNOWN  -1
+#define INTEGRITYLEVEL_LOW   0
+#define INTEGRITYLEVEL_MEDIUM  1
+#define INTEGRITYLEVEL_HIGHT  2
+#endif /* VISTA_SECURITY */
 
 #define FREE(ptr) if(ptr) { free(ptr); ptr = NULL; }
 #define STRDUP(ptr) ((ptr) ? strdup(ptr) : NULL)
@@ -91,6 +136,160 @@ void DPRINT(char *format, ...)
 
 #endif
 
+#ifdef VISTA_SECURITY
+/* for DEBUG */
+void ShowLastError(LPTSTR lpszFunction) 
+{ 
+    LPVOID lpMsgBuf;
+    LPVOID lpDisplayBuf;
+
+    DWORD dw = GetLastError(); 
+
+    FormatMessage(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | 
+        FORMAT_MESSAGE_FROM_SYSTEM |
+        FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL,
+        dw,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (LPTSTR) &lpMsgBuf,
+        0, NULL );
+
+    lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT, 
+        (lstrlen((LPCTSTR)lpMsgBuf)+lstrlen((LPCTSTR)lpszFunction)+40)*sizeof(TCHAR)); 
+    wsprintf((LPTSTR)lpDisplayBuf, 
+        TEXT("%s failed with error %d: %s"), 
+        lpszFunction, dw, lpMsgBuf); 
+    MessageBox(NULL, (LPCTSTR)lpDisplayBuf, TEXT("Error"), MB_OK); 
+
+    LocalFree(lpMsgBuf);
+    LocalFree(lpDisplayBuf);
+}
+/********************************************************************/
+/* Vista security stuff                                             */
+/********************************************************************/
+static int GetProcessIntegrityLevel(HANDLE hProcess)
+{
+  int integrityLevel = INTEGRITYLEVEL_UNKNOWN;
+  HANDLE hToken;
+  BOOL bRet;
+
+  DWORD dwLengthNeeded;
+  DWORD dwError = ERROR_SUCCESS;
+
+  PTOKEN_MANDATORY_LABEL pTIL = NULL;
+  DWORD dwIntegrityLevel;
+ 
+  bRet = OpenProcessToken(hProcess, TOKEN_QUERY | TOKEN_QUERY_SOURCE, &hToken);
+  if(!bRet) {
+	  return INTEGRITYLEVEL_UNKNOWN;
+  }
+
+  // Get the Integrity level.
+  bRet = GetTokenInformation(hToken, TokenIntegrityLevel, 
+						NULL, 0, &dwLengthNeeded);
+  if(!bRet) {
+      dwError = GetLastError();
+      if (dwError == ERROR_INSUFFICIENT_BUFFER) {
+          pTIL = (PTOKEN_MANDATORY_LABEL)LocalAlloc(0, dwLengthNeeded);
+          if (pTIL == NULL) {
+		      CloseHandle(hToken);
+		      return INTEGRITYLEVEL_UNKNOWN;
+		  }
+
+		  bRet = GetTokenInformation(hToken, TokenIntegrityLevel, 
+								pTIL, dwLengthNeeded, &dwLengthNeeded);
+		  if(!bRet) {
+		      LocalFree(pTIL);
+			  CloseHandle(hToken);
+		  }
+
+		  dwIntegrityLevel = *GetSidSubAuthority(pTIL->Label.Sid, 
+								(DWORD)(UCHAR)(*GetSidSubAuthorityCount(pTIL->Label.Sid)-1));
+		  if (dwIntegrityLevel < SECURITY_MANDATORY_MEDIUM_RID) {
+				// Low Integrity
+					integrityLevel = INTEGRITYLEVEL_LOW;
+		  } else if (dwIntegrityLevel >= SECURITY_MANDATORY_MEDIUM_RID && 
+									dwIntegrityLevel < SECURITY_MANDATORY_HIGH_RID) {
+				// Medium Integrity
+					integrityLevel = INTEGRITYLEVEL_MEDIUM;
+		  } else if (dwIntegrityLevel >= SECURITY_MANDATORY_HIGH_RID) {
+				// High Integrity
+					integrityLevel = INTEGRITYLEVEL_HIGHT;
+		  }
+		  LocalFree(pTIL);
+	  }
+   }
+   CloseHandle(hToken);
+   return integrityLevel;
+}
+
+/* Get Well-Known SID for an Integrity Level */
+static LPCTSTR GetIntegritySid(int IntegrityLevel) 
+{
+  LPCTSTR szSid = NULL;
+
+  switch(IntegrityLevel) {
+  case INTEGRITYLEVEL_LOW:
+	  szSid = SID_INTEGRITYLEVEL_LOW;
+      break;
+  case INTEGRITYLEVEL_MEDIUM:
+	  szSid = SID_INTEGRITYLEVEL_MEDIUM;
+	  break;
+  case INTEGRITYLEVEL_HIGHT:
+	  szSid = SID_INTEGRITYLEVEL_HIGH;
+	  break;
+  }
+  return szSid;
+}
+
+/* Get Security Token For Specified Integrity Level */
+static HANDLE GetTokenForIntegrityLevel(HANDLE hProcess, int IntegrityLevel)
+{
+  BOOL bRet;
+  HANDLE hToken;
+  HANDLE hNewToken = NULL;
+
+  PSID pIntegritySid = NULL;
+  LPCTSTR szIntegritySid;
+  TOKEN_MANDATORY_LABEL TIL = {0};
+  
+  szIntegritySid = GetIntegritySid(IntegrityLevel);
+  if(szIntegritySid == NULL) {
+	  return NULL;
+  }
+
+  bRet = OpenProcessToken(hProcess, MAXIMUM_ALLOWED, &hToken);
+  if(!bRet) {
+	  return NULL;
+  }
+
+  bRet = DuplicateTokenEx(hToken, MAXIMUM_ALLOWED, NULL, SecurityImpersonation, TokenPrimary, &hNewToken);
+  if(!bRet) {
+	  CloseHandle(hToken);
+	  return NULL;
+  }
+
+  bRet = ConvertStringSidToSid(szIntegritySid, &pIntegritySid);
+  if(!bRet) {
+	  CloseHandle(hNewToken);
+	  CloseHandle(hToken);
+	  return NULL;
+  }
+  TIL.Label.Attributes = SE_GROUP_INTEGRITY;
+  TIL.Label.Sid = pIntegritySid;
+ 
+  bRet = SetTokenInformation(hNewToken, TokenIntegrityLevel, &TIL,
+			  sizeof(TOKEN_MANDATORY_LABEL)+ GetLengthSid(pIntegritySid));
+  if(!bRet) {
+	  CloseHandle(hNewToken);
+	  CloseHandle(hToken);
+	  return NULL;
+  }
+  CloseHandle(hToken);
+  return(hNewToken);
+}
+#endif /* VISTA_SECURITY */
 
 /********************************************************************/
 /* Initialization stuff                                             */
@@ -141,6 +340,7 @@ LRESULT CALLBACK PluginWindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lP
     if(!DuplicateHandle(hProcess, hPipe, GetCurrentProcess(), 
 			&squeak->hClientPipe, 0, FALSE, 
 			DUPLICATE_SAME_ACCESS)) {
+	  ShowLastError("ClientPipe DuplicateError");
       MessageBox(squeak->hWnd, "Can not establish communication channel!", 
 		 "Squeak Plugin Error:", MB_OK | MB_ICONSTOP);
     }
@@ -250,29 +450,34 @@ void SubclassWindow(HWND hWnd, SqueakPlugin *squeak)
 int ReadRegistrySettings(HMODULE hPluginModule, SqueakPlugin *squeak) {
   DWORD dwType, dwSize, ok;
   TCHAR tmp[MAX_PATH+1];
+  TCHAR installDirectory[MAX_PATH+1];
   HKEY hk;
 
+  /* try private setting first */
   ok = RegOpenKey(HKEY_CURRENT_USER, HKEY_SQUEAK_ROOT, &hk);
+  if(ok != ERROR_SUCCESS) {
+	  /* try shared setting next */
+      ok = RegOpenKey(HKEY_LOCAL_MACHINE, HKEY_SQUEAK_ROOT, &hk);
+  }
   /* Read the Squeak path from the subkey. */
   dwSize = MAX_PATH;
   ok = RegQueryValueEx(hk,"InstallDirectory",NULL, &dwType, 
-		       (LPBYTE) tmp, &dwSize);
+		       (LPBYTE) installDirectory, &dwSize);
   if(ok == ERROR_SUCCESS) {
-    if(tmp[dwSize-2] != '\\') {
-      tmp[dwSize-1] = '\\';
-      tmp[dwSize] = 0;
+    if(installDirectory[dwSize-2] != '\\') {
+      installDirectory[dwSize-1] = '\\';
+      installDirectory[dwSize] = 0;
     }
   } else {
     /* No base directory found; use default plugin location */
-    GetModuleFileName(hPluginModule, tmp, MAX_PATH);
+    GetModuleFileName(hPluginModule, installDirectory, MAX_PATH);
     /* MessageBox(0, squeak->vmName,"Module name", MB_OK); */
-    strrchr(tmp,'\\')[1] = 0;
+    strrchr(installDirectory,'\\')[1] = 0;
   }
 
-  strcpy(squeak->imageName, tmp);
-  strcpy(squeak->vmName, tmp);
-
   /* Read VM name from the subkey */
+  strcpy(squeak->vmName, installDirectory);
+
   dwSize = MAX_PATH;
   ok = RegQueryValueEx(hk, "VM", NULL, &dwType, (LPBYTE)tmp, &dwSize);
   if(ok == ERROR_SUCCESS) {
@@ -285,14 +490,27 @@ int ReadRegistrySettings(HMODULE hPluginModule, SqueakPlugin *squeak) {
 
   /* Read image name from the subkey */
   dwSize = MAX_PATH;
-  ok = RegQueryValueEx(hk, "Image", NULL, &dwType, (LPBYTE)tmp, &dwSize);
+
+  ok = RegQueryValueEx(hk, "UserImage", NULL, &dwType, (LPBYTE)tmp, &dwSize);
   if(ok == ERROR_SUCCESS) {
-    tmp[dwSize = 0];
-    strcat(squeak->imageName, tmp);
-  } else {
-    /* No image name found; use default image name */
-    strcat(squeak->imageName, "SqueakPlugin.image");
+		tmp[dwSize = 0];
+	  	strcpy(squeak->imageName, tmp);
   }
+  else{
+	  ok = RegQueryValueEx(hk, "Image", NULL, &dwType, (LPBYTE)tmp, &dwSize);
+	  if(ok == ERROR_SUCCESS) {
+		tmp[dwSize = 0];
+
+		strcpy(squeak->imageName, installDirectory);
+		strcat(squeak->imageName, tmp);
+	
+	  } else {
+		/* No image name found; use default image name */
+		strcpy(squeak->imageName, installDirectory);
+		strcat(squeak->imageName, "SqueakPlugin.image");
+	  }
+  }
+/*  MessageBox(0, squeak->imageName,"Image name", MB_OK); */
   RegCloseKey(hk);
   return 1;
 }
@@ -389,6 +607,9 @@ int SqueakPluginRun(SqueakPlugin *squeak, HWND hWnd, int terminateOld)
   char lbuf[50];
   char *runString;
   int ok, runSize;
+  int integrityLevel;
+  HANDLE hProcess;
+  HANDLE hToken;
 
   if(!squeak) {
 	  MessageBox(0, "No plugin instance", "Squeak", MB_OK);
@@ -434,15 +655,44 @@ int SqueakPluginRun(SqueakPlugin *squeak, HWND hWnd, int terminateOld)
     strcat(runString," -memory: ");
     strcat(runString, ltoa(squeak->memory, lbuf, 10) );
   }
+#ifdef VISTA_SECURITY /* for IE7/Vista security */
+  hProcess = GetCurrentProcess();
+  integrityLevel = GetProcessIntegrityLevel(hProcess);
+/* In Protected Mode, untrustedUserDirectory must be 'low rights' area */
+  if(integrityLevel == INTEGRITYLEVEL_LOW) {
+	  strcat(runString, " -lowRights");
+  }
+#endif /* VISTA_SECURITY */
 
   strcat(runString, " \"");
   strcat(runString, squeak->imageName);
   strcat(runString, "\" ");
   if(squeak->params) strcat(runString, squeak->params);
 
-  /* MessageBox(0,runString,"Starting Squeak",MB_OK); */
 
-  ok = CreateProcess(NULL, /* no image name */
+#ifdef VISTA_SECURITY /* for IE7/Vista security */
+/* Call VM as invoker's integirty */
+  hToken = GetTokenForIntegrityLevel(hProcess, integrityLevel);
+  if(hToken) {
+		ok = CreateProcessAsUser(
+				hToken, /* security token */
+				NULL, 
+				runString, /* full command line */
+                NULL, /* no process security attributes */
+                NULL, /* no thread security attributes */
+                FALSE, /* don't inherit handles */
+                0, /* no special flags */
+                NULL, /* no separate environment */
+                NULL, /* no startup dir */
+                &sInfo, /* startup info */
+                &squeak->processInfo); /* process info */
+		/* CloseHandle(hToken); */
+	} else {
+		ok = 0;
+	}
+	CloseHandle(hProcess);
+#else
+    ok = CreateProcess(NULL, /* no image name */
                 runString, /* but full command line */
                 NULL, /* no process security attributes */
                 NULL, /* no thread security attributes */
@@ -452,6 +702,7 @@ int SqueakPluginRun(SqueakPlugin *squeak, HWND hWnd, int terminateOld)
                 NULL, /* no startup dir */
                 &sInfo, /* startup info */
                 &squeak->processInfo); /* process info */
+#endif
   if(ok == 0) { /* Error! */
     DWORD lastError = GetLastError();
     LPVOID lpMsgBuf;
@@ -480,14 +731,17 @@ int SqueakPluginRun(SqueakPlugin *squeak, HWND hWnd, int terminateOld)
 		      &squeak->hParentWriteEnd,
 		      0,
 		      FALSE,
-		      DUPLICATE_SAME_ACCESS | DUPLICATE_CLOSE_SOURCE))
+			  DUPLICATE_SAME_ACCESS | DUPLICATE_CLOSE_SOURCE)) {
+	ShowLastError("Communication Channel-a");
     MessageBox(hWnd, "Unable to establish communication channel", 
 	       "Squeak Plugin error", MB_OK | MB_ICONSTOP);
+  }
   if(PostThreadMessage(
 		squeak->processInfo.dwThreadId, 
 		g_WM_BROWSER_PIPE, 
 		(WPARAM) hCurrentProcess, 
 		(LPARAM) squeak->hParentWriteEnd) == 0) {
+	    ShowLastError("Communication Channel-b");
 		MessageBox(hWnd, "Failed to establish communication channel", 
 	       "Squeak Plugin error", MB_OK | MB_ICONSTOP);
   }
@@ -733,4 +987,6 @@ void SqueakPluginStreamState(SqueakPlugin *squeak, char *url, int ok)
     }
   }
 }
+
+
 
