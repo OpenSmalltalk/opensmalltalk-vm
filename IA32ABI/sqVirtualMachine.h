b1bbe6c7f99cae592ba5bed038f9e9f177889529
John McIntosh <johnmci@smalltalkconsulting.com>
2009-04-09 21:58:37 +0000

    Mac Carbon VM 4.0.1b1U
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@1983 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: b5f6d5b6501e55e2c5514b6ce65551e7b1b00e05

--- IA32ABI/sqVirtualMachine.h
+++ IA32ABI/sqVirtualMachine.h
@@ -232,6 +232,37 @@ typedef struct VirtualMachine {
 	sqInt  (*sendInvokeCallbackStackRegistersJmpbuf)(sqInt thunkPtrAsInt, sqInt stackPtrAsInt, sqInt regsPtrAsInt, sqInt jmpBufPtrAsInt);
 	sqInt  (*reestablishContextPriorToCallback)(sqInt callbackContext);
 	sqInt  (*classUnsafeAlien)(void);
+	/* New methods for proxy version 1.8 */
+	
+	/* callbackEnter: Re-enter the interpreter loop for a callback.
+	Arguments:
+	callbackID: Pointer to a location receiving the callback ID
+	used in callbackLeave
+	Returns: True if successful, false otherwise */
+	sqInt (*callbackEnter)(sqInt *callbackID);
+	
+	/* callbackLeave: Leave the interpreter from a previous callback
+	Arguments:
+	callbackID: The ID of the callback received from callbackEnter()
+	Returns: True if succcessful, false otherwise. */
+	sqInt (*callbackLeave)(sqInt  callbackID);
+	
+	/* addGCRoot: Add a variable location to the garbage collector.
+	The contents of the variable location will be updated accordingly.
+	Arguments:
+	varLoc: Pointer to the variable location
+	Returns: True if successful, false otherwise. */
+	sqInt (*addGCRoot)(sqInt *varLoc);
+	
+	/* removeGCRoot: Remove a variable location from the garbage collector.
+	Arguments:
+	varLoc: Pointer to the variable location
+	Returns: True if successful, false otherwise.
+	*/
+	sqInt (*removeGCRoot)(sqInt *varLoc);
+#endif
+
+#if VM_PROXY_MINOR > 8
 #endif
 
 } VirtualMachine;
