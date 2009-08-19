6cc3977ff612bead8184edb235df550407f9e869
Ian Piumarta <piumarta@gmail.com>
2009-08-19 11:53:51 +0000

    add cross/plugins/SurfacePlugin to include search path for RomePlugin
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@2073 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: 03eda0bcbe58e539a4a02262915faff14c142a45

--- platforms/unix/plugins/RomePlugin/build.cmake
+++ platforms/unix/plugins/RomePlugin/build.cmake
@@ -1,3 +1,3 @@
-INCLUDE_DIRECTORIES (${@plugin@_include_dirs})
+INCLUDE_DIRECTORIES (${cross}/plugins/SurfacePlugin ${@plugin@_include_dirs})
 LINK_DIRECTORIES (${@plugin@_library_dirs})
 TARGET_LINK_LIBRARIES (@plugin@ ${@plugin@_libraries})
