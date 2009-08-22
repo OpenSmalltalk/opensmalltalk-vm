dcf9cc50e23a77e403618e796b793a64cbcd35f7
Ian Piumarta <piumarta@gmail.com>
2009-08-22 21:15:29 +0000

    fix serial plugin API to use pointer types where appropriate
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@2078 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: 81e46e1a02ed745e2ce3a86cfe74a32c7edcded2

--- SerialPlugin/SerialPlugin.h
+++ SerialPlugin/SerialPlugin.h
@@ -7,9 +7,10 @@ int serialPortShutdown(void);
 #pragma export on
 int serialPortClose(int portNum);
 int serialPortCount(void);
-int serialPortOpen(
-  int portNum, int baudRate, int stopBitsType, int parityType, int dataBits,
-  int inFlowCtrl, int outFlowCtrl, int xOnChar, int xOffChar);
-int serialPortReadInto(int portNum, int count, int bufferPtr);
-int serialPortWriteFrom(int portNum, int count, int bufferPtr);
+int serialPortOpen(int portNum, int baudRate, int stopBitsType, int parityType, int dataBits,
+		   int inFlowCtrl, int outFlowCtrl, int xOnChar, int xOffChar);
+int serialPortReadInto(int portNum, int count, void *bufferPtr);
+int serialPortReadIntoByName(const char *portName, int count, void *bufferPtr);
+int serialPortWriteFrom(int portNum, int count, void *bufferPtr);
+int serialPortWriteFromByName(const char *portName, int count, void *bufferPtr);
 #pragma export off
