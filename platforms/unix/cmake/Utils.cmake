3d5c6dc10dd6bd06bcc0eaf66de753dee24a3af5
Ian Piumarta <piumarta@gmail.com>
2009-08-26 19:01:08 +0000

    add PLUGIN_FIND and PLUGIN_REQUIRE macros
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@2092 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: 77d025b6aa5ed3de5b5c859583211e4744697812

--- platforms/unix/cmake/Utils.cmake
+++ platforms/unix/cmake/Utils.cmake
@@ -1,25 +1,30 @@
 # Some random useful things to avoid repetition
 # 
-# Last edited: 2009-08-13 20:28:39 by piumarta on emilia-2.local
+# Last edited: 2009-08-26 10:41:32 by piumarta on ubuntu.piumarta.com
 
 MACRO (STRING_APPEND var str)
-    IF (DEFINED ${var})
-        SET (${var} "${${var}} ${str}")
-    ELSE (DEFINED ${var})
-        SET (${var} "${str}")
-    ENDIF (DEFINED ${var})
+  IF (DEFINED ${var})
+    SET (${var} "${${var}} ${str}")
+  ELSE (DEFINED ${var})
+    SET (${var} "${str}")
+  ENDIF (DEFINED ${var})
 ENDMACRO (STRING_APPEND)
 
+MACRO (LIST_APPEND list)
+  LIST (APPEND ${list} "${ARGN}")
+  LIST (REMOVE_DUPLICATES ${list})
+ENDMACRO (LIST_APPEND)
+
 MACRO (FILE_APPEND to from)
-    IF (EXISTS ${from})
-    	FILE (READ ${from} tmp)
-    	FILE (APPEND ${to} "${tmp}")
-    ENDIF (EXISTS ${from})
+  IF (EXISTS ${from})
+    FILE (READ ${from} tmp)
+    FILE (APPEND ${to} "${tmp}")
+  ENDIF (EXISTS ${from})
 ENDMACRO (FILE_APPEND)
 
 MACRO (FILE_COPY to from)
-    IF (EXISTS ${from})
-    	FILE (READ ${from} tmp)
-    	FILE (WRITE ${to} "${tmp}")
-    ENDIF (EXISTS ${from})
+  IF (EXISTS ${from})
+    FILE (READ ${from} tmp)
+    FILE (WRITE ${to} "${tmp}")
+  ENDIF (EXISTS ${from})
 ENDMACRO (FILE_COPY)
