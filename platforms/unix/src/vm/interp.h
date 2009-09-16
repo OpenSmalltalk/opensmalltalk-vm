3ae41757fd34d08d4edd5daf03ea3e09452c6f39
Ian Piumarta <piumarta@gmail.com>
2009-09-16 21:22:43 +0000

    update interp.h from vmm-138 to get VMMAKER_VERSION
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@2134 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: 853b754dd2aebd95adf8f7f12086bbb5eecd250c

--- platforms/unix/src/vm/interp.h
+++ platforms/unix/src/vm/interp.h
@@ -1,6 +1,8 @@
-/* Automatically generated from Squeak on an Array(26 August 2009 9:59:45 pm)
+/* Automatically generated from Squeak on an Array(16 September 2009 2:17:05 pm)
 by VMMaker 3.11.3
  */
+
+#define VMMAKER_VERSION "3.11.3"
 
 #define SQ_VI_BYTES_PER_WORD 4
 
