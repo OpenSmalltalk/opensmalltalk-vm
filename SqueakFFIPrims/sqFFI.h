086b9067388d7bcad987f892a00acb1f01881041
Andreas Raab <andreas.raab@gmx.de>
2010-02-11 08:23:28 +0000

    Add ExternalForm patches from Josh.
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@2149 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: 0d447bb91d7c31a23eb1052bc4a97332dd43bfa6

--- SqueakFFIPrims/sqFFI.h
+++ SqueakFFIPrims/sqFFI.h
@@ -6,7 +6,7 @@
 *   AUTHOR:  Andreas Raab (ar)
 *   ADDRESS: Walt Disney Imagineering, Glendale, CA
 *   EMAIL:   andreasr@wdi.disney.com
-*   RCSID:   $Id: sqFFI.h,v 1.1 2001/10/24 23:12:24 rowledge Exp $
+*   RCSID:   $Id$
 *
 *   NOTES:
 *
@@ -128,5 +128,23 @@ int ffiStoreStructure(int address, int structSize);
 /* return the float value from a previous call */
 double ffiReturnFloatValue(void);
 
+/* The following are for creating, manipulating, and detroying "manual surfaces".
+   These are surfaces that are managed by Squeak code, which has manual control
+   over the memory location where the image data is stored (the pointer used may 
+   be obtained via FFI calls, or other means).
+   
+   Upon creation, no memory is allocated for the surface.  Squeak code is 
+   responsible for passing in a pointer to the memory to use.  It is OK to set 
+   the pointer to different values, or to NULL.  If the pointer is NULL, then
+   BitBlt calls to ioLockSurface() will fail.
+   
+   createManualFunction() returns a non-negative surface ID if successful, and
+   -1 otherwise.  The other return true for success, and false for failure.
+*/   
+#include "../SurfacePlugin/SurfacePlugin.h"
+void initManualSurfaceFunctionPointers(fn_ioRegisterSurface reg, fn_ioUnregisterSurface unreg, fn_ioFindSurface find);
+int createManualSurface(int width, int height, int rowPitch, int depth, int isMSB);
+int destroyManualSurface(int surfaceID);
+int setManualSurfacePointer(int surfaceID, void* ptr);
 
 #endif /* SQ_FFI_H */
