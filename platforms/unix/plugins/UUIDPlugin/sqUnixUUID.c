3a4c14249f638ecbac68336c4b60c29543f75569
Eliot Miranda <eliot.miranda@gmail.com>
2010-07-23 01:09:10 +0000

    Improved build instructions.  Merged fix for NetBSD to unix UUID plugin.
    git-svn-id: http://squeakvm.org/svn/squeak/branches/Cog@2248 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: feb4efe126d81b2c8598cbef5663c5dd5598aec3

--- platforms/unix/plugins/UUIDPlugin/sqUnixUUID.c
+++ platforms/unix/plugins/UUIDPlugin/sqUnixUUID.c
@@ -1,20 +1,18 @@
 #include <uuid/uuid.h>
 #include "sq.h"
 
-int sqUUIDInit(void)
-{
-  return 1;
-}
+int sqUUIDInit(void) { return 1; }
 
-int sqUUIDShutdown(void)
-{
-  return 1;
-}
+int sqUUIDShutdown(void) { return 1; }
 
 int MakeUUID(char *location)
 {
   uuid_t uuid;
+#if defined(__NetBSD__)
+  uuidgen(&uuid, 1);
+#else
   uuid_generate(uuid);
+#endif
   memcpy((void *)location, (void *)&uuid, sizeof(uuid));
   return 1;
 }
