9a838bc98a0443f3c192d840a23759cf5b706b6c
John McIntosh <johnmci@smalltalkconsulting.com>
2010-10-13 18:37:38 +0000

    iOS take Cog soundplugin.h back to thrunk implementation
    git-svn-id: http://squeakvm.org/svn/squeak/branches/Cog@2328 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: 2413d8bf1e32d5a1c716e447f94694e0dcfb6e7a

--- platforms/Cross/plugins/SoundPlugin/SoundPlugin.h
+++ platforms/Cross/plugins/SoundPlugin/SoundPlugin.h
@@ -1,39 +1,28 @@
 /* SoundPlugin.h - header for Sound Plugins :-) tim@sumeru.stanford.edu */
 
 /* module initialization/shutdown */
-int soundInit(void);
-int soundShutdown(void);
+sqInt soundInit(void);
+sqInt soundShutdown(void);
 
 /* sound output */
-int snd_AvailableSpace(void);
-int snd_InsertSamplesFromLeadTime(int frameCount, int srcBufPtr, int samplesOfLeadTime);
-int snd_PlaySamplesFromAtLength(int frameCount, int arrayIndex, int startIndex);
-int snd_PlaySilence(void);
-int snd_Start(int frameCount, int samplesPerSec, int stereo, int semaIndex);
-int snd_Stop(void);
+sqInt snd_AvailableSpace(void);
+sqInt snd_InsertSamplesFromLeadTime(sqInt frameCount, void *srcBufPtr, sqInt samplesOfLeadTime);
+sqInt snd_PlaySamplesFromAtLength(sqInt frameCount, void *srcBufPtr, sqInt startIndex);
+sqInt snd_PlaySilence(void);
+sqInt snd_Start(sqInt frameCount, sqInt samplesPerSec, sqInt stereo, sqInt semaIndex);
+sqInt snd_Stop(void);
 
 /* sound input */
-int snd_SetRecordLevel(int level);
-int snd_GetRecordLevel(void);	// howard.stearns@qwaq.com October 24 2008
-int snd_StartRecording(int desiredSamplesPerSec, int stereo, int semaIndex);
-int snd_StopRecording(void);
+void snd_SetRecordLevel(sqInt level);
+sqInt snd_StartRecording(sqInt desiredSamplesPerSec, sqInt stereo, sqInt semaIndex);
+sqInt snd_StopRecording(void);
 double snd_GetRecordingSampleRate(void);
-int snd_RecordSamplesIntoAtLength(int buf, int startSliceIndex, int bufferSizeInBytes);
-void snd_Volume(double *left, double *right); //johnmci@smalltalkconsulting.com Nov 6th 2000
-void snd_SetVolume(double left, double right);//johnmci@smalltalkconsulting.com Nov 6th 2000
-int snd_EnableAEC(int trueOrFalse); //josh.gargus@teleplace.com  March 23 2010
-int snd_SupportsAEC(); //josh.gargus@teleplace.com  March 24 2010
-
-int getNumberOfSoundPlayerDevices(void);	// howard.stearns@qwaq.com October 20 2008 ...
-int getNumberOfSoundRecorderDevices(void);
-char * getDefaultSoundPlayer(void);					
-char * getDefaultSoundRecorder(void);
-char * getSoundPlayerDeviceName(int i);
-char * getSoundRecorderDeviceName(int i);
-void setDefaultSoundPlayer(char *deviceName);	
-void setDefaultSoundRecorder(char *deviceName);
+sqInt snd_RecordSamplesIntoAtLength(void *buf, sqInt startSliceIndex, sqInt bufferSizeInBytes);
+void snd_Volume(double *left, double *right);  /* johnmci@smalltalkconsulting.com Nov 6th 2000 */
+void snd_SetVolume(double left, double right); /* johnmci@smalltalkconsulting.com Nov 6th 2000 */
 
 /* mixer interface */
-int snd_GetSwitch(int id, int captureFlag, int channel);
-int snd_SetSwitch(int id, int captureFlag, int parameter);
-int snd_SetDevice(int id, char *name);
+sqInt snd_GetSwitch(sqInt identifier, sqInt captureFlag, sqInt channel);
+sqInt snd_SetSwitch(sqInt identifier, sqInt captureFlag, sqInt parameter);
+sqInt snd_SetDevice(sqInt identifier, char *name);
+
