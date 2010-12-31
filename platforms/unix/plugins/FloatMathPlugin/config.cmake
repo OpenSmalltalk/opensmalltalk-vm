8d25081cedb51fb6d3f2ebf1c60547e58c608293
Ian Piumarta <piumarta@gmail.com>
2010-12-31 01:48:33 +0000

    set LE flag and disable GCC optimisations for FloatMathPlugin
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@2335 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: c711c033eb2829e29fcc87cc93e6c16d97f1bc40

--- platforms/unix/plugins/FloatMathPlugin/config.cmake
+++ platforms/unix/plugins/FloatMathPlugin/config.cmake
@@ -1,1 +1,14 @@
 PLUGIN_DEFINITIONS (-DNO_ISNAN=1)
+
+# fdlibm.h does not recognize x86_64, so set endianness here for all platforms.
+
+TEST_BIG_ENDIAN (IS_BIG_ENDIAN)
+IF (NOT IS_BIG_ENDIAN)
+  PLUGIN_DEFINITIONS (-D__LITTLE_ENDIAN=1)
+ENDIF ()
+
+# GCC optimizations break fdlibm so disable them for now.
+
+IF (CMAKE_COMPILER_IS_GNUCC)
+  SET (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O0 -mno-fused-madd")
+ENDIF ()
