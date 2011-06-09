0e95326a7bf610b7af843620ee30bf506b9640b8
Andreas Raab <andreas.raab@gmx.de>
2011-06-09 09:07:24 +0000

    Delete branches/Cog/platforms/win32/plugins and replace it with an svn:external to trunk/platforms/win32/plugins.
    git-svn-id: http://squeakvm.org/svn/squeak/branches/Cog@2417 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: b99e9ff7ad9065ed976277a533b9a7595c18d39f

--- platforms/win32/plugins/Mpeg3Plugin/sqOpenFile.c
+++
@@ -1,5 +0,0 @@
-#include "sqVirtualMachine.h"
-extern struct VirtualMachine * interpreterProxy;
-void sqFilenameFromStringOpen(char *buffer,long fileIndex, long fileLength, int flag) {
-    interpreterProxy->ioFilenamefromStringofLengthresolveAliases(buffer,fileIndex, fileLength, flag);
-}
