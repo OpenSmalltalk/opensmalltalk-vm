188b174f85608d56b3df258e9dde3da3035385ec
Andreas Raab <andreas.raab@gmx.de>
2011-06-09 06:51:20 +0000

    Merging support files from cogvm branch.
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@2396 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: 3c3088723ee8af88e5c253bc20f919af20b0fa7d

--- platforms/win32/plugins/CroquetPlugin/sqWin32CroquetPlugin.c
+++ platforms/win32/plugins/CroquetPlugin/sqWin32CroquetPlugin.c
@@ -3,13 +3,13 @@
 
 static int loaded = 0;
 static HMODULE hAdvApi32 = NULL;
-static BOOLEAN __stdcall (*RtlGenRandom)(PVOID, ULONG) = NULL;
+static BOOLEAN (__stdcall *RtlGenRandom)(PVOID, ULONG) = NULL;
 
 int ioGatherEntropy(char *bufPtr, int bufSize) {
   if(!loaded) {
     loaded = 1;
     hAdvApi32 = LoadLibrary("advapi32.dll");
-    (void*)RtlGenRandom = GetProcAddress(hAdvApi32, "SystemFunction036");
+    RtlGenRandom = (void*)GetProcAddress(hAdvApi32, "SystemFunction036");
   }
   if(!RtlGenRandom) return 0;
   return RtlGenRandom(bufPtr, bufSize);
