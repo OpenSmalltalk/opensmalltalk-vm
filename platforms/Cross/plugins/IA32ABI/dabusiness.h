673dedb898a1b2e65b9b64d6309ff20aba4e4664
Andreas Raab <andreas.raab@gmx.de>
2011-06-09 08:29:00 +0000

    Merging support files from cogvm branch.
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@2413 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: 273dd460e52a54854c2755e84560920742de591c

--- platforms/Cross/plugins/IA32ABI/dabusiness.h
+++ platforms/Cross/plugins/IA32ABI/dabusiness.h
@@ -1,16 +1,13 @@
 /*
- * Copyright 2008 Cadence Design Systems, Inc.
- * 
- * Licensed under the Apache License, Version 2.0 (the ''License''); you may not use this file except in compliance with the License.  You may obtain a copy of the License at  http://www.apache.org/licenses/LICENSE-2.0
- */
-/*
  *  dabusiness.h
  *
  *  Written by Eliot Miranda 11/07.
- *  Parts written by John M McIntosh 12/08
+ *	Updated 5/2011 to cope with Cog stack direction.
  *
  * Body of the various callIA32XXXReturn functions.
  * Call a foreign function according to IA32-ish ABI rules.
+ * N.B. In Cog Stack and Cogit VMs numArgs is negative to access args from
+ * the downward-growing stack.
  */
 	long i, size;
 	sqInt funcAlien, resultMaybeAlien;
@@ -19,23 +16,23 @@
 	char *argstart;
 #endif
 
-	/* For macintel we can ignore the typearray */ 
-
-	EnsureHaveVMThreadID();
-
+#if STACKVM /* Need to access args downwards from first arg */
+  if (numArgs < 0)
+	for (i = size = 0; --i > numArgs;) {
+		sqInt arg = argVector[i];
+		if (objIsAlien(arg) && sizeField(arg))
+			size += moduloPOT(sizeof(long),abs(sizeField(arg)));
+		else /* assume an integer or pointer.  check below. */
+			size += sizeof(long);
+	}
+  else
+#endif /* STACKVM */
 	for (i = numArgs, size = 0; --i >= 0;) {
 		sqInt arg = argVector[i];
 		if (objIsAlien(arg) && sizeField(arg))
 			size += moduloPOT(sizeof(long),abs(sizeField(arg)));
-		else /* assume an integer,double or pointer.  check below. */
-			if (interpreterProxy->isFloatObject(arg)) {
-				if (hasTypeArray) 
-					size += figureOutFloatSize(typeSignatureArray,i);
-				else
-					size += sizeof(double);
-			}
-			else
-				size += sizeof(long);
+		else /* assume an integer or pointer.  check below. */
+			size += sizeof(long);
 	}
 
 #if STACK_ALIGN_BYTES
@@ -54,10 +51,48 @@
 	 * %esp + 4, so the outgoing stack is offset by one word if uncorrected.
 	 * Grab the actual stack pointer to correct.
 	 */
-	getsp(argvec);
+	argvec = getsp();
 # endif
 #endif
 
+#if STACKVM /* Need to access args downwards from first arg */
+  if (numArgs < 0)
+	for (i = size = 0; --i > numArgs;) {
+		sqInt arg = argVector[i];
+		if (isSmallInt(arg)) {
+			*(long *)argvec = intVal(arg);
+			argvec += sizeof(long);
+		}
+		else if (objIsAlien(arg)) {
+			long  argByteSize;
+
+			if (!(size = sizeField(arg)))
+				size = argByteSize = sizeof(void *);
+			else
+				argByteSize = abs(size);
+			memcpy(argvec, startOfDataWithSize(arg,size), argByteSize);
+			argvec += moduloPOT(sizeof(long), argByteSize);
+		}
+		else if (objIsUnsafeAlien(arg)) {
+			sqInt bitsObj = interpreterProxy->fetchPointerofObject(0,arg);
+			void *v = interpreterProxy->firstIndexableField(bitsObj);
+			*(void **)argvec = v;
+			argvec += sizeof(long);
+		}
+		else {
+			long v = interpreterProxy->signed32BitValueOf(arg);
+			if (interpreterProxy->failed()) {
+				interpreterProxy->primitiveFailFor(0);
+				v = interpreterProxy->positive32BitValueOf(arg);
+				if (interpreterProxy->failed())
+					return PrimErrBadArgument;
+			}
+			*(long *)argvec = v;
+			argvec += sizeof(long);
+		}
+	}
+  else
+#endif /* STACKVM */
 	for (i = 0; i < numArgs; i++) {
 		sqInt arg = argVector[i];
 		if (isSmallInt(arg)) {
@@ -81,25 +116,15 @@
 			argvec += sizeof(long);
 		}
 		else {
-			if (interpreterProxy->isFloatObject(arg)) {
-				double v = interpreterProxy->floatValueOf(arg);
+			long v = interpreterProxy->signed32BitValueOf(arg);
+			if (interpreterProxy->failed()) {
+				interpreterProxy->primitiveFailFor(0);
+				v = interpreterProxy->positive32BitValueOf(arg);
 				if (interpreterProxy->failed())
 					return PrimErrBadArgument;
-				if (hasTypeArray && figureOutFloatSize(typeSignatureArray,i) == sizeof(float)) {
-					float floatv = v;
-					*(float *)argvec = floatv;
-					argvec += sizeof(float);			
-				} else {
-					*(double *)argvec = v;
-					argvec += sizeof(double);			
-				}
-			} else {
-				long v = interpreterProxy->signed32BitValueOf(arg);
-				if (interpreterProxy->failed())
-					return PrimErrBadArgument;
-				*(long *)argvec = v;
-				argvec += sizeof(long);
 			}
+			*(long *)argvec = v;
+			argvec += sizeof(long);
 		}
 	}
 
@@ -110,3 +135,14 @@
 	setsp(argstart);
 #endif
 	r = f();
+	/* post call need to refresh stack pointer in case of call-back and GC. */
+	resultMaybeAlien = interpreterProxy->stackValue(resultOffset);
+	if (objIsAlien(resultMaybeAlien)) {
+		if (!(size = sizeField(resultMaybeAlien)))
+			size = sizeof(void *);
+		memcpy(startOfDataWithSize(resultMaybeAlien,size),
+				&r,
+				min((unsigned)abs(size), sizeof(r)));
+	}
+
+	return PrimNoErr;
--- platforms/Cross/plugins/IA32ABI/ia32abi.h
+++ platforms/Cross/plugins/IA32ABI/ia32abi.h
@@ -1,14 +1,10 @@
 /*
- * Copyright 2008 Cadence Design Systems, Inc.
- * 
- * Licensed under the Apache License, Version 2.0 (the ''License''); you may not use this file except in compliance with the License.  You may obtain a copy of the License at  http://www.apache.org/licenses/LICENSE-2.0
- */
-/*
  *  platforms/Cross/plugins/IA32ABI/ia32abi.h
  *
- *  Written by Eliot Miranda 11/07.
+ *  Written by Eliot Miranda 11/2007.
+ *	Updated 5/2011 to cope with Cog stack direction.
  *
- * Call foreign functons returning results in either %eax, %edx (Integral)
+ * Call foreign functions returning results in either %eax, %edx (Integral)
  * or %f0 (Float, Double).  
  *
  * The primitive will have signatures of the form
@@ -24,40 +20,22 @@
  *	functionAddress <Alien> primFFICallResult: result <Alien>
  *	  with: firstArg <Alien> ... with: lastArg <Alien>
  *		<primitive: 'primCallOutXXX' module: 'IA32ABI'>
+ *
+ * N.B. In Cog Stack and Cogit VMs numArgs is negative to access args from
+ * the downward-growing stack.
  */
 
 #define SIGNATURE	sqInt *argVector/* call args on stack or in array */, \
-					int numArgs,	/* arg count of function to call   */ \
+					int numArgs,	/* arg count of function to call (*) */ \
 					int funcOffset, /* stack offset of func Alien   */ \
-					int resultOffset,/* stack offset of result Alien */ \
-					int hasTypeArray, /* last parm value is type array of sizes to support floating point arguments for powerpc */ \
-					int typeSignatureArray /* type signature array or nil object, or nil */
+					int resultOffset/* stack offset of result Alien */
 
 extern sqInt callIA32IntegralReturn(SIGNATURE);
 extern sqInt callIA32FloatReturn   (SIGNATURE);
 extern sqInt callIA32DoubleReturn  (SIGNATURE);
 extern long  thunkEntry            (void *thunkp, long *stackp);
 extern void *allocateExecutablePage(long *pagesize);
-
-/*
- * Returning values from callbacks is done through a CallBackReturnSpec
- * which contains a type tag and values.  It is designed to be overlaid upon
- * an FFICallbackReturnProxy created at the Smalltalk level to return values.
- */
-typedef  struct {
-	long type;
-# define retint32  0
-# define retint64  1
-# define retdouble 2
-# define retstruct 3
-	long _pad; /* so no doubt that valflt64 & valint32 et al are at byte 8 */
-union {
-		long valint32;
-		struct { long low, high; } valint64;
-		double valflt64;
-		struct { void *addr; long size; } valstruct;
-	} rvs;
-} CallBackReturnSpec;
+extern VMCallbackContext *getMostRecentCallbackContext(void);
 
 /* Use the most minimal setjmp/longjmp pair available; no signal handling
  * wanted or necessary.
