ee7c17c325f82425748de64cba8abdb7921bc951
Andreas Raab <andreas.raab@gmx.de>
2011-06-09 08:29:00 +0000

    Merging support files from cogvm branch.
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@2413 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: 1a0248cfeb50630f6568a338a6377470ffe21e29

--- IA32ABI/ia32abicc.c
+++ IA32ABI/ia32abicc.c
@@ -1,31 +1,12 @@
-#if defined(__powerpc__) || defined(PPC) || defined(_POWER) || defined(_IBMR2) || defined(__ppc__)
-#else
 /*
- * Copyright 2008 Cadence Design Systems, Inc.
- * 
- * Licensed under the Apache License, Version 2.0 (the ''License''); you may not use this file except in compliance with the License.  You may obtain a copy of the License at  http://www.apache.org/licenses/LICENSE-2.0
- */
-/* 
  *  ia32abicc.c
  *
  * Support for Call-outs and Call-backs from the Plugin.
  *  Written by Eliot Miranda 11/07.
- * Changes by John M McIntosh 12/08.
- *
  */
 
 #if defined(_MSC_VER) || defined(__MINGW32__)
 # include "windows.h" /* for GetSystemInfo & VirtualAlloc */
-extern int inIoProcessEvents;
-# define noteEnterCallback() (++inIoProcessEvents)
-# define noteExitCallback()  do { if (inIoProcessEvents) --inIoProcessEvents; } while (0)
-# if DEBUG
-DWORD vmThread = 0;
-#  define EnsureHaveVMThreadID() do { \
-						if (!vmThread) vmThread = GetCurrentThreadId(); \
-						} while (0)
-#  define CheckInVMThread() (vmThread == GetCurrentThreadId())
-# endif /* DEBUG */
 #elif __APPLE__ && __MACH__
 # include <sys/mman.h> /* for mprotect */
 # if OBJC_DEBUG /* define this to get debug info for struct objc_class et al */
@@ -37,35 +18,24 @@ struct objc_class *baz;
 void setbaz(void *p) { baz = p; }
 void *getbaz() { return baz; }
 # endif
+# include <stdlib.h> /* for valloc */
+# include <sys/mman.h> /* for mprotect */
 #else
-# include <malloc.h> /* for valloc */
+# include <stdlib.h> /* for valloc */
 # include <sys/mman.h> /* for mprotect */
 #endif
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+
+#include <string.h> /* for memcpy et al */
+#include <setjmp.h>
+#include <stdio.h> /* for fprintf(stderr,...) */
+
+#include "vmCallback.h"
 #include "sqMemoryAccess.h"
 #include "sqVirtualMachine.h"
 #include "ia32abi.h"
 
-#include <setjmp.h>
-
 #if !defined(min)
 # define min(a,b) ((a) < (b) ? (a) : (b))
-#endif
-
-#if !defined(noteEnterCallback)
-# define noteEnterCallback() 0
-#endif
-#if !defined(noteExitCallback)
-# define noteExitCallback() 0
-#endif
-#if !defined(EnsureHaveVMThreadID)
-# define EnsureHaveVMThreadID() 0
-#endif
-#if !defined(CheckInVMThread)
-# define CheckInVMThread() 1
 #endif
 
 #ifdef SQUEAK_BUILTIN_PLUGIN
@@ -87,8 +57,8 @@ struct VirtualMachine* interpreterProxy;
 # define alloca _alloca
 #endif
 #if __GNUC__
-# define setsp(sp) asm volatile ("movl %0,%%esp" : : "memory"(sp))
-# define getsp(sp) asm volatile ("movl %%esp,%0" : "=r"(sp) : )
+# define setsp(sp) asm volatile ("movl %0,%%esp" : : "m"(sp))
+# define getsp() ({ void *esp; asm volatile ("movl %%esp,%0" : "=r"(esp) : ); esp;})
 #endif
 #if __APPLE__ && __MACH__ && __i386__
 # define STACK_ALIGN_BYTES 16
@@ -98,7 +68,7 @@ struct VirtualMachine* interpreterProxy;
 # define setsp(ignored) 0
 #endif
 
-#define moduloPOT(m,v) ((v)+(m)-1 & ~((m)-1))
+#define moduloPOT(m,v) (((v)+(m)-1) & ~((m)-1))
 #define alignModuloPOT(m,v) ((void *)moduloPOT(m,(unsigned long)(v)))
 
 #define objIsAlien(anOop) (interpreterProxy->includesBehaviorThatOf(interpreterProxy->fetchClassOf(anOop), interpreterProxy->classAlien()))
@@ -124,17 +94,6 @@ struct VirtualMachine* interpreterProxy;
 #define isSmallInt(oop) ((oop)&1)
 #define intVal(oop) (((long)(oop))>>1)
 
-int figureOutFloatSize(int typeSignatureArray,int index) {
-	int floatSize,objectSize;
-	char *floatSizePointer;
-	sqInt oops = interpreterProxy->stackValue(typeSignatureArray);
-	objectSize = interpreterProxy->stSizeOf(oops);
-	if (index >= objectSize) 
-		return sizeof(double);
-	floatSizePointer = interpreterProxy->firstIndexableField(oops);
-	floatSize = floatSizePointer[index];
-	return floatSize;
-}
 /*
  * Call a foreign function that answers an integral result in %eax (and
  * possibly %edx) according to IA32-ish ABI rules.
@@ -147,7 +106,6 @@ __int64 (*f)(), r;
 long long (*f)(), r;
 #endif
 #include "dabusiness.h"
-#include "dabusinessPostLogic.h"
 }
 
 /*
@@ -157,7 +115,6 @@ long long (*f)(), r;
 sqInt
 callIA32FloatReturn(SIGNATURE) { float (*f)(), r;
 #include "dabusiness.h"
-#include "dabusinessPostLogic.h"
 }
 
 /*
@@ -167,8 +124,28 @@ callIA32FloatReturn(SIGNATURE) { float (*f)(), r;
 sqInt
 callIA32DoubleReturn(SIGNATURE) { double (*f)(), r;
 #include "dabusiness.h"
-#include "dabusinessPostLogic.h"
 }
+
+/* Queueing order for callback returns.  To ensure that callback returns occur
+ * in LIFO order we provide mostRecentCallbackContext which is tested by the return
+ * primitive primReturnFromContextThrough.  In a threaded VM this will have to
+ * be thread-specific (as yet unimplemented).
+ */
+#if COGMTVM
+# error as yet unimplemented
+/* Test if need and allocate a thread-local variable index in
+ * allocateExecutablePage (low frequency operation).  Keep a per-thread
+ * mostRecentCallbackContext.
+ */
+#else
+static VMCallbackContext *mostRecentCallbackContext = 0;
+
+VMCallbackContext *
+getMostRecentCallbackContext() { return mostRecentCallbackContext; }
+
+# define getRMCC(t) mostRecentCallbackContext
+# define setRMCC(t) (mostRecentCallbackContext = (void *)(t))
+#endif
 
 /*
  * Entry-point for call-back thunks.  Args are thunk address and stack pointer,
@@ -182,8 +159,8 @@ callIA32DoubleReturn(SIGNATURE) { double (*f)(), r;
  *		thunkp
  * esp->retpc (thunkEntry)
  *
- * Pushing the stack pointer twice is done to keep the stack alignment to 16
- * bytes, a requirement on Mac OS X, and harmless elsewhere.
+ * The stack pointer is pushed twice to keep the stack alignment to 16 bytes, a
+ * requirement on platforms using SSE2 such as Mac OS X, and harmless elsewhere.
  *
  * This function's roles are to use setjmp/longjmp to save the call point
  * and return to it, and to return any of the various values from the callback.
@@ -192,70 +169,70 @@ callIA32DoubleReturn(SIGNATURE) { double (*f)(), r;
  * arguments, the function would take 8 arguments, the 6 register args as
  * longs, followed by the thunkp and stackp passed on the stack.  The register
  * args would get copied into a struct on the stack. A pointer to the struct
- * is then passed as the 3rd argument of sendInvokeCallbackStackRegistersJmpbuf
+ * is then passed as an element of the VMCallbackContext.
  */
 long
 thunkEntry(void *thunkp, long *stackp)
 {
-	jmp_buf trampoline;
-	volatile CallBackReturnSpec *rs;
+	VMCallbackContext vmcc;
+	VMCallbackContext *previousCallbackContext;
+	int flags, returnType;
 
-	if (sizeof(int) != sizeof(rs)) {
-		perror("setjmp cannot return a pointer; reimplement!\n");
-		exit(1);
-	}
-	if (!CheckInVMThread()) {
-		printf("NOT IN VM THREAD!\n");
-		perror("Not in VM thread!\n");
-		exit(666);
+	if ((flags = interpreterProxy->ownVM(0)) < 0) {
+		fprintf(stderr,"Warning; callback failed to own the VM\n");
+		return -1;
 	}
 
-	noteEnterCallback();
-	if (!(rs = (void *)setjmp(trampoline))) {
-		interpreterProxy->
-			sendInvokeCallbackStackRegistersJmpbuf(	(sqInt)thunkp,
-													(sqInt)(stackp + 2),
-													0,
-													(sqInt)&trampoline);
-		perror("Warning; callback failed to invoke\n");
-		return 0;
+	if (!(returnType = setjmp(vmcc.trampoline))) {
+		previousCallbackContext = getRMCC();
+		setRMCC(&vmcc);
+		vmcc.thunkp = thunkp;
+		vmcc.stackp = stackp + 2; /* skip address of retpc & retpc (thunk) */
+		vmcc.intregargsp = 0;
+		vmcc.floatregargsp = 0;
+		interpreterProxy->sendInvokeCallbackContext(&vmcc);
+		fprintf(stderr,"Warning; callback failed to invoke\n");
+		setRMCC(previousCallbackContext);
+		interpreterProxy->disownVM(flags);
+		return -1;
 	}
-	noteExitCallback();
+	setRMCC(previousCallbackContext);
+	interpreterProxy->disownVM(flags);
 
-	switch (rs->type) {
+	switch (returnType) {
 
-	case retint32:	return rs->rvs.valint32;
+	case retword:	return vmcc.rvs.valword;
 
-	case retint64: {
-		long vhigh = rs->rvs.valint64.high;
+	case retword64: {
+		long vhigh = vmcc.rvs.valleint64.high;
 #if _MSC_VER
 				_asm mov edx, dword ptr vhigh;
 #elif __GNUC__
 				asm("mov %0,%%edx" : : "m"(vhigh));
 #else
-# error need to load edx with rs->rvs.valint64.high on this compiler
+# error need to load edx with vmcc.rvs.valleint64.high on this compiler
 #endif
-				return rs->rvs.valint64.low;
+				return vmcc.rvs.valleint64.low;
 	}
 
 	case retdouble: {
-		double valflt64 = rs->rvs.valflt64;
+		double valflt64 = vmcc.rvs.valflt64;
 #if _MSC_VER
 				_asm fld qword ptr valflt64;
 #elif __GNUC__
 				asm("fldl %0" : : "m"(valflt64));
 #else
-# error need to load %f0 with rs->rvs.valflt64 on this compiler
+# error need to load %f0 with vmcc.rvs.valflt64 on this compiler
 #endif
 				return 0;
 	}
 
 	case retstruct:	memcpy( (void *)(stackp[1]),
-						rs->rvs.valstruct.addr,
-						rs->rvs.valstruct.size);
-				return stackp[1];
+							vmcc.rvs.valstruct.addr,
+							vmcc.rvs.valstruct.size);
+					return stackp[1];
 	}
-	perror("Warning; invalid callback return type\n");
+	fprintf(stderr,"Warning; invalid callback return type\n");
 	return 0;
 }
 
@@ -267,7 +244,7 @@ thunkEntry(void *thunkp, long *stackp)
  * is some (possiby unitary) multiple of the pagesize.
  *
  * We assume the Smalltalk image code will manage subdividing the executable
- * page amongst thunks so there is no need to free these pages, sicne the image
+ * page amongst thunks so there is no need to free these pages, since the image
  * will recycle parts of the page for reclaimed thunks.
  */
 #if defined(_MSC_VER) || defined(__MINGW32__)
@@ -280,6 +257,9 @@ allocateExecutablePage(long *size)
 	void *mem;
 
 #if defined(_MSC_VER) || defined(__MINGW32__)
+#if !defined(MEM_TOP_DOWN)
+# define MEM_TOP_DOWN 0x100000
+#endif
 	if (!pagesize) {
 		SYSTEM_INFO	sysinf;
 
@@ -309,4 +289,3 @@ allocateExecutablePage(long *size)
 #endif
 	return mem;
 }
-#endif
