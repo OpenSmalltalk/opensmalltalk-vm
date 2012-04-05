e3b4940bdcac3e937cab38a90f746e7e7bf13a1e
Eliot Miranda <eliot.miranda@gmail.com>
2012-04-05 18:45:10 +0000

    Add distinct source code control version info for the plugins tree.
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@2544 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: 8dc8118c8df57d930ab3915f95e2ce5506aaf9cf

---
+++ sqPluginsSCCSVersion.h
@@ -0,0 +1,43 @@
+/*
+ * A set of definitions for C source code control systems, to provide accurate
+ * and definitive version information to the VM.  This file identifies the
+ * platform plugin code, which is shared between Cog and the trunk interpreter.
+ * It is included by platforms/Cross/vm/sqSCCSVersion.h.
+ *
+ * Currently instantiated only for Subversion.  Please add definitions for
+ * other repositories as appropriate.
+ */
+
+#if SUBVERSION
+static char SvnRawPluginsRevisionString[] = "$Rev$";
+# define PLUGINS_REV_START (SvnRawPluginsRevisionString + 6)
+
+static char SvnRawPluginsRepositoryURL[] = "$URL$";
+# define URL_START (SvnRawPluginsRepositoryURL + 6)
+
+static char *
+pluginsRevisionAsString()
+{
+	char *maybe_space = strchr(PLUGINS_REV_START,' ');
+	if (maybe_space)
+		*maybe_space = 0;
+	return PLUGINS_REV_START;
+}
+
+static char *
+pluginsRepositoryURL()
+{
+	char *maybe_platforms = strstr(URL_START, "/platforms");
+	if (maybe_platforms)
+		*maybe_platforms = 0;
+	return URL_START;
+}
+# undef PLUGINS_REV_START
+# undef URL_START
+#else /* SUBVERSION */
+static char *
+pluginsRevisionAsString() { return "?"; }
+
+static char *
+pluginsRepositoryURL() { return "unknown"; }
+#endif /* SUBVERSION */
