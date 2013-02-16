41288ad945dd9d84d45ebc64eb731bc56f40f1c7
Tim Rowledge <tim@rowledge.org>
2013-02-16 01:22:12 +0000

    header file name changed
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@2684 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: 0ec623590c1307329aca422b5abc41a660b566ee

--- platforms/RiscOS/plugins/ScratchPlugin/sqRPCScratchOps.c
+++ platforms/RiscOS/plugins/ScratchPlugin/sqRPCScratchOps.c
@@ -23,7 +23,7 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
-#include "scratchOps.h"
+#include "ScratchPlugin.h"
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
