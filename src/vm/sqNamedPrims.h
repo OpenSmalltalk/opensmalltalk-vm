5292bf0f2b3ef23dc875b5131b6808eadab4d9f1
David T. Lewis <lewis@mail.msen.com>
2014-02-23 15:51:51 +0000

    Sources per VMMaker-dtl.342 (VMMaker 4.13.3)
    Fix BalloonEnginePlugin>>stepToNextBezierForward:at: by removing unneeded
    cCoerce (this was an earlier workaround for a slang generation bug, no
    longer relevant).
    
    Make it possible to produce a working VM with inlining disabled.
    
    
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@2870 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: 39ef916db91c2bfda57aa6f458340cb51e5e4117

--- src/vm/sqNamedPrims.h
+++ src/vm/sqNamedPrims.h
@@ -1,4 +1,4 @@
-/* Smalltalk from Squeak4.5 with VMMaker 4.13.2 translated as C source on 12 February 2014 7:19:45 pm */
+/* Smalltalk from Squeak4.5 with VMMaker 4.13.3 translated as C source on 23 February 2014 10:39:13 am */
 /* This is an automatically generated table of all builtin modules in the VM */
 
 extern sqExport vm_exports[];
