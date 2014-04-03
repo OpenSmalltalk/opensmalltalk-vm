5e52c215167b2808422a43ee38c1323236a5108a
Eliot Miranda <eliot.miranda@gmail.com>
2014-04-03 02:28:25 +0000

    CogVM source as per VMMaker.oscog-eem.663
    Fix pc-mapping for NewspeakV4.  Dynamic super sends should
    /not/ be annotated with IsNSSendCall, but wth the vanilla IsSendCall.
    This fixes a bug converting an interpreter activation of a method
    with a loop and a duynamic super send to a machine code frame.
    
    Sista:
    Add sistasrc & sistamacbuild.  It's time ;-).
    Reload TempReg from ReceiverResultReg on return from
    counter trip, so that branch retry tests the right value.
    Add a primitive to reset branch counters in a method.
    
    
    git-svn-id: http://squeakvm.org/svn/squeak/branches/Cog@2887 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: 5029f9a00dc17780b654dfc5d254a816f7f67a48

---
+++ sistamacbuild/sqNamedPrims.h
@@ -0,0 +1,79 @@
+/* Automatically generated on Mon Sep 23 09:23:27 PDT 2013 */
+extern sqExport vm_exports[];
+extern sqExport os_exports[];
+extern sqExport ADPCMCodecPlugin_exports[];
+extern sqExport AsynchFilePlugin_exports[];
+extern sqExport B3DAcceleratorPlugin_exports[];
+extern sqExport B2DPlugin_exports[];
+extern sqExport BitBltPlugin_exports[];
+extern sqExport BMPReadWriterPlugin_exports[];
+extern sqExport ZipPlugin_exports[];
+extern sqExport DropPlugin_exports[];
+extern sqExport DSAPrims_exports[];
+extern sqExport FFTPlugin_exports[];
+extern sqExport FilePlugin_exports[];
+extern sqExport HostWindowPlugin_exports[];
+extern sqExport IA32ABI_exports[];
+extern sqExport InternetConfigPlugin_exports[];
+extern sqExport JoystickTabletPlugin_exports[];
+extern sqExport JPEGReaderPlugin_exports[];
+extern sqExport JPEGReadWriter2Plugin_exports[];
+extern sqExport Klatt_exports[];
+extern sqExport LargeIntegers_exports[];
+extern sqExport LocalePlugin_exports[];
+extern sqExport MacMenubarPlugin_exports[];
+extern sqExport Matrix2x3Plugin_exports[];
+extern sqExport MIDIPlugin_exports[];
+extern sqExport MiscPrimitivePlugin_exports[];
+extern sqExport RePlugin_exports[];
+extern sqExport SecurityPlugin_exports[];
+extern sqExport SerialPlugin_exports[];
+extern sqExport SocketPlugin_exports[];
+extern sqExport SoundCodecPrims_exports[];
+extern sqExport SoundGenerationPlugin_exports[];
+extern sqExport SoundPlugin_exports[];
+extern sqExport StarSqueakPlugin_exports[];
+extern sqExport SurfacePlugin_exports[];
+extern sqExport UUIDPlugin_exports[];
+extern sqExport VMProfileMacSupportPlugin_exports[];
+
+sqExport *pluginExports[] = {
+	vm_exports,
+	os_exports,
+	ADPCMCodecPlugin_exports,
+	AsynchFilePlugin_exports,
+	B3DAcceleratorPlugin_exports,
+	B2DPlugin_exports,
+	BitBltPlugin_exports,
+	BMPReadWriterPlugin_exports,
+	ZipPlugin_exports,
+	DropPlugin_exports,
+	DSAPrims_exports,
+	FFTPlugin_exports,
+	FilePlugin_exports,
+	HostWindowPlugin_exports,
+	IA32ABI_exports,
+	InternetConfigPlugin_exports,
+	JoystickTabletPlugin_exports,
+	JPEGReaderPlugin_exports,
+	JPEGReadWriter2Plugin_exports,
+	Klatt_exports,
+	LargeIntegers_exports,
+	LocalePlugin_exports,
+	MacMenubarPlugin_exports,
+	Matrix2x3Plugin_exports,
+	MIDIPlugin_exports,
+	MiscPrimitivePlugin_exports,
+	RePlugin_exports,
+	SecurityPlugin_exports,
+	SerialPlugin_exports,
+	SocketPlugin_exports,
+	SoundCodecPrims_exports,
+	SoundGenerationPlugin_exports,
+	SoundPlugin_exports,
+	StarSqueakPlugin_exports,
+	SurfacePlugin_exports,
+	UUIDPlugin_exports,
+	VMProfileMacSupportPlugin_exports,
+	NULL
+};
