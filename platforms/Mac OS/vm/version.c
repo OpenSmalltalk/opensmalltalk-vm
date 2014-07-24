15a4a94d4de61375074cbbe5a750ccc2747690e5
Eliot Miranda <eliot.miranda@gmail.com>
2014-07-24 17:11:23 +0000

    Add the time zone to the version info on Mac OS X and Win32. Nuke the win32 version.h wart and simplify & update win32's version.c.
    git-svn-id: http://squeakvm.org/svn/squeak/branches/Cog@3055 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: feeec5008addd4332f0cc0b187eada2948cd7dcb

--- platforms/Mac OS/vm/version.c
+++ platforms/Mac OS/vm/version.c
@@ -2,4 +2,14 @@
 #	define __VERSION__ "Unknown"
 #endif
 
-char vmBuildString[] = "Mac OS X built on "__DATE__" "__TIME__" Compiler: "__VERSION__;
+#if !defined(TZ)
+# define TZ ""
+# define SPACER ""
+#else
+# define SPACER " "
+#endif
+
+char vmBuildString[] = \
+	"Mac OS X built on " \
+	__DATE__" "__TIME__ SPACER TZ \
+	" Compiler: " __VERSION__;
--- platforms/win32/vm/version.c
+++ platforms/win32/vm/version.c
@@ -1,3 +1,35 @@
-#include "version.h"
+#if !defined(TZ)
+# define TZ ""
+# define SPACER ""
+#else
+# define SPACER " "
+#endif
 
-char vmBuildString[] = VM_BUILD_STRING;
+#ifdef _MSC_VER
+# if _MSC_VER >= 1900
+	define the version string here
+# elif _MSC_VER >= 1800
+#	define __VERSION__ "Microsoft Visual Studio 2013"
+# elif _MSC_VER >= 1700
+#	define __VERSION__ "Microsoft Visual Studio 2012"
+# elif _MSC_VER >= 1600
+#	define __VERSION__ "Microsoft Visual Studio 2010"
+# elif _MSC_VER >= 1500
+#	define __VERSION__ "Microsoft Visual Studio 2008"
+# elif _MSC_VER >= 1400
+#	define __VERSION__ "Microsoft Visual Studio 2005"
+# elif _MSC_VER >= 1300
+#	define __VERSION__ "Microsoft Visual Studio 2003"
+# elif _MSC_VER >= 1200
+#	define __VERSION__ "Microsoft Visual Studio 6"
+# elif _MSC_VER >= 1100
+#	define __VERSION__ "Microsoft Visual Studio 5"
+# else
+	define the version string here
+# endif	//MSVC versions
+#endif	//MSVC versions
+
+char vmBuildString[] = \
+	"Win32 built on " \
+	__DATE__" "__TIME__ SPACER TZ \
+	" Compiler: " __VERSION__;
