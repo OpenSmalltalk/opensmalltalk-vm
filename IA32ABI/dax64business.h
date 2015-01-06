4cec648a2d79785366c6a7b4219bf59720f59d71
Eliot Miranda <eliot.miranda@gmail.com>
2015-01-06 22:15:43 +0000

    Add the x86-64 Alien plugin files.
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@3210 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: d972d610b98cadb5a37044b35c4d6b50556271d0

---
+++ IA32ABI/dax64business.h
@@ -0,0 +1,145 @@
+/*
+ *  dax64business.h
+ *
+ *  Written by Eliot Miranda 12/14
+ *
+ * Body of the various callIA32XXXReturn functions.
+ * Call a foreign function according to x64-ish ABI rules.
+ * N.B. In Cog Stack and Cogit VMs numArgs is negative to access args from
+ * the downward-growing stack.
+ */
+	long i, size;
+	sqInt funcAlien, resultMaybeAlien;
+	char *argvec;
+	char *argstart;
+
+#if STACKVM /* Need to access args downwards from first arg */
+  if (numArgs < 0)
+	for (i = size = 0; --i >= numArgs;) {
+		sqInt arg = argVector[i+1];
+		if (objIsAlien(arg) && sizeField(arg))
+			size += moduloPOT(sizeof(long),abs(sizeField(arg)));
+		else if (interpreterProxy->isFloatObject(arg))
+			size += sizeof(double);
+		else /* assume an integer or pointer.  check below. */
+			size += sizeof(long);
+	}
+  else
+#endif /* STACKVM */
+	for (i = numArgs, size = 0; --i >= 0;) {
+		sqInt arg = argVector[i];
+		if (objIsAlien(arg) && sizeField(arg))
+			size += moduloPOT(sizeof(long),abs(sizeField(arg)));
+		else if (interpreterProxy->isFloatObject(arg))
+			size += sizeof(double);
+		else /* assume an integer or pointer.  check below. */
+			size += sizeof(long);
+	}
+
+	/* At point of call stack must be aligned to STACK_ALIGN_BYTES.  So alloca
+	 * at least enough for this plus the argvector, and start writing argvector
+	 * at aligned point.  Then just prior to call cut-back stack to aligned.
+	 */
+	argvec = alloca(STACK_ALIGN_BYTES + moduloPOT(STACK_ALIGN_BYTES,size));
+	argvec = alignModuloPOT(STACK_ALIGN_BYTES, argvec);
+	argstart = argvec;
+
+#if STACKVM /* Need to access args downwards from first arg */
+  if (numArgs < 0)
+	for (i = size = 0; --i >= numArgs;) {
+		sqInt arg = argVector[i+1];
+		if (isSmallInt(arg)) {
+			*(long *)argvec = intVal(arg);
+			argvec += sizeof(long);
+		}
+		else if (objIsAlien(arg)) {
+			long  argByteSize;
+
+			if (!(size = sizeField(arg)))
+				size = argByteSize = sizeof(void *);
+			else
+				argByteSize = abs(size);
+			memcpy(argvec, startOfDataWithSize(arg,size), argByteSize);
+			argvec += moduloPOT(sizeof(long), argByteSize);
+		}
+		else if (objIsUnsafeAlien(arg)) {
+			sqInt bitsObj = interpreterProxy->fetchPointerofObject(0,arg);
+			void *v = interpreterProxy->firstIndexableField(bitsObj);
+			*(void **)argvec = v;
+			argvec += sizeof(long);
+		}
+		else if (interpreterProxy->isFloatObject(arg)) {
+			double d = interpreterProxy->floatValueOf(arg);
+			*(double *)argvec = d;
+			argvec += sizeof(double);
+		}
+		else {
+			long v = interpreterProxy->signed32BitValueOf(arg);
+			if (interpreterProxy->failed()) {
+				interpreterProxy->primitiveFailFor(0);
+				v = interpreterProxy->positive32BitValueOf(arg);
+				if (interpreterProxy->failed())
+					return PrimErrBadArgument;
+			}
+			*(long *)argvec = v;
+			argvec += sizeof(long);
+		}
+	}
+  else
+#endif /* STACKVM */
+	for (i = 0; i < numArgs; i++) {
+		sqInt arg = argVector[i];
+		if (isSmallInt(arg)) {
+			*(long *)argvec = intVal(arg);
+			argvec += sizeof(long);
+		}
+		else if (objIsAlien(arg)) {
+			long  argByteSize;
+
+			if (!(size = sizeField(arg)))
+				size = argByteSize = sizeof(void *);
+			else
+				argByteSize = abs(size);
+			memcpy(argvec, startOfDataWithSize(arg,size), argByteSize);
+			argvec += moduloPOT(sizeof(long), argByteSize);
+		}
+		else if (objIsUnsafeAlien(arg)) {
+			sqInt bitsObj = interpreterProxy->fetchPointerofObject(0,arg);
+			void *v = interpreterProxy->firstIndexableField(bitsObj);
+			*(void **)argvec = v;
+			argvec += sizeof(long);
+		}
+		else if (interpreterProxy->isFloatObject(arg)) {
+			double d = interpreterProxy->floatValueOf(arg);
+			*(double *)argvec = d;
+			argvec += sizeof(double);
+		}
+		else {
+			long v = interpreterProxy->signed32BitValueOf(arg);
+			if (interpreterProxy->failed()) {
+				interpreterProxy->primitiveFailFor(0);
+				v = interpreterProxy->positive32BitValueOf(arg);
+				if (interpreterProxy->failed())
+					return PrimErrBadArgument;
+			}
+			*(long *)argvec = v;
+			argvec += sizeof(long);
+		}
+	}
+
+	funcAlien = interpreterProxy->stackValue(funcOffset);
+	f = *(void **)startOfParameterData(funcAlien);
+	/* cut stack back to start of aligned args */
+	setsp(argstart);
+	r = f();
+	/* post call need to refresh stack pointer in case of call-back and GC. */
+	resultMaybeAlien = interpreterProxy->stackValue(resultOffset);
+	if (objIsAlien(resultMaybeAlien)) {
+		if (!(size = sizeField(resultMaybeAlien)))
+			size = sizeof(void *);
+		memcpy(startOfDataWithSize(resultMaybeAlien,size),
+				&r,
+				min((unsigned)abs(size), sizeof(r)));
+	}
+
+	return PrimNoErr;
