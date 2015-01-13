46c067da604293804aa900f784a0a419ed82b1e2
Eliot Miranda <eliot.miranda@gmail.com>
2015-01-13 18:04:53 +0000

    Add a processor-agnostic file to include the relevant processor-specific callout and callback support for the ALien plugin.
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@3227 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: de732c745d80769e5a3541e07042f7ba8c112911

---
+++ IA32ABI/xabicc.c
@@ -0,0 +1,14 @@
+/*
+ *  xabicc.c - platform-agnostic root for ALien call-outs and callbacks.
+ *
+ * Support for Call-outs and Call-backs from the IA32ABI Plugin.
+ * The plgin is misnamed.  It should be the AlienPlugin, but its history
+ * dictates otherwise.
+ */
+#if i386|i486|i586|i686
+# include "ia32abicc.c"
+#elif powerpc|ppc
+# include "ppcia32abicc.c"
+#elif x86_64|x64
+# include "x64ia32abicc.o"
+#endif
