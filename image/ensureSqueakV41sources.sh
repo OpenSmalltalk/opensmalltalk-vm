8ed6e8c111dcf640b828ed1f5cae2555ebbebbe3
Eliot Miranda <eliot.miranda@gmail.com>
2015-03-18 00:12:19 +0000

    Fix slip in ensureSqueakV41sources for gzipped download case
    git-svn-id: http://squeakvm.org/svn/squeak/branches/Cog@3280 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: 7a63738e60865289cead1185728187dd2f40e172

--- image/ensureSqueakV41sources.sh
+++ image/ensureSqueakV41sources.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+. ./envvars.sh
 test -f SqueakV41.sources && exit 0
 if [ -f ../sources/SqueakV41.sources ]; then
 	ln ../sources/SqueakV41.sources .
 	exit 0
 fi
 geturl http://ftp.squeak.org/sources_files/SqueakV41.sources.gz
-gunzip SqueakV41.sources.gz
+gunzip SqueakV41.sources
