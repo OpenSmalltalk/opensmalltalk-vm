3b7b0c66e52e62e8ec793bd6db0f1a5e376563a7
Eliot Miranda <eliot.miranda@gmail.com>
2015-03-24 02:11:16 +0000

    Small tweaks to VMMakewr image build scripts and readmes, including a checksum for the SPur trunk images.
    git-svn-id: http://squeakvm.org/svn/squeak/branches/Cog@3286 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: 3e89fc90a21ac20ae84b2ced7c38c0c44d923c65

--- image/getlatestspurtrunkimage.sh
+++ image/getlatestspurtrunkimage.sh
@@ -3,3 +3,6 @@
 
 geturl http://www.mirandabanda.org/files/Cog/SpurImages/trunk46-spur.image
 geturl http://www.mirandabanda.org/files/Cog/SpurImages/trunk46-spur.changes
+geturl http://www.mirandabanda.org/files/Cog/SpurImages/trunk46-spur.sum
+
+cksum trunk46-spur.changes trunk46-spur.image | diff - trunk46-spur.sum >/dev/null && ls -l trunk46-spur.image trunk46-spur.changes || echo image and/or changes have wrong checksum 1>&2
--- image/uploadspurimage.sh
+++ image/uploadspurimage.sh
@@ -9,16 +9,17 @@ BASENAME=trunk46-spur
 
 test "$1" = -here || cd `dirname $0`
 echo uploading trunk46-spur from `pwd`
+cksum $BASENAME.[ci]* >$BASENAME.sum
 
 DIR=$RemoteRoot/$DATE
 echo ssh -x $RemoteUser mkdir $DIR
 ssh -x $RemoteUser mkdir $DIR
-echo scp -p $BASENAME.image $BASENAME.changes "$@" $RemoteUser:$DIR
+echo scp -p $BASENAME.image $BASENAME.changes $BASENAME.sum "$@" $RemoteUser:$DIR
 scp -p $BASENAME.image $BASENAME.changes "$@" $RemoteUser:$DIR
 echo ssh $RemoteUser chmod a-w $DIR/* \\\; ls -al $DIR
 ssh $RemoteUser chmod a-w $DIR/* \; ls -al $DIR
 echo ssh $RemoteUser rm $RemoteRoot/{$BASENAME.image,$BASENAME.changes}
 ssh $RemoteUser rm $RemoteRoot/{$BASENAME.image,$BASENAME.changes}
 echo ssh $RemoteUser cd $RemoteRoot \\\; ln -s $DATE/{$BASENAME.image,$BASENAME.changes} .
-ssh $RemoteUser cd $RemoteRoot \; ln -s $DATE/{$BASENAME.image,$BASENAME.changes} .
+ssh $RemoteUser cd $RemoteRoot \; ln -s $DATE/{$BASENAME.image,$BASENAME.changes,$BASENAME.sum} .
 ssh $RemoteUser ls -l $RemoteRoot $RemoteRoot/$DATE
