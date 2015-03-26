34c80dbdc1d189defefb85f2bcc7f051d1f85ca5
Eliot Miranda <eliot.miranda@gmail.com>
2015-03-26 18:52:48 +0000

    Add a script to build debuggable ARM sim libraries. Make those makeit scripts for plugins install in all the right places.
    git-svn-id: http://squeakvm.org/svn/squeak/branches/Cog@3294 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: aa523703396b86a9197876ae72f32cfd9e004af7

--- processors/ARM/mac/BUILDarmsim-dbg.sh
+++ processors/ARM/mac/BUILDarmsim-dbg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This script builds
+# This script builds debug versions of
 #	gdb-7.6/opcodes/libopcodes.a
 #	gdb-7.6/sim/arm/libsim.a
 
@@ -9,11 +9,11 @@ ln -s ../../binutils-2.25/bfd/bfd.h gdb-7.6/bfd/bfd.h
 ln -s ../../binutils-2.25/bfd/config.h gdb-7.6/bfd/config.h
 libtool -V >gdb-7.6/bfd/libtool-soversion
 (cd gdb-7.6/opcodes # i.e. processors/ARM/gdb-7.6/opcodes
-./configure --target=arm-linux --build=i386-apple-darwin9 CFLAGS="-arch i386 -g -O2" LDFLAGS="-arch i386"
+./configure --target=arm-linux --build=i386-apple-darwin9 CFLAGS="-arch i386 -g -O0" LDFLAGS="-arch i386"
 make
 
 cd ../sim/common # i.e. processors/ARM/gdb-7.6/sim/common
-./configure --target=arm-linux --build=i386-apple-darwin9 CFLAGS="-arch i386 -g -O2" LDFLAGS="-arch i386"
+./configure --target=arm-linux --build=i386-apple-darwin9 CFLAGS="-arch i386 -g -O0" LDFLAGS="-arch i386"
 cd ../arm # i.e. processors/ARM/gdb-7.6/sim/arm
-./configure --target=arm-linux --build=i386-apple-darwin9 CFLAGS="-arch i386 -g -O2" LDFLAGS="-arch i386"
+./configure --target=arm-linux --build=i386-apple-darwin9 CFLAGS="-arch i386 -g -O0" LDFLAGS="-arch i386"
 make)
