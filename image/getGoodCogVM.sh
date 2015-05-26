e7da1423e03eba12221a52b6fbf4a6b14035008f
Eliot Miranda <eliot.miranda@gmail.com>
2015-05-26 20:40:31 +0000

    Update the getGoodFooVM scripts to use the 15.20.3354 VMs
    git-svn-id: http://squeakvm.org/svn/squeak/branches/Cog@3358 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: 2fad2a3e9b18c45811a518bb127add71379bf94e

--- image/getGoodCogVM.sh
+++ image/getGoodCogVM.sh
@@ -1,32 +1,32 @@
 #!/bin/sh
-# Sets the VM env var to the r3284 Cog VM for the current platform.
+# Sets the VM env var to the r3354 Cog VM for the current platform.
 # will download and install the VM in this directory if necessary.
 
-TAG=15.12.3284
-REV=3284
-LCBINDIR=4.0-3284
+TAG=15.20.3354
+REV=3354
+LCBINDIR=4.0-3354
 URL=http://www.mirandabanda.org/files/Cog/VM/VM.r$REV/
 
 . ./envvars.sh
 
 case "$OS" in
 Darwin) get_vm_from_tar \
-            Cog.app/Contents/MacOS/Squeak 51f69399642245df1978b81b529ed7a0 \
-            Cog.app-$TAG.tgz 34828c5b563e5e64ede280429ca1a745
+            Cog.app/Contents/MacOS/Squeak a6f0ece7775772220e8bfe1e48524959 \
+            Cog.app-$TAG.tgz 269ba0ef5e63aeaa52287834b6242966
         VM=Cog.app/Contents/MacOS/Squeak;;
 Linux)
     if expr $OSREL \> 2.6.12; then
         get_vm_from_tar \
-        coglinuxht/lib/squeak/$LCBINDIR/squeak b8c8ccdb36d1fd893a9b15778e2bf500 \
-        coglinuxht-$TAG.tgz 2035d84a2a4f2dc27d40b988348c21f8
+        coglinuxht/lib/squeak/$LCBINDIR/squeak 7ed115ceb484859c182ced6541137ce8 \
+        coglinuxht-$TAG.tgz 9f50e0fa254d763fae635eeb98ecdeb0
     else
         get_vm_from_tar \
-        coglinux/lib/squeak/$LCBINDIR/squeak 4c9add7b905d9b80ccca522926df16d4 \
-        coglinux-$TAG.tgz af87e9a96be77318cce91a9fd07396ad
+        coglinux/lib/squeak/$LCBINDIR/squeak 43804a192631dabba11f4051611475b0 \
+        coglinux-$TAG.tgz 16b277420542af403be016dde6769c18
     fi;;
 CYGWIN*) get_vm_from_zip \
-            cogwin/SqueakConsole.exe 60f116a058a6d6ed55b3eda594b9e267 \
-            cogwin-$TAG.zip 0c4d4eab877bbdc884fb691176640aea
+            cogwin/SqueakConsole.exe f55a39e14e6d0228c718f7b868e70bbc \
+            cogwin-$TAG.zip b5253c1675b9e0d3887d50bd050fd75d
     VM=cogwin/SqueakConsole.exe;;
 *)  echo "don't know how to run Squeak on your system.  bailing out." 1>&2; exit 2
 esac
--- image/getGoodSpurVM.sh
+++ image/getGoodSpurVM.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
-# Sets the VM env var to the r3284 Cog Spur VM for the current platform.
+# Sets the VM env var to the r3354 Cog Spur VM for the current platform.
 # will download and install the VM in this directory if necessary.
 
-TAG=15.12.3284
-REV=3284
-LSBINDIR=5.0-3284
+TAG=15.20.3354
+REV=3354
+LSBINDIR=5.0-3354
 URL=http://www.mirandabanda.org/files/Cog/VM/VM.r$REV/
 
 . ./envvars.sh
 
 case "$OS" in
 Darwin) get_vm_from_tar \
-            CogSpur.app/Contents/MacOS/Squeak c54b15ba899696e2efbe8844c8ba50c3 \
-            CogSpur.app-$TAG.tgz cb11c0850aa43b0bef658254b1e02bc0
+            CogSpur.app/Contents/MacOS/Squeak 4858caf533bcac30301b63b4a2b03dee \
+            CogSpur.app-$TAG.tgz 69dfd7c4af7c5c1323b57f8df1945759
         VM=CogSpur.app/Contents/MacOS/Squeak;;
 Linux) get_vm_from_tar \
-        cogspurlinuxht/lib/squeak/$LSBINDIR/squeak 2092203ae9f5e6daedc9be0a1a85fa8a \
-        cogspurlinuxht-$TAG.tgz 3ee0ff388b993297340a40dd8df6388c
+        cogspurlinuxht/lib/squeak/$LSBINDIR/squeak e7ef9060beedb0e3d049de685d269eb1 \
+        cogspurlinuxht-$TAG.tgz 7d91b75be4f17ea0ea836eba5237fa10
     VM=cogspurlinuxht/squeak;;
 CYGWIN*) get_vm_from_zip \
-            cogspurwin/SqueakConsole.exe c703176603aa14bf83d68ad9fa2749f0 \
-            cogspurwin-$TAG.zip 048352452883f843ab28c0a9be821891
+            cogspurwin/SqueakConsole.exe f0cf0edce87d42cf1cc7bd8afc4ed8ca \
+            cogspurwin-$TAG.zip 75d735ce6f9b4368662e21b26de1ae11
     VM=cogspurwin/SqueakConsole.exe;;
 *)  echo "don't know how to run Squeak on your system.  bailing out." 1>&2; exit 2
 esac
