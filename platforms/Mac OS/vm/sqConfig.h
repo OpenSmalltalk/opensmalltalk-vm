e01ad27d84b684ca9491a66e2b9364c0f0159288
Eliot Miranda <eliot.miranda@gmail.com>
2015-06-18 21:08:45 +0000

    Cog VM source as per VMMaker.oscog-eem.1363
    Move the Literal initialization code into CogAbstractInstruction and make sure
    they are fully initializaed (old code left bogus annotations).
    
    At least under X11 the OOLL ARM Cog VM now runs.
    
    Conditionalize the PlatformNoDbgRegParms defines to be x86 only to avoid
    warnings on ARM.
    
    
    git-svn-id: http://squeakvm.org/svn/squeak/branches/Cog@3381 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: 279b2150eadffc89b0b2ad98745e722d5b0f4310

--- platforms/Mac OS/vm/sqConfig.h
+++ platforms/Mac OS/vm/sqConfig.h
@@ -22,6 +22,12 @@
 #endif
 
 #if defined(__GNUC__)
-# define PlatformNoDbgRegParms __attribute__ ((regparm (0)))
+/* Define the "don't generate functions with register parameters" attribute
+ * for x86 and similar.  Do so for debugging; gdb typically can't call static
+ * functions that have been optimized to use register arguments.
+ */
+# if defined(_M_I386) || defined(_X86_) || defined(i386) || defined(i486) || defined(i586) || defined(i686) || defined(__i386__) || defined(__386__) || defined(X86) || defined(I386)
+#	define PlatformNoDbgRegParms __attribute__ ((regparm (0)))
+# endif
 # define NeverInline __attribute__ ((noinline))
 #endif
--- platforms/win32/vm/sqConfig.h
+++ platforms/win32/vm/sqConfig.h
@@ -33,6 +33,13 @@
 #define VMBIGENDIAN 0
 
 #if defined(__GNUC__) && (__GNUC__ == 3 || (!defined(__MINGW32__) && !defined(__MINGW64__)))
+/* Define the "don't generate functions with register parameters" attribute
+ * for x86 and similar.  Do so for debugging; gdb typically can't call static
+ * functions that have been optimized to use register arguments.
+ */
+# if defined(_M_I386) || defined(_X86_) || defined(i386) || defined(i486) || defined(i586) || defined(i686) || defined(__i386__) || defined(__386__) || defined(X86) || defined(I386)
+#	define PlatformNoDbgRegParms __attribute__ ((regparm (0)))
+# endif
 # define PlatformNoDbgRegParms __attribute__ ((regparm (0)))
 # define NeverInline __attribute__ ((noinline))
 #endif
