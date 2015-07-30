38e0b4f9434f3dab0a7d828d3aa67db4832df8b7
Eliot Miranda <eliot.miranda@gmail.com>
2015-07-30 01:46:49 +0000

    Integrate Levente Uzonyi, Marcel Taeumel and Tobias Pape's V2 SSL plugin header. This change adds only a version number define.
    git-svn-id: http://squeakvm.org/svn/squeak/trunk@3415 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: a1c0a167da43050eb7a1adc8282436d8b4697d12

--- platforms/Cross/plugins/SqueakSSL/SqueakSSL.h
+++ platforms/Cross/plugins/SqueakSSL/SqueakSSL.h
@@ -1,6 +1,16 @@
 #ifndef SqueakSSL_H
 #define SqueakSSL_H
 
+/**********************************************************/
+/* SqueakSSL Version Information                          */
+/* Version 1: Inital version                              */
+/* Version 2: SNI support by Levente Uzonyi and help by   */
+/*            Marcel Taeumel and Tobias Pape              */
+/**********************************************************/
+
+
+#define SQSSL_VERSION 2
+
 /*************************/
 /* SSL connection states */
 /*************************/
