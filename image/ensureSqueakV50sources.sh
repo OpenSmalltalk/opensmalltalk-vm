292e6fbcb42c7ee745b00c20da7d62379f44e341
Eliot Miranda <eliot.miranda@gmail.com>
2015-09-26 22:54:37 +0000

    Include the script taht fetches SqueakV50.sources. Get the new 32-bit Mac build to use the x86 IA32ABI support file, not the x64 one.
    git-svn-id: http://squeakvm.org/svn/squeak/branches/Cog@3457 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: ae0d69349b6bea8a26db6639371b3d6f329571d5

---
+++ image/ensureSqueakV50sources.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+. ./envvars.sh
+test -f SqueakV50.sources && exit 0
+if [ -f ../sources/SqueakV50.sources ]; then
+	ln ../sources/SqueakV50.sources .
+	exit 0
+fi
+geturl http://ftp.squeak.org/sources_files/SqueakV50.sources.gz
+gunzip SqueakV50.sources
