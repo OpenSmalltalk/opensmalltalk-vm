c8ddf4af6f59428b110ab07dae946b6478b7bf30
Eliot Miranda <eliot.miranda@gmail.com>
2016-01-17 03:12:08 +0000

    CogVM source as per VMMaker.oscog-eem.1646
    Add a 64-bit Newspeak Cog Spur configuration.
    
    
    Fix the Unix vm makefile for 64 bits.  It must not specify the
    processor-specific cogit files because they differ on 64-bit and
    32-bit platforms.  instead it can only specify cogit.c, the umbrella
    that includes the relevant cogitFOO.c.
    
    
    git-svn-id: http://squeakvm.org/svn/squeak/branches/Cog@3561 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: 57b32376a785a43a9af94ab0366424057dd8ff7f

---
+++ nsspur64src/vm/exampleSqNamedPrims.h
@@ -0,0 +1,12 @@
+/* This is an automatically generated table of all builtin modules in the VM
+   CoInterpreterPrimitives VMMaker.oscog-eem.1646
+ */
+
+extern sqExport vm_exports[];
+extern sqExport os_exports[];
+
+sqExport *pluginExports[] = {
+	vm_exports,
+	os_exports,
+	NULL
+};
