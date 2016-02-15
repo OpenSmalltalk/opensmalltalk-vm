ade4353d00a6a440567679e66e744dd8699b1b27
Eliot Miranda <eliot.miranda@gmail.com>
2016-02-15 19:28:01 +0000

    Add image/uploadspurimage.sh do do same.  And previous commit updated the get vm scripts to use the 3602 VMs.
    git-svn-id: http://squeakvm.org/svn/squeak/branches/Cog@3605 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: a2b0a3a69818a4e020a777d67c98b0adf3d6ece2

--- image/getGoodCogVM.sh
+++ image/getGoodCogVM.sh
@@ -11,21 +11,22 @@ URL=http://www.mirandabanda.org/files/Cog/VM/VM.r$REV/
 
 case "$OS" in
 Darwin) get_vm_from_tar \
-            Cog.app/Contents/MacOS/Squeak  \
-            Cog.app-$TAG.tgz         VM=Cog.app/Contents/MacOS/Squeak;;
+            Cog.app/Contents/MacOS/Squeak 881a82bdab55694933114608c2e083f1 \
+            Cog.app-$TAG.tgz e80c18e45b33f23dc17bedc5108385f9
+        VM=Cog.app/Contents/MacOS/Squeak;;
 Linux)
     if expr $OSREL \> 2.6.12; then
         get_vm_from_tar \
         coglinuxht/lib/squeak/$LCBINDIR/squeak  \
-        coglinuxht-$TAG.tgz 5067a62fde21ff4f39181619126b8380
+        coglinuxht-$TAG.tgz 0122db0348fbf6ee27e0189d8c6101ab
     else
         get_vm_from_tar \
         coglinux/lib/squeak/$LCBINDIR/squeak  \
-        coglinux-$TAG.tgz 60078d8c3f5d24ea8d8362ffb5bc7b3b
+        coglinux-$TAG.tgz 3dd2fc07aee66be652b7636bf2337090
     fi;;
 CYGWIN*) get_vm_from_zip \
             cogwin/SqueakConsole.exe 5a11b5d0fd2aa32e966cc76d85fd6876 \
-            cogwin-$TAG.zip 4a0b12b67164b788aa615089b5e8cb81
+            cogwin-$TAG.zip 71546e5fde965441ca6da248a2fd003f
     VM=cogwin/SqueakConsole.exe;;
 *)  echo "don't know how to run Squeak on your system.  bailing out." 1>&2; exit 2
 esac
---
+++ image/uploadspurimage.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# Upload a trunk46-spur.image/.changes to mirandabanda.org
+RemoteUser=eliotmiranda@highland-park.dreamhost.com
+RemoteRoot=mirandabanda.org/files/Cog/SpurImages
+IFS="	
+"
+DATE=`date +%Y-%m-%d`
+BASENAME=trunk46-spur
+
+test "$1" = -here || cd `dirname $0`
+
+DIR=$RemoteRoot/$DATE
+echo ssh -x $RemoteUser mkdir $DIR
+ssh -x $RemoteUser mkdir $DIR
+for BASENAME in "$@"; do
+	echo uploading $BASENAME from `pwd`
+	cksum $BASENAME.[ci]* >$BASENAME.sum
+	echo scp -p $BASENAME.image $BASENAME.changes $BASENAME.sum "$@" $RemoteUser:$DIR
+	scp -p $BASENAME.image $BASENAME.changes $BASENAME.sum "$@" $RemoteUser:$DIR
+done
+
+echo ssh $RemoteUser chmod a-w $DIR/* \\\; ls -al $DIR
+ssh $RemoteUser chmod a-w $DIR/* \; ls -al $DIR
+echo ssh $RemoteUser rm -f $RemoteRoot/{*.image,*.changes,*.sum}
+ssh $RemoteUser rm -f $RemoteRoot/{*.image,*.changes,*.sum}
+echo ssh $RemoteUser cd $RemoteRoot \\\; ln -s $DATE/{*.image,*.changes,*.sum} .
+ssh $RemoteUser cd $RemoteRoot \; ln -s $DATE/{*.image,*.changes,*.sum} .
+
+ssh $RemoteUser ls -l $RemoteRoot $RemoteRoot/$DATE
