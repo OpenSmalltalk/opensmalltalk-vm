ac9194068a302e0cec9efcc554ce0f738846150f
Eliot Miranda <eliot.miranda@gmail.com>
2016-04-09 00:17:14 +0000

    Make the relevant workspaces in the VMMaker image syntax highlighted. Nuke an obsolete workspace. Add a script to create an image containing FullBlockCLosure and MethodMassage for working on Sista block support.
    git-svn-id: http://squeakvm.org/svn/squeak/branches/Cog@3671 fa1542d4-bde8-0310-ad64-8ed1123d492a
    
    Former-commit-id: ee2ece60edef77e7212b1627752b745be8482bed

---
+++ image/buildsistareaderimage.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Build a Spur image that starts up in a simple REPL, which is
+# really useful for VMMaker simulation testing.
+. ./envvars.sh
+
+./updatespurimage.sh
+
+./ensureSqueakV50sources.sh
+
+cp -p trunk50.image sistareader.image
+cp -p trunk50.changes sistareader.changes
+
+. ./getGoodSpurVM.sh
+
+echo $VM sistareader.image LoadSistaSupport.st
+$VM sistareader.image LoadSistaSupport.st
+
+echo $VM sistareader.image LoadReader.st
+$VM sistareader.image LoadReader.st
+
+echo $VM sistareader.image StartReader.st
+$VM sistareader.image StartReader.st
