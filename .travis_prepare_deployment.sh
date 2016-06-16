0db37508cf839277ecd37a89f9b1842efaa2807f
Bert Freudenberg <bert@freudenbergs.de>
2016-06-16 11:44:46 +0200

    ci: fix bintray deployment on mac
    Former-commit-id: dd15b358f111a95954e243ad03b01075e9ed66db

--- .travis_prepare_deployment.sh
+++ .travis_prepare_deployment.sh
@@ -1,5 +1,6 @@
 set -e
 
 echo "`cat .bintray.json | .git_filters/RevDateURL.smudge`" > .bintray.json
-sed -i 's/$Rev: \([0-9][0-9]*\) \$/\1/' .bintray.json
-sed -i 's/$Date: \(.*\) \$/\1/' .bintray.json
+sed -i.bak 's/$Rev: \([0-9][0-9]*\) \$/\1/' .bintray.json
+sed -i.bak 's/$Date: \(.*\) \$/\1/' .bintray.json
+rm -f .bintray.json.bak
