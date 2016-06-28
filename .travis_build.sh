b4ee2ce2863e456aaa6203ab191839392160b155
Fabio Niephaus <code@fniephaus.com>
2016-06-28 09:13:04 +0200

    Rename linux32ARM builds to linux32ARMv6
    @eliotmiranda

--- .travis_build.sh
+++ .travis_build.sh
@@ -60,7 +60,7 @@ fi
 [[ -z "${ARCH}" ]] && exit 2
 [[ -z "${FLAVOR}" ]] && exit 3
 
-if [[ "${ARCH}" == "linux32ARM" ]]; then
+if [[ "${ARCH}" == "linux32ARM"* ]]; then
     # we're in  chroot at this point
     TRAVIS_BUILD_DIR="$(pwd)"
 fi
