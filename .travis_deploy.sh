9acc27313f17b0e8618d6bc97e6f0ef1d9f71cd6
Tim Felgentreff <timfelgentreff@gmail.com>
2016-07-25 12:02:38 +0200

    fix bintray cleanup script

--- .travis_deploy.sh
+++ .travis_deploy.sh
@@ -18,6 +18,36 @@ if [[ $PR == "false" ]] && ( [[ "$BR" == "Cog" || "$BR" == "master" ]] ); then
 
     # Clean out old versions from bintray, leaving only the last version per month
     if [[ "${TRAVIS_OS_NAME}" == "linux" ]] && [[ "${ARCH}" == "linux64x64" ]] && [[ "${FLAVOR}" == "squeak.cog.spur" ]]; then
-	    ruby -rdate -rnet/http -rjson -e "lastver=DateTime.now; user='timfel'; pass='${BINTRAYAPIKEY}'; url='https://'+user+':'+pass+'@api.bintray.com/packages/opensmalltalk/vm/cog'; JSON.parse(Net::HTTP.get(URI(url)))['versions'][10..-1].each {|v| ver=DateTime.parse(v); if ver.month!=lastver.month then lastver=ver; next else lastver=ver end; uri=URI(url+'/versions/'+v); http=Net::HTTP.new(uri.host,uri.port); http.use_ssl=true; http.start { req=Net::HTTP::Delete.new(uri); req.basic_auth(user,pass); resp=http.request(req); puts 'Deleted ' + v + ' ' + resp.body } }"
+	ruby -rdate -rnet/http -rjson -e "lastver=DateTime.now;
+            user='timfel';
+            pass='${BINTRAYAPIKEY}';
+            url='https://api.bintray.com/packages/opensmalltalk/vm/cog';
+            uri=URI(url);
+            json=nil;
+            http=Net::HTTP.new(uri.host,uri.port);
+            http.use_ssl=true;
+            http.start {
+                req=Net::HTTP::Get.new(uri);
+                req.basic_auth(user,pass);
+                resp=http.request(req);
+                json=JSON.parse(resp.body)
+            };
+            json['versions'][10..-1].each { |v|
+                ver=DateTime.parse(v);
+                if ver.month != lastver.month then
+                    lastver=ver; next
+                else
+                    lastver=ver
+                end;
+                uri=URI(url+'/versions/'+v);
+                http=Net::HTTP.new(uri.host,uri.port);
+                http.use_ssl=true;
+                http.start {
+                    req=Net::HTTP::Delete.new(uri);
+                    req.basic_auth(user,pass);
+                    resp=http.request(req);
+                    puts 'Deleted ' + v + ' ' + resp.body
+                }
+            }"
     fi
 fi
