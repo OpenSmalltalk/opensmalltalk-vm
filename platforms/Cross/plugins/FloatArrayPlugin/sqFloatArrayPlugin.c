dd702d74a4da48000b2fc60a55670c4c27532e21
Ana Laura Perez Cerrato <lauraperezcerrato@gmail.com>
2016-09-08 14:35:17 -0300

    Changed implementation to be iterative instead of recursive, optimized sorting of small arrays

--- platforms/Cross/plugins/FloatArrayPlugin/sqFloatArrayPlugin.c
+++ platforms/Cross/plugins/FloatArrayPlugin/sqFloatArrayPlugin.c
@@ -1,14 +1,62 @@
 void quicksort(float* array, int from, int to) {
+    int stack[to - from + 1];
+    int top = -1;
+    
+    top++;
+    stack[top] = from;
+    top++;
+    stack[top] = to;
+    
+    while (top >= 0) {
+        int high;
+        int low;
+        
+        high = stack[top];
+        top--;
+        low = stack[top];
+        top--;
+        
+        int pivotPosition = partition(array, low, high);
+        
+        if (pivotPosition - 1 > low) {
+            if (pivotPosition - 1 - low > 10) {
+                top++;
+                stack[top] = low;
+                top++;
+                stack[top] = pivotPosition - 1;
+            } else {
+                insort(array, low, pivotPosition - 1);
+            }
+        }
+        
+        if (pivotPosition + 1 < high) {
+            if (high - (pivotPosition + 1) > 10) {
+                top++;
+                stack[top] = pivotPosition + 1;
+                top++;
+                stack[top] = high;
+            } else {
+                insort(array, pivotPosition + 1, high);
+            }
+        }
+        
+    }
+    
     if (from < to) {
         int p = partition(array, from, to);
         quicksort(array, from, p);
         quicksort(array, p + 1, to);
     }
+    
     return;
 }
 
 int partition(float* array, int from, int to) {
-    float pivot = array[from];
+    float possiblePivots[] = { array[from], array[(to - from) / 2 + from], array[to] };
+    float pivot;
+    pivot = possiblePivots[0] < possiblePivots[2] ? possiblePivots[0] : possiblePivots[2];
+    pivot = pivot > possiblePivots[1] ? pivot : possiblePivots[1];
+    
     int i = from - 1;
     int j = to + 1;
     while (-1) {
@@ -25,5 +73,19 @@ int partition(float* array, int from, int to) {
         float b = array[j];
         array[i] = b;
         array[j] = a;
+    }
+}
+
+void insort(float* array, int from, int to) {
+    int i;
+    for (i = from; i <= to; i++) {
+        int j = i;
+        while (j > 0 && array[j - 1] > array[j]) {
+            float a = array[j - 1];
+            float b = array[j];
+            array[j] = b;
+            array[j - 1] = a;
+            j--;
+        }
     }
 }
