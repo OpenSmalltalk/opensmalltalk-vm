31d68abfcf8977e8fe5a1198d36e68796e0e1349
Esteban Lorenzano <estebanlm@gmail.com>
2016-09-12 19:52:46 +0200

    fix for UNICODE, as Nico Cellier suggested here: https://github.com/estebanlm/opensmalltalk-vm/commit/e24f4da5a82e2b37a3e3274801ff52b4c5ed0685#commitcomment-18987441

--- platforms/win32/vm/sqWin32Window.c
+++ platforms/win32/vm/sqWin32Window.c
@@ -3265,7 +3265,7 @@ int printUsage(int level)
 {
   switch(level) {
     case 0: /* No command line given */
-      abortMessage(TEXT("Usage: " VM_NAME " [options] <imageFile>"));
+      abortMessage(TEXT("Usage: ") TEXT(VM_NAME) TEXT(" [options] <imageFile>"));
       break;
     case 1: /* full usage */
       abortMessage(TEXT("%s"),
