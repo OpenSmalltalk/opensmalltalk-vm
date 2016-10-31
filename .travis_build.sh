59cdf001817ad3b69278684d3bec608eea911076
Fabio Niephaus <code@fniephaus.com>
2016-10-31 21:58:19 +0100

    Run Newspeak bootstrapping after building vm

--- .travis_build.sh
+++ .travis_build.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 set -e
 
-travis_fold() {
-  local action=$1
-  local name=$2
-  local title="${3:-}"
-
-  if [[ "${TRAVIS:-}" = "true" ]]; then
-    echo -en "travis_fold:${action}:${name}\r\033[0K"
-  fi
-  if [[ -n "${title}" ]]; then
-    echo -e "\033[34;1m${title}\033[0m"
-  fi
-}
+source ./.travis_helpers.sh
 
 if [[ "${APPVEYOR}" ]]; then
     ARCH="win32x86"
