3aa8b3e280aae02d9f5ba136b07986e6163c153c
Esteban Lorenzano <estebanlm@gmail.com>
2016-11-18 14:27:03 +0100

    Revert "just verify gcc for windows when using cygwin"
    This reverts commit db1a0e9d828755cf4a68c7c4b58f51346a9fe174.

--- .travis_build.sh
+++ .travis_build.sh
@@ -10,14 +10,14 @@ if [[ "${APPVEYOR}" ]]; then
 
     # Appveyor's GCC is pretty new, patch the Makefiles and replace the tools to
     # make it work
-	if [[ "$(uname -o)" == "Cygwin" ]]; then
-		echo
-		echo "Using gcc $(i686-w64-mingw32-gcc --version)"
-		echo "Using 64-bit gcc $(x86_64-w64-mingw32-gcc --version)"
-		echo
-		test -d /usr/i686-w64-mingw32/sys-root/mingw/lib || echo "No lib dir"
-		test -d /usr/i686-w64-mingw32/sys-root/mingw/include || echo "No inc dir"
-	fi
+
+    echo
+    echo "Using gcc $(i686-w64-mingw32-gcc --version)"
+    echo "Using 64-bit gcc $(x86_64-w64-mingw32-gcc --version)"
+    echo
+    test -d /usr/i686-w64-mingw32/sys-root/mingw/lib || echo "No lib dir"
+    test -d /usr/i686-w64-mingw32/sys-root/mingw/include || echo "No inc dir"
+
 else
     PLATFORM="$(uname -s)"
 fi
