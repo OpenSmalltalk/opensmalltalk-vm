0e510b13177d52165e689d78414c6870b2a62196
Eliot Miranda <eliot.miranda@gmail.com>
2016-11-20 16:43:03 -0800

    CogVM source as per VMMaker.oscog-eem.1998
    The new FilePlugin code must protect against zero-divide when given empty arrays.
    
    image build scripts:
    Quick fix to getting a trunk image, and for overriding the VM.

--- image/updatespurimage.sh
+++ image/updatespurimage.sh
@@ -10,21 +10,28 @@ if false; then
 		cp -p "$SQUEAK50".changes trunk50.changes
 		cp -p "$SQUEAK50".image trunk50.image
 	fi
-else
+elif false; then
 	# New way; download the latest trunk from squeak.org/downloads
 	geturl http://build.squeak.org/job/Trunk/default/lastSuccessfulBuild/artifact/target/TrunkImage.zip
 	unzip -ou TrunkImage.zip
 	mv SpurTrunkImage.changes trunk50.changes
 	mv SpurTrunkImage.image trunk50.image
-	if test \! -f SqueakV50.sources ; then
-		if test -f ../sources/SqueakV50.sources; then
-			ln ../sources/SqueakV50.sources .
-		else
-			if test \! -f SqueakV50.sources.zip; then
-				geturl http://ftp.squeak.org/5.0/SqueakV50.sources.zip
-			fi
-			unzip SqueakV50.sources.zip
+else
+	# New new way; download from http://files.squeak.org/6.0alpha/Squeak6.0alpha-16548-32bit/
+	geturl http://files.squeak.org/6.0alpha/Squeak6.0alpha-16548-32bit/Squeak6.0alpha-16548-32bit.zip
+	unzip -ou Squeak6.0alpha-16548-32bit.zip
+	mv Squeak6.0alpha-16548-32bit.changes trunk50.changes
+	mv Squeak6.0alpha-16548-32bit.image trunk50.image
+fi
+
+if test \! -f SqueakV50.sources ; then
+	if test -f ../sources/SqueakV50.sources; then
+		ln ../sources/SqueakV50.sources .
+	else
+		if test \! -f SqueakV50.sources.zip; then
+			geturl http://ftp.squeak.org/5.0/SqueakV50.sources.zip
 		fi
+		unzip SqueakV50.sources.zip
 	fi
 fi
 
