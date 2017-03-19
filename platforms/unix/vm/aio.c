db76be5eca9b3de7279bf64092b8a7e8463fcb41
Eliot Miranda <eliot.miranda@gmail.com>
2017-03-19 15:10:34 -0700

    CogVM source as per VMMaker.oscog-eem.2167
    StackInterpreter:
    Add the statIdleUsecs parameter to track time in the internals of
    ioRelinquishProcessorForMicroseconds and hence allow the system reporter to
    render the GC times meaningful.  Platform code invokes addIdleUsecs to tally
    idle time.

--- platforms/unix/vm/aio.c
+++ platforms/unix/vm/aio.c
@@ -261,8 +261,11 @@ aioPoll(long microSeconds)
 		n = select(maxFd, &rd, &wr, &ex, &tv);
 		if (n > 0)
 			break;
-		if (n == 0)
+		if (n == 0) {
+			if (microSeconds)
+				addIdleUsecs(microSeconds);
 			return 0;
+		}
 		if (errno && (EINTR != errno)) {
 			fprintf(stderr, "errno %d\n", errno);
 			perror("select");
@@ -305,6 +308,7 @@ aioSleepForUsecs(long microSeconds)
 			struct timespec rmtp;
 
 			nanosleep(&rqtp, &rmtp);
+			addIdleUsecs((rqtp.tv_nanoseconds - rmtp.tv_nanoseconds) / 1000);
 			microSeconds = 0;	/* poll but don't block */
 		}
 	}
