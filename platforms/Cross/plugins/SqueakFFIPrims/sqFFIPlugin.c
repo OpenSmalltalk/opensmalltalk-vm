ca7e8db32835f5d2489ebddc7e14a51c9bbd4697
Eliot Miranda <eliot.miranda@gmail.com>
2017-05-16 15:17:10 -0700

    Change platform sources to refer to _WIN32 & _WIN64, not WIN32 & WIN64. Change getSystemAttribute: 1005 (the Windowing system name) to answer 'Windows' insrtead of 'Win32' (we are stuck with answering 'Win32' on 'Win64' unless we want to break /lots/ of image platform-dependent code).  If this breaks things then we'll have tio revert.  But fingers crossed it is not a problem; there are no uses of 1005 assuming other than 'X11' in a base Squeak image.
    Nuke spursistastacksrc; it's a misnomer.  Sista presupposes a Cogit.

--- platforms/Cross/plugins/SqueakFFIPrims/sqFFIPlugin.c
+++ platforms/Cross/plugins/SqueakFFIPrims/sqFFIPlugin.c
@@ -9,7 +9,7 @@
 
 #include <stdio.h>
 #include <stdlib.h> /* proto for alloca in MINGW */
-#if !WIN32 && !__FreeBSD__ && !__OpenBSD__
+#if !_WIN32 && !__FreeBSD__ && !__OpenBSD__
 # include <alloca.h>
 #endif
 #include <string.h>
