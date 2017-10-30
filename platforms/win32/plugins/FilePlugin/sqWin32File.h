f32c8bb4101ce232e3e47d6857d651828fb29a9b
Nicolas Cellier <nicolas.cellier.aka.nice@gmail.com>
2017-10-30 21:08:20 +0100

    Fix win32 long path name with a simpler solution a la Esteban
    Having two macros ALLOC_WIN32_PATH and REALLOC_WIN32_PATH that do almost the same thing smells.
    It is a lot simpler to concatenate native utf8 path and file names, then call ALLOC_WIN32_PATH once
    like Esteban Lorenzano did propose, rather than doing the work on converted WCHAR win32Path.
    
    Some more details:
    The concatenated fullPath does not require a trailing null char because MultiByteToWideChar() invoked by ALLOC_WIN32_PATH cares of it
    The lastString cache is now handled in utf8 too, without trailing null either- hence we don't use strcmp()

--- platforms/win32/plugins/FilePlugin/sqWin32File.h
+++ platforms/win32/plugins/FilePlugin/sqWin32File.h
@@ -38,32 +38,11 @@
     out_path[2] = L'?'; out_path[3] = L'\\'; \
     MultiByteToWideChar(CP_UTF8, 0, in_name, in_size, out_path + 4, sz); \
     out_path[sz + 4] = 0; \
-    sz += 4; \
   } else { \
     out_path = (WCHAR*)alloca((sz + 1) * sizeof(WCHAR)); \
     MultiByteToWideChar(CP_UTF8, 0, in_name, in_size, out_path, sz); \
     out_path[sz] = 0; \
   } \
-}
-
-//  if(wcscpy_s(in_out_wide_path, in_size < sz ? in_size : sz, tmp) != 0) FAIL(); \
-
-#define REALLOC_WIN32_PATH(in_out_wide_path, in_out_size) { \
-  int sz = wcslen(in_out_wide_path); \
-  WCHAR *tmp = in_out_wide_path; \
-  if(in_out_size >= 32767) FAIL(); \
-  if(in_out_size < sz) tmp[in_out_size] = 0; \
-  if(in_out_size >= MAX_PATH-12 && sz < MAX_PATH-12) { \
-    in_out_wide_path = (WCHAR*)alloca((in_out_size+4+1) * sizeof(WCHAR)); \
-    in_out_wide_path[0] = L'\\'; in_out_wide_path[1] = L'\\'; \
-    in_out_wide_path[2] = L'?'; in_out_wide_path[3] = L'\\'; \
-    wcscpy(in_out_wide_path+4, tmp); \
-    in_out_size += 4; \
-  } else { \
-    in_out_wide_path = (WCHAR*)alloca((in_out_size+1) * sizeof(WCHAR)); \
-    wcscpy(in_out_wide_path, tmp); \
-  } \
-  in_out_wide_path[in_out_size] = 0; \
 }
 
 #endif /* __SQ_WIN32_FILE_H */
