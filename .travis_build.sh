1cc7c55564ce121840a165f90bf9f29cb7947291
Fabio Niephaus <code@fniephaus.com>
2018-01-15 21:15:57 +0100

    Revert "Move files into products dir"
    This reverts commit ca1736480cd31e305bb682b775def5375c6df787.

--- .travis_build.sh
+++ .travis_build.sh
@@ -106,7 +106,8 @@ build_windows() {
     # We cannot zip dbg and ast if we pass -f to just to the full thing...
     # Once this builds, let's pass -A instead of -f and put the full zip (but we should do several zips in the future)
     bash -e ./mvm -f || exit 1
-    mv "./build/vm/"* "${PRODUCTS_DIR}"
+    rm -rf "${PRODUCTS_DIR}" || true # ensure there is no PRODUCTS_DIR
+    mv "./build/vm" "${PRODUCTS_DIR}"
     # zip -r "${output_file}.zip" "./builddbg/vm/" "./buildast/vm/" "./build/vm/"
     popd
 }
