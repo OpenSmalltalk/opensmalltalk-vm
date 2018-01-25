b67f810b5ee46167569bd7bfd3d43fbac438d1b5
Fabio Niephaus <code@fniephaus.com>
2018-01-25 17:55:34 +0100

    Fix copy and paste error

--- deploy/pack-vm.sh
+++ deploy/pack-vm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-readonly BUILD_DIR="${TRAVIS_BUILD_DIR:-${APPVEYOR_BUILD_FOLDER}}/products"
+readonly BUILD_DIR="${TRAVIS_BUILD_DIR:-${APPVEYOR_BUILD_FOLDER}}"
 readonly PRODUCTS_DIR="${BUILD_DIR}/products"
 if [[ ! -d "${PRODUCTS_DIR}" ]]; then
   echo "No products directory found."
