6d8a9392beb3a1c6e534cab7971d920a1804f6dc
Vincent Blondeau <vincent.blondeau@lamresearch.com>
2018-04-23 17:06:41 -0700

    code ordering

--- platforms/win32/plugins/FilePlugin/sqWin32FilePrims.c
+++ platforms/win32/plugins/FilePlugin/sqWin32FilePrims.c
@@ -31,51 +31,48 @@
 #include <windows.h>
 #include <malloc.h>
 
-#include <errno.h>
-#include <Winternl.h>
-
-
 #include <winbase.h>
 
-/************************************************************************************************************/
-/* few addtional definitions for those having older include files especially #include <fileextd.h>          */
-/************************************************************************************************************/
-#if (WINVER < 0x0600)
-	/*Copied from winbase.h*/
-	typedef struct _FILE_NAME_INFO {
-		DWORD FileNameLength;
-		WCHAR FileName[1];
-	} FILE_NAME_INFO, *PFILE_NAME_INFO;
-	typedef enum _FILE_INFO_BY_HANDLE_CLASS {
-		FileBasicInfo = 0,
-		FileStandardInfo = 1,
-		FileNameInfo = 2,
-		FileRenameInfo = 3,
-		FileDispositionInfo = 4,
-		FileAllocationInfo = 5,
-		FileEndOfFileInfo = 6,
-		FileStreamInfo = 7,
-		FileCompressionInfo = 8,
-		FileAttributeTagInfo = 9,
-		FileIdBothDirectoryInfo = 10, // 0xA
-		FileIdBothDirectoryRestartInfo = 11, // 0xB
-		FileIoPriorityHintInfo = 12, // 0xC
-		FileRemoteProtocolInfo = 13, // 0xD
-		FileFullDirectoryInfo = 14, // 0xE
-		FileFullDirectoryRestartInfo = 15, // 0xF
-		FileStorageInfo = 16, // 0x10
-		FileAlignmentInfo = 17, // 0x11
-		FileIdInfo = 18, // 0x12
-		FileIdExtdDirectoryInfo = 19, // 0x13
-		FileIdExtdDirectoryRestartInfo = 20, // 0x14
-		MaximumFileInfoByHandlesClass
-	} FILE_INFO_BY_HANDLE_CLASS, *PFILE_INFO_BY_HANDLE_CLASS;
-#endif //(WINVER < 0x0600)
 
 #include "sq.h"
 #include "FilePlugin.h"
 
 #include "sqWin32File.h"
+
+/************************************************************************************************************/
+/* few addtional definitions for those having older include files especially #include <fileextd.h>          */
+/************************************************************************************************************/
+#if (WINVER < 0x0600)
+/*Copied from winbase.h*/
+typedef struct _FILE_NAME_INFO {
+	DWORD FileNameLength;
+	WCHAR FileName[1];
+} FILE_NAME_INFO, *PFILE_NAME_INFO;
+typedef enum _FILE_INFO_BY_HANDLE_CLASS {
+	FileBasicInfo = 0,
+	FileStandardInfo = 1,
+	FileNameInfo = 2,
+	FileRenameInfo = 3,
+	FileDispositionInfo = 4,
+	FileAllocationInfo = 5,
+	FileEndOfFileInfo = 6,
+	FileStreamInfo = 7,
+	FileCompressionInfo = 8,
+	FileAttributeTagInfo = 9,
+	FileIdBothDirectoryInfo = 10, // 0xA
+	FileIdBothDirectoryRestartInfo = 11, // 0xB
+	FileIoPriorityHintInfo = 12, // 0xC
+	FileRemoteProtocolInfo = 13, // 0xD
+	FileFullDirectoryInfo = 14, // 0xE
+	FileFullDirectoryRestartInfo = 15, // 0xF
+	FileStorageInfo = 16, // 0x10
+	FileAlignmentInfo = 17, // 0x11
+	FileIdInfo = 18, // 0x12
+	FileIdExtdDirectoryInfo = 19, // 0x13
+	FileIdExtdDirectoryRestartInfo = 20, // 0x14
+	MaximumFileInfoByHandlesClass
+} FILE_INFO_BY_HANDLE_CLASS, *PFILE_INFO_BY_HANDLE_CLASS;
+#endif //(WINVER < 0x0600)
 
 extern struct VirtualMachine *interpreterProxy;
 
