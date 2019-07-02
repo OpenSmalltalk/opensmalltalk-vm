e06f0f83323e87a82d8cb2a8fcbc22cedf3c29f8
Eliot Miranda <eliot.miranda@gmail.com>
2019-07-01 17:45:40 -0700

    Fix SerialPlugin.h to agree with the EXPORT specs in sqNullSerialPort.c

--- platforms/Cross/plugins/SerialPlugin/SerialPlugin.h
+++ platforms/Cross/plugins/SerialPlugin/SerialPlugin.h
@@ -5,16 +5,17 @@ int serialPortInit(void);
 int serialPortShutdown(void);
 
 #pragma export on
-int serialPortClose(int portNum);
-int serialPortCloseByName(const char *portName);
-int serialPortCount(void);
-int serialPortOpen(int portNum, int baudRate, int stopBitsType, int parityType, int dataBits,
-		   int inFlowCtrl, int outFlowCtrl, int xOnChar, int xOffChar);
-int serialPortOpenByName(const char *portName, int baudRate, int stopBitsType,
+EXPORT (int) serialPortClose(int portNum);
+EXPORT (int) serialPortCloseByName(const char *portName);
+EXPORT (int) serialPortCount(void);
+EXPORT (int) serialPortOpen(int portNum, int baudRate, int stopBitsType,
+							int parityType, int dataBits, int inFlowCtrl,
+							int outFlowCtrl, int xOnChar, int xOffChar);
+EXPORT (int) serialPortOpenByName(const char *portName, int baudRate, int stopBitsType,
            int parityType, int dataBits,
 		   int inFlowCtrl, int outFlowCtrl, int xOnChar, int xOffChar);
-int serialPortReadInto(int portNum, int count, void *bufferPtr);
-int serialPortReadIntoByName(const char *portName, int count, void *bufferPtr);
-int serialPortWriteFrom(int portNum, int count, void *bufferPtr);
-int serialPortWriteFromByName(const char *portName, int count, void *bufferPtr);
+EXPORT (int) serialPortReadInto(int portNum, int count, void *bufferPtr);
+EXPORT (int) serialPortReadIntoByName(const char *portName, int count, void *bufferPtr);
+EXPORT (int) serialPortWriteFrom(int portNum, int count, void *bufferPtr);
+EXPORT (int) serialPortWriteFromByName(const char *portName, int count, void *bufferPtr);
 #pragma export off
