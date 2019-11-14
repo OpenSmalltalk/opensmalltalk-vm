91b7b98b5a93869b9e395ffb5fb0209d3c0127c0
Eliot Miranda <eliot.miranda@gmail.com>
2019-11-13 18:17:28 -0800

    Add a somewhat cut down version of the gdb 8.3.1 source tree that can provide ARMv8 simulation (and should be able to provide ARMv5,6,7 simulation too). Add library build dirctories for 64-bit linux & macos.  Add the source generation files to generate the processor simulator accessors.  The build has been tested on 64-bit MacOS (10.13.x) and 64-bit Linux (CentOS 6.5). The next step is to build a simulator plugin for "aarch64" a.k.a. ARMv8.

---
+++ processors/ARM/exploration64/printcpu.c
@@ -0,0 +1,98 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <time.h>
+
+#define COG 1
+#define FOR_COG_PLUGIN 1
+
+#include <config.h>
+#include <sim-main.h>
+
+static char buf[10];
+
+char *
+lower(char *s)
+{
+	int i;
+	for (i = 0; i < strlen(s); i++)
+		buf[i] = tolower(s[i]);
+	buf[i] = 0;
+	return buf;
+}
+
+#if FOR64BITS
+# define CLASS "!GdbARMAlien64"
+#else
+# define CLASS "!GdbARMAlien"
+#endif
+
+int
+main()
+{
+//#define offsetof(type,field) (long)(&(((type *)0)->field))
+#define stoffsetof(type,field) (offsetof(type,field)+1)
+#define print(n,r) \
+printf(CLASS " methodsFor: 'accessing' stamp: 'eem %d/%d/%d %d:%02d'!\r"\
+	"%s\r\t^self unsignedLongLongAt: %ld! !\r", m,d,y,h,i, lower(#r), \
+	stoffsetof(struct _sim_cpu,gr[n]));\
+printf(CLASS " methodsFor: 'accessing' stamp: 'eem %d/%d/%d %d:%02d'!\r"\
+	"%s: anUnsignedInteger\r\t^self unsignedLongLongAt: %ld put: anUnsignedInteger! !\r", m,d,y,h,i, lower(#r), \
+	stoffsetof(struct _sim_cpu,gr[n]))
+
+#define printpc(r) \
+printf(CLASS " methodsFor: 'accessing' stamp: 'eem %d/%d/%d %d:%02d'!\r"\
+	"%s\r\t^self unsignedLongLongAt: %ld! !\r", m,d,y,h,i, lower(#r), \
+	stoffsetof(struct _sim_cpu,pc));\
+printf(CLASS " methodsFor: 'accessing' stamp: 'eem %d/%d/%d %d:%02d'!\r"\
+	"%s: anUnsignedInteger\r\t^self unsignedLongLongAt: %ld put: anUnsignedInteger! !\r", m,d,y,h,i, lower(#r), \
+	stoffsetof(struct _sim_cpu,pc))
+
+	time_t nowsecs = time(0);
+	struct tm now = *localtime(&nowsecs);
+	int m = now.tm_mon + 1; /* strange but true */
+	int d = now.tm_mday;
+	int y = now.tm_year + 1900;
+	int h = now.tm_hour;
+	int i = now.tm_min;
+
+	printf("\"Hello world!!\"!\r");
+	printf(CLASS " class methodsFor: 'instance creation' stamp: 'eem %d/%d/%d %d:%02d'!\r"
+	"dataSize\r\t^%ld! !\r", m,d,y,h,i, sizeof(struct sim_state));
+
+	print(0,r0);
+	print(1,r1);
+	print(2,r2);
+	print(3,r3);
+	print(4,r4);
+	print(5,r5);
+	print(6,r6);
+	print(7,r7);
+	print(8,r8);
+	print(9,r9);
+	print(10,r10);
+	print(11,r11);
+	print(12,r12);
+	print(13,r13);
+	print(14,r14);
+	print(15,r15);
+	print(16,r16);
+	print(17,r17);
+	print(18,r18);
+	print(19,r19);
+	print(20,r20);
+	print(21,r21);
+	print(22,r22);
+	print(23,r23);
+	print(24,r24);
+	print(25,r25);
+	print(26,r26);
+	print(27,r27);
+	print(28,r28);
+	print(29,FP);
+	print(30,LR);
+	print(31,SP);
+	printpc(pc);
+
+	return 0;
+}
---
+++ processors/ARM/exploration64/printcpuctrl.c
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <time.h>
+
+#define COG 1
+#define FOR_COG_PLUGIN 1
+
+#include <config.h>
+#include <sim-main.h>
+
+static char buf[10];
+
+char *
+lower(char *s)
+{
+	int i;
+	for (i = 0; i < strlen(s); i++)
+		buf[i] = tolower(s[i]);
+	buf[i] = 0;
+	return buf;
+}
+
+#if FOR64BITS
+# define CLASS "!GdbARMAlien64"
+#else
+# define CLASS "!GdbARMAlien"
+#endif
+
+int
+main()
+{
+//#define offsetof(type,field) (long)(&(((type *)0)->field))
+#define stoffsetof(type,field) (offsetof(type,field)+1)
+#define print(f,s) \
+printf(CLASS " methodsFor: 'accessing' stamp: 'eem %d/%d/%d %d:%02d'!\r"\
+	"%s\r\t^self unsignedLongLongAt: %ld! !\r", m,d,y,h,i, #s, \
+	stoffsetof(struct _sim_cpu,f));\
+printf(CLASS " methodsFor: 'accessing' stamp: 'eem %d/%d/%d %d:%02d'!\r"\
+	"%s: anUnsignedInteger\r\t^self unsignedLongLongAt: %ld put: anUnsignedInteger! !\r", m,d,y,h,i, #s, \
+	stoffsetof(struct _sim_cpu,f))
+
+	time_t nowsecs = time(0);
+	struct tm now = *localtime(&nowsecs);
+	int m = now.tm_mon + 1; /* strange but true */
+	int d = now.tm_mday;
+	int y = now.tm_year + 1900;
+	int h = now.tm_hour;
+	int i = now.tm_min;
+
+	printf("\"Hello world!!\"!\r");
+	printf(CLASS " class methodsFor: 'instance creation' stamp: 'eem %d/%d/%d %d:%02d'!\r"
+	"dataSize\r\t^%ld! !\r", m,d,y,h,i, sizeof(struct _sim_cpu));
+
+	print(CPSR,rawCPSR);
+	print(FPSR,fpCPSR);
+	print(FPCR,fpCPCR);
+	print(nextpc,nextpc);
+
+	return 0;
+}
