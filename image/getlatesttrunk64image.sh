aa688da5c4dbe09a4d2e09e3adde54346ed5596f
Eliot Miranda <eliot.miranda@gmail.com>
2020-03-09 21:44:34 -0700

    CogVM source as per VMMaker.oscog-eem.2724
    Slang:
    Fix the right shift: dont convert to usqInt a type longer than usqInt
    
    SpurMemoryManager/SpurPlanningCompactor:
    Fix one bug thrown up by Marcel's EphemeronLink example.  The planning
    compactor failed to update objects in the mournQueue objStack.
    
    Make sure prepareObjStacksForPlanningCompactor is actually invoked (!!).
    
    Refactor a couple of obj stack routines to take an "also do contents" flag,
    printObjStack:printContents:, relocateObjStackForPlanningCompactor:andContents:,
    the last one fixing the bug in question.
    
    Refactor runLeakCheckerForFreeSpace: into runLeakCheckerForFreeSpace:ignoring:
    to allow the most recenty allocated object in clone (the shallowCopy primitive)
    to be ignored, because its contents are not yet initialized when the free space
    check is done.
    
    Make sure to run the free space integrity check as part of leak checking.
    
    VMMaker image:
    Add MULTIPLEBYTECODESETS true to all launch examples in the VM Simulation
    workspace.  Use Squeak 5.3 to build new VMMaker images.

--- image/getlatesttrunk64image.sh
+++ image/getlatesttrunk64image.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 . ./envvars.sh
 # curl clags -s silent -o output to
-URL=http://files.squeak.org/5.2/
-LATEST=`curl -s $URL | tr "=" "\\012" | grep Squeak5.2.*64bit | tail -1 | sed 's/"\(.*\)\/">.*$/\1/'`
+URL=http://files.squeak.org/5.3/
+LATEST=`curl -s $URL | tr "=" "\\012" | grep Squeak5.3.*64bit | tail -1 | sed 's/"\(.*\)\/">.*$/\1/'`
 echo curl -o $LATEST.zip $URL/$LATEST/$LATEST.zip
 curl -o $LATEST.zip $URL/$LATEST/$LATEST.zip
 unzip $LATEST.zip
