a9208a784aa3cae0b39d4c33bb912442e342b129
Eliot Miranda <eliot.miranda@gmail.com>
2020-04-01 15:15:57 -0700

    CogVM source as per VMMaker.oscog-eem.2737
    CoInterpreter: Improve on the new simpler invoke interpreter scheme by including
    the relevant return address so that the C stack looks properly connected after
    reentering interpret.  This depends on getReturnAddress being defined in C.
    Both MSVC and GCC (and those that support its builtins such as clang and ICC)
    provide builtins/intrinsics for accessing the return address, so we don't bother
    to implement it in the Cogit.
    
    The implementation of getReturnAddress is in sq.h, not sqPlatformSpecific.h to
    reduce duplication.  But if the current implementations in terms of MSVC or GCC
    instrinsics don't apply, one may override in sqPlatformSpecific.h.
    
    Windows builds:
    Make most of the windows build makefiles switch hit between the cygwin/mingw
    and the MSVC variants depending on a variable set by MSVC command prompts.
    Hence to build using MSVC simply run make in an MSVC prompt.

--- platforms/Cross/vm/sq.h
+++ platforms/Cross/vm/sq.h
@@ -226,6 +226,22 @@ sqInt sqGetFilenameFromString(char * aCharBuffer, char * aFilenameString, sqInt 
 
 #include "sqPlatformSpecific.h"
 
+/* getReturnAddress optionally defined here rather than in sqPlatformSpecific.h
+ * to reduce duplication. The GCC intrinics are provided by other compilers too.
+ */
+#if COGVM && !defined(getReturnAddress)
+# if _MSVC
+#	define getReturnAddress() _ReturnAddress()
+#	include <intrin.h>
+#	pragma intrinsic(_ReturnAddress)
+# elif defined(__GNUC__) /* gcc, clang, icc etc */
+#	define getReturnAddress() __builtin_extract_return_addr(__builtin_return_address(0))
+# else
+#	error "Cog requires getReturnAddress defining for the current platform."
+# endif
+#endif /* COG && !defined(getReturnAddress */
+
+
 /* Interpreter entry points. */
 
 /* Disable Intel compiler inlining of error which is used for breakpoints */
