810c4fea386bedbae4e077fd36c4667f0c787a6f
Eliot Miranda <eliot.miranda@gmail.com>
2020-04-20 21:25:39 -0700

    CogVM source as per VMMaker.oscog-eem.2742
    Cogit: mark the floating-point square root generators (the only non-arithmetic
    primitives) as notOption: #BIT_IDENTICAL_FLOATING_POINT in preparation for
    making it possioble to link the VM against Nicolas' revamp of fdlibm.  This is
    for Terf/Croquet.
    
    CoInterpreter: rename primitiveArrayBecomeOneWayCopyHash to
    primitiveArrayBecomeOneWayCopyHashArg to differentiate it from
    primitiveArrayBecomeOneWayNoCopyHash.

--- processors/IA32/bochs/cpu/cpu.cc
+++ processors/IA32/bochs/cpu/cpu.cc
@@ -144,6 +144,15 @@ void BX_CPU_C::cpu_loop(Bit32u max_instr_count)
 
 no_async_event:
 
+#if COG
+    bxInstruction_c iStorage, *i = &iStorage;
+# if BX_SUPPORT_X86_64
+	if (BX_CPU_THIS_PTR cpu_mode == BX_MODE_LONG_64)
+	  fetchDecode64((Bit8u *)(RIP + BX_CPU_THIS_PTR eipPageBias), i, 15);
+	else
+# endif
+	  fetchDecode32((Bit8u *)(EIP + BX_CPU_THIS_PTR eipPageBias), i, 15);
+#else
     Bit32u eipBiased = RIP + BX_CPU_THIS_PTR eipPageBias;
 
     if (eipBiased >= BX_CPU_THIS_PTR eipPageWindowSize) {
@@ -151,7 +160,7 @@ no_async_event:
       eipBiased = RIP + BX_CPU_THIS_PTR eipPageBias;
     }
 
-#if BX_SUPPORT_ICACHE
+# if BX_SUPPORT_ICACHE
     bx_phy_address pAddr = BX_CPU_THIS_PTR pAddrA20Page + eipBiased;
     bxICacheEntry_c *entry = BX_CPU_THIS_PTR iCache.get_entry(pAddr);
     bxInstruction_c *i = entry->i;
@@ -171,10 +180,11 @@ no_async_event:
       serveICacheMiss(entry, eipBiased, pAddr);
       i = entry->i;
     }
-#else
+# else
     bxInstruction_c iStorage, *i = &iStorage;
     fetchInstruction(i, eipBiased);
-#endif
+# endif // BX_SUPPORT_ICACHE
+#endif // COG
 
 #if BX_SUPPORT_TRACE_CACHE
     unsigned length = entry->ilen;
@@ -241,6 +251,15 @@ void BX_CPU_C::cpu_single_step()
   BX_CPU_THIS_PTR EXT = 0;
   BX_CPU_THIS_PTR errorno = 0;
 
+#if COG
+    bxInstruction_c iStorage, *i = &iStorage;
+# if BX_SUPPORT_X86_64
+	if (BX_CPU_THIS_PTR cpu_mode == BX_MODE_LONG_64)
+	  fetchDecode64((Bit8u *)(RIP + BX_CPU_THIS_PTR eipPageBias), i, 15);
+	else
+# endif
+	  fetchDecode32((Bit8u *)(EIP + BX_CPU_THIS_PTR eipPageBias), i, 15);
+#else
     Bit32u eipBiased = RIP + BX_CPU_THIS_PTR eipPageBias;
 
     if (eipBiased >= BX_CPU_THIS_PTR eipPageWindowSize) {
@@ -248,7 +267,7 @@ void BX_CPU_C::cpu_single_step()
       eipBiased = RIP + BX_CPU_THIS_PTR eipPageBias;
     }
 
-#if BX_SUPPORT_ICACHE
+# if BX_SUPPORT_ICACHE
     bx_phy_address pAddr = BX_CPU_THIS_PTR pAddrA20Page + eipBiased;
     bxICacheEntry_c *entry = BX_CPU_THIS_PTR iCache.get_entry(pAddr);
     bxInstruction_c *i = entry->i;
@@ -268,10 +287,11 @@ void BX_CPU_C::cpu_single_step()
       serveICacheMiss(entry, eipBiased, pAddr);
       i = entry->i;
     }
-#else // BX_SUPPORT_ICACHE
+# else // BX_SUPPORT_ICACHE
     bxInstruction_c iStorage, *i = &iStorage;
     fetchInstruction(i, eipBiased);
-#endif // BX_SUPPORT_ICACHE
+# endif // BX_SUPPORT_ICACHE
+#endif // COG
 
 #if BX_SUPPORT_TRACE_CACHE
 # error "COG assumes no BX_SUPPORT_TRACE_CACHE
@@ -679,6 +699,7 @@ unsigned BX_CPU_C::handleAsyncEvent(void)
 
 void BX_CPU_C::prefetch(void)
 {
+#if !COG
   bx_address laddr = BX_CPU_THIS_PTR get_laddr(BX_SEG_REG_CS, RIP);
   bx_phy_address pAddr;
   unsigned pageOffset = PAGE_OFFSET(laddr);
@@ -686,6 +707,7 @@ void BX_CPU_C::prefetch(void)
   // Calculate RIP at the beginning of the page.
   BX_CPU_THIS_PTR eipPageBias = pageOffset - RIP;
   BX_CPU_THIS_PTR eipPageWindowSize = 4096;
+#endif
 
 #if BX_SUPPORT_X86_64
   if (Is64BitMode()) {
