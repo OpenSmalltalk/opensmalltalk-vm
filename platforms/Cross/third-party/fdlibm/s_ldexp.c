eab1bf9a150bdeed596e731ecec54dfd0d6eee61
Eliot Miranda <eliot.miranda@gmail.com>
2020-04-22 14:42:17 -0700

    Use Nicolas' modernisation of fdlibm to provide cross-platform bit-identical floating-point (CPBIFP) in the standard VM, no longer being dependent on FloatMathPlugin, and hence not having to change image-level primitive definitions to access CPBIFP.  This is work in progress, but does not disturb normal builds.  To access this code you must define BIT_IDENTICAL_FLOATING_POINT in a Makefile.  e.g. here is a Mac makefile: --------8<--------
    BIT_IDENTICAL_FLOATING_POINT=BIT_IDENTICAL_FLOATING_POINT
    VMSRCDIR:= ../../spur64src/vm
    SOURCEFILE:=SqueakV50.sources
    
    include ../common/Makefile.app.squeak
    --------8<--------
    and here is a Windows makefile:
    --------8<--------
    
    VM:=CROQUET
    BIT_IDENTICAL_FLOATING_POINT:=BIT_IDENTICAL_FLOATING_POINT
    VMSRCDIR:=../../spur64src/vm
    
    VSCMD_ARG_HOST_ARCH := $(shell echo $$VSCMD_ARG_HOST_ARCH)
    ifeq ($(VSCMD_ARG_HOST_ARCH),)
    include ../common/Makefile
    else
    include ../common/Makefile.msvc
    endif
    --------8<--------
    Nothing has been done for linux yet, but it will soon; I have investment in the
    Pi 4.
    
    The strategy is as follows:
    Nicolas' updated fdlibm code is included in platforms/Cross/third-party/fdlibm.
    This code is built into a libm.a by each build alongside its internal plugin
    .libs, and linked into the main VM, iff the Makefile defines
    BIT_IDENTICAL_FLOATING_POINT.  [Nothing has yet been done for external plugins.
    We either need to construct a shared library/dll for libm for these, or link
    it statically as per the main VM.]  Since sq.h includes <math.h> it now also
    includes sqMathShim.h.  If BIT_IDENTICAL_FLOATING_POINT is defined as non-zero
    on the compiler command line, sqMathShim.h pulls in fdlibm.h and redefines all
    relevant math functions to __ieee754 duals, e.g.  # define acos __ieee754_acos.
    If BIT_IDENTICAL_FLOATING_POINT is not so defined sqMathShim.h does nothing.
    Hence, unless BIT_IDENTICAL_FLOATING_POINT is not defined in the Makefile the
    VM builds as before.

---
+++ platforms/Cross/third-party/fdlibm/s_ldexp.c
@@ -0,0 +1,22 @@
+/* @(#)s_ldexp.c 1.3 95/01/18 */
+/*
+ * ====================================================
+ * Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.
+ *
+ * Developed at SunSoft, a Sun Microsystems, Inc. business.
+ * Permission to use, copy, modify, and distribute this
+ * software is freely granted, provided that this notice 
+ * is preserved.
+ * ====================================================
+ */
+
+#include "fdlibm.h"
+#include <errno.h>
+
+double ldexp(double value, int exp)
+{
+        if(!finite(value)||value==0.0) return value;
+        value = scalbn(value,exp);
+        if(!finite(value)||value==0.0) errno = ERANGE;
+        return value;
+}
--- platforms/Cross/vm/sq.h
+++ platforms/Cross/vm/sq.h
@@ -16,6 +16,7 @@
 #include "sqConfig.h"
 
 #include <math.h>
+#include "sqMathShim.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -169,14 +170,14 @@ long ioMicroMSecs(void);
 
 #if STACKVM
 extern void forceInterruptCheckFromHeartbeat(void);
-unsigned volatile long long  ioUTCMicrosecondsNow();
-unsigned volatile long long  ioUTCMicroseconds();
-unsigned volatile long long  ioLocalMicrosecondsNow();
-unsigned volatile long long  ioLocalMicroseconds();
-unsigned          long long  ioUTCStartMicroseconds();
-sqInt	ioLocalSecondsOffset();
-void	ioUpdateVMTimezone();
-void	ioSynchronousCheckForEvents();
+unsigned volatile long long ioUTCMicrosecondsNow(void);
+unsigned volatile long long ioUTCMicroseconds(void);
+unsigned volatile long long ioLocalMicrosecondsNow(void);
+unsigned volatile long long ioLocalMicroseconds(void);
+unsigned          long long ioUTCStartMicroseconds(void);
+sqInt	ioLocalSecondsOffset(void);
+void	ioUpdateVMTimezone(void);
+void	ioSynchronousCheckForEvents(void);
 void	checkHighPriorityTickees(usqLong);
 # if ITIMER_HEARTBEAT		/* Hack; allow heartbeat to avoid */
 extern int numAsyncTickees; /* prodHighPriorityThread unless necessary */
@@ -302,7 +303,7 @@ sqInt ioIsWindowObscured(void);
 sqInt ioRelinquishProcessorForMicroseconds(sqInt microSeconds);
 #if STACKVM || NewspeakVM
 /* thread subsystem support for e.g. sqExternalSemaphores.c */
-void ioInitThreads();
+void ioInitThreads(void);
 
 /* Management of the external semaphore table (max size set at startup) */
 #if !defined(INITIAL_EXT_SEM_TABLE_SIZE)
@@ -352,7 +353,7 @@ extern char *thrlog[];
 	asprintf(thrlog + myidx, __VA_ARGS__); \
 } while (0)
 
-extern sqOSThread getVMOSThread();
+extern sqOSThread getVMOSThread(void);
 /* Please read the comment for CogThreadManager in the VMMaker package for
  * documentation of this API.  N.B. code is included from sqPlatformSpecific.h
  * before the code here.  e.g.
