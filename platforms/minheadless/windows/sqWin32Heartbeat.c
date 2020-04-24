OpenSmalltalk/opensmalltalk-vm

commit 9d52fef2240b7bdf6cfa1a7d6cf8c967220f0e85

Thu shalt not cast a FILETIME to unsigned __int64
pointer aliasing apart, such cast is explicitly discouraged in MSDN https://docs.microsoft.com/en-us/windows/win32/api/minwinbase/ns-minwinbase-filetime

> Do not cast a pointer to a FILETIME structure to either a ULARGE_INTEGER* or __int64* value because it can cause alignment faults on 64-bit Windows.

platforms/win32/vm/sqWin32Heartbeat.c
@@ -98,12 +98,13 @@ currentUTCMicroseconds(unsigned __int64 *utcTickBaseUsecsp, DWORD *lastTickp, DW
 	 * resync to the system time.  
 	 */
 	if (currentTick < prevTick) {
-
+		unsigned __int64 now;
 		*baseTickp = currentTick;
 		GetSystemTimeAsFileTime(&utcNow);
-		*utcTickBaseUsecsp = *(unsigned __int64 *)&utcNow
-							/ TocksPerMicrosecond
-							- MicrosecondsFrom1601To1901;
+		now = ((unsigned __int64) utcNow.dwHighDateTime) << 32 & utcNow.dwLowDateTime;
+		*utcTickBaseUsecsp = now
+					/ TocksPerMicrosecond
+					- MicrosecondsFrom1601To1901;
 		return *utcTickBaseUsecsp;
 	}
 	return *utcTickBaseUsecsp

commit 536a6b7b01361bafae3d057a1498b7dfd6aa82b7

Hot fix for issue #488 Win32 DateAndTime
I broke it in https://github.com/OpenSmalltalk/opensmalltalk-vm/commit/9d52fef2240b7bdf6cfa1a7d6cf8c967220f0e85
While at it, fix the 4 copies of the function (without an IDE, I lost quite some time inspecting the wrong one!).

platforms/minheadless/windows/sqWin32Heartbeat.c
@@ -98,12 +98,13 @@ currentUTCMicroseconds(unsigned __int64 *utcTickBaseUsecsp, DWORD *lastTickp, DW
 	 * resync to the system time.
 	 */
 	if (currentTick < prevTick) {
-
+		unsigned __int64 now;
 		*baseTickp = currentTick;
 		GetSystemTimeAsFileTime(&utcNow);
-		*utcTickBaseUsecsp = *(unsigned __int64 *)&utcNow
-							/ TocksPerMicrosecond
-							- MicrosecondsFrom1601To1901;
+		now = (unsigned __int64) utcNow.dwHighDateTime << 32 | utcNow.dwLowDateTime;
+		*utcTickBaseUsecsp = now
+					/ TocksPerMicrosecond
+					- MicrosecondsFrom1601To1901;
 		return *utcTickBaseUsecsp;
 	}
 	return *utcTickBaseUsecsp

platforms/minheadless/windows/sqWin32Time.c
@@ -97,10 +97,11 @@ currentUTCMicroseconds(unsigned __int64 *utcTickBaseUsecsp, DWORD *lastTickp, DW
 	 * resync to the system time.
 	 */
 	if (currentTick < prevTick) {
-
+		unsigned __int64 now;
 		*baseTickp = currentTick;
 		GetSystemTimeAsFileTime(&utcNow);
-		*utcTickBaseUsecsp = *(unsigned __int64 *)&utcNow
+		now = ((unsigned __int64) utcNow.dwHighDateTime) << 32 | utcNow.dwLowDateTime;
+		*utcTickBaseUsecsp = now
 							/ TocksPerMicrosecond
 							- MicrosecondsFrom1601To1901;
 		return *utcTickBaseUsecsp;

platforms/win32/vm/sqWin32Heartbeat.c
@@ -101,7 +101,7 @@ currentUTCMicroseconds(unsigned __int64 *utcTickBaseUsecsp, DWORD *lastTickp, DW
 		unsigned __int64 now;
 		*baseTickp = currentTick;
 		GetSystemTimeAsFileTime(&utcNow);
-		now = ((unsigned __int64) utcNow.dwHighDateTime) << 32 & utcNow.dwLowDateTime;
+		now = (unsigned __int64) utcNow.dwHighDateTime << 32 | utcNow.dwLowDateTime;
 		*utcTickBaseUsecsp = now
 					/ TocksPerMicrosecond
 					- MicrosecondsFrom1601To1901;

platforms/win32/vm/sqWin32Time.c
@@ -97,10 +97,11 @@ currentUTCMicroseconds(unsigned __int64 *utcTickBaseUsecsp, DWORD *lastTickp, DW
 	 * resync to the system time.  
 	 */
 	if (currentTick < prevTick) {
-
+		unsigned __int64 now;
 		*baseTickp = currentTick;
 		GetSystemTimeAsFileTime(&utcNow);
-		*utcTickBaseUsecsp = *(unsigned __int64 *)&utcNow
+		now = ((unsigned __int64) utcNow.dwHighDateTime) << 32 | utcNow.dwLowDateTime;
+		*utcTickBaseUsecsp = now
 							/ TocksPerMicrosecond
 							- MicrosecondsFrom1601To1901;
 		return *utcTickBaseUsecsp;