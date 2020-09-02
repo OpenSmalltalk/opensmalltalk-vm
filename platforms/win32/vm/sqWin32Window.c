6ba4b1294071796fe2f4d4d1c57a06aaa26d1484
LinqLover <christoph.thiede@student.hpi.de>
2020-09-02 18:12:23 +0200

    Use GetTickCount() instead of GetMessageTime().
    The latter would be imprecise and depend on the VM receiving real MSG messages at the same time.

--- platforms/win32/vm/sqWin32Window.c
+++ platforms/win32/vm/sqWin32Window.c
@@ -1269,7 +1269,7 @@ int recordDragDropEvent(HWND wnd, int dragType, int x, int y, int numFiles)
   /* first the basics */
   evt->type = EventTypeDragDropFiles;
   // Be consistent with tagMSG.time source which is recorded for mouse events
-  evt->timeStamp = GetMessageTime();
+  evt->timeStamp = GetTickCount();
   evt->dragType = dragType;
   evt->x = x;
   evt->y = y;
