536280cdbe8add995616eaa0c78424b95eb00f15
Eliot Miranda <eliot.miranda@gmail.com>
2020-09-14 13:52:51 -0700

    Fix 64-bit builds by addig the missing define to sqSetjmpShim.h and making sure that _setjmp-x64.asm/.o gets included in the VM and plugin dll builds. Make the same changes to the 32-bit builds, but leave them broken because we do not have a _setjmp-x86.asm yet.  Easily derived.

--- platforms/Cross/vm/sqSetjmpShim.h
+++ platforms/Cross/vm/sqSetjmpShim.h
@@ -39,5 +39,7 @@
 #endif
 /* clang on mingw redeclares _setjmp so we have to provide an alternative */
 #if __MINGW32__ || __MINGW64__ /* clang on cygwin/mingw */
-int __attribute__((__nowthrow__,__returns_twice__)) _setjmp0(jmp_buf);
+int __attribute__((__nothrow__,__returns_twice__)) _setjmp0(jmp_buf);
+# undef _setjmp
+# define _setjmp(b) _setjmp0(b)
 #endif
