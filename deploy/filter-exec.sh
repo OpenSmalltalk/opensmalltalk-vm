b622310eb8db5cf880cf52e753b42d42d3903493
Tobias Pape <tobias@netshed.de>
2020-09-17 15:37:20 +0200

    [deploy] Deploy this branch ONCE so we can try

--- deploy/filter-exec.sh
+++ deploy/filter-exec.sh
@@ -27,7 +27,7 @@ if [[ -n "${PR_SHA}" ]]; then
   exit
 fi
 
-if [[ "${BRANCH_NAME}" != "Cog" ]] && [[ -z "${TAG_NAME}" ]]; then
+if [[ "${BRANCH_NAME}" != "Cog" ]] && [[ -z "${TAG_NAME}" ]] && [[ "${BRANCH_NAME}" != "krono/highdpi-v2" ]]; then
   echo "Skipping a deployment with the script provider because this branch is not permitted."
   exit
 fi
