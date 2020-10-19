89efbcc16d4a6acdcfd4f7c90e568bbd31f6ab54
Eliot Miranda <eliot.miranda@gmail.com>
2020-10-19 12:08:03 -0700

    Chunk image read/writes into 1Gb maxima.  >4Gb image save/restore works (at least on macOS).

--- platforms/unix/vm/sqImageFileAccess.h
+++ platforms/unix/vm/sqImageFileAccess.h
@@ -24,14 +24,6 @@
 /* Read the image from the given file starting at the given image offset */
 size_t readImageFromFileHeapSizeStartingAt(sqImageFile f, usqInt desiredHeapSize, squeakFileOffsetType imageOffset);
 
-static inline void
-sqImageFileClose(sqImageFile f)
-{
-	/* Don't exit; if snapshotting, continuing is probably the best course */
-	if (close(f) < 0)
-		perror("close");
-}
-
 static inline sqImageFile
 sqImageFileOpen(const char *fileName, const char *mode)
 {
@@ -39,38 +31,79 @@ sqImageFileOpen(const char *fileName, const char *mode)
 				  !strcmp(mode,"rb") ? O_RDONLY : O_RDWR+O_CREAT,
 				  0666);
 	if (fd < 0) {
-		perror("open");
+		perror("sqImageFileOpen open");
 		exit(errno);
 	}
 	return fd;
 }
 
-// sqImageFileRead answers the number of items read, not number of bytes
-// size_t is for sizes.  ssize_t is for sizes + the -1 error flag
-static inline size_t
-sqImageFileRead(void *ptr, long sz, long count, sqImageFile f)
+static inline void
+sqImageFileClose(sqImageFile f)
 {
-	ssize_t nread = read(f, ptr, (size_t)(count * sz));
-	if (nread == (size_t)-1) {
-		perror("read");
-		exit(errno);
+	/* Don't exit; if snapshotting, continuing is probably the best course */
+	if (close(f) < 0)
+		perror("sqImageFileClose close");
+}
+
+#if !defined(min)
+# define min(a,b) ((a)<=(b)?(a):b)
+#endif
+#define OneGb 0x40000000 // hah,hah,hah,hah https://y2u.be/watch?v=EJR1H5tf5wE
+
+// sqImageFileRead answers the number of items read, not number of bytes
+// size_t is for sizes.  ssize_t is signed, for sizes + the -1 error flag
+static inline size_t
+sqImageFileRead(void *ptr_arg, long sz, long count, sqImageFile f)
+{
+	size_t to_be_read = sz * count, nread_in_total = 0;
+	unsigned char *ptr = ptr_arg;
+
+	/* read may refuse to write more than 2Gb-1.  At least on MacOS 10.13.6,
+	 * read craps out above 2Gb, so chunk the read into to 1Gb segments.
+	 */
+	do {
+		ssize_t n = read(f, ptr, min(to_be_read-nread_in_total,OneGb));
+
+		// Don't exit!!; if snapshotting, we obviously must continue
+		if (n == (size_t)-1) {
+			perror("sqImageFileRead read");
+			return nread_in_total;
+		}
+		nread_in_total += n;
+		ptr += n;
 	}
-	return nread / sz;
+	while (nread_in_total < to_be_read);
+
+	return nread_in_total / sz;
 }
 
 // sqImageFileWrite answers the number of items written, not number of bytes
-// size_t is for sizes.  ssize_t is for sizes + the -1 error flag
+// size_t is for sizes.  ssize_t is signed, for sizes + the -1 error flag
 static inline size_t
-sqImageFileWrite(void *ptr, long sz, long count, sqImageFile f)
+sqImageFileWrite(void *ptr_arg, size_t sz, size_t count, sqImageFile f)
 {
-	ssize_t nwritten = write(f, ptr, (size_t)(count * sz));
+	size_t to_be_written = sz * count, nwritten_in_total = 0;
+	unsigned char *ptr = ptr_arg;
 
-	/* Don't exit; if snapshotting, continuing is probably the best course */
-	if (nwritten == (size_t)-1)
-		perror("write");
+	/* write may refuse to write more than 2Gb-1.  At least on MacOS 10.13.6,
+	 * write craps out above 2Gb, so chunk the write into to 1Gb segments.
+	 */
+	do {
+		ssize_t n = write(f, ptr, min(to_be_written-nwritten_in_total,OneGb));
 
-	return nwritten / sz;
+		// Don't exit!!; if snapshotting, we obviously must continue
+		if (n == (size_t)-1) {
+			perror("sqImageFileWrite write");
+			return nwritten_in_total;
+		}
+		nwritten_in_total += n;
+		ptr += n;
+	}
+	while (nwritten_in_total < to_be_written);
+
+	return nwritten_in_total / sz;
 }
+#undef OneGb
 
 static inline off_t
 sqImageFilePosition(sqImageFile f)
