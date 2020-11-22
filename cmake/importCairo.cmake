9158f811fbc426625308b3cf79066758a0b01291
Guille Polito <guillermopolito@gmail.com>
2020-11-22 22:08:13 +0100

    Put back cairo download

--- cmake/importCairo.cmake
+++ cmake/importCairo.cmake
@@ -7,15 +7,10 @@ elseif (NOT WITHOUT_DEPENDENCIES)
 	include(cmake/DownloadProject.cmake)
 
 	# Cairo does not support building on CMake
-	# In Win32
-
-	if(WIN)
-		#	add_third_party_dependency("cairo-1.15.4" "build/vm")
-		#	add_third_party_dependency("libpixman-0.38.4" "build/vm")
-		#	add_third_party_dependency("libpng-1.6.34" "build/vm")
-	else()
-		message(FATAL_ERROR "No Cairo support specified for host ${CMAKE_HOST_SYSTEM_NAME}")
-	endif()
+  # Download it for now
+  add_third_party_dependency("cairo-1.15.4" "build/vm")
+  add_third_party_dependency("libpixman-0.38.4" "build/vm")
+  add_third_party_dependency("libpng-1.6.34" "build/vm")
 else()
 	message(FATAL_ERROR "Could not find Cairo")
 endif()
