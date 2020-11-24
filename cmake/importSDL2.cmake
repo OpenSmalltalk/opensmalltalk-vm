d8d6d2cfe037228ce322d7aa0ad4a1ec95c5c440
Guille Polito <guillermopolito@gmail.com>
2020-11-24 17:06:36 +0100

    make SDL2 to be able to prefer downloads

--- cmake/importSDL2.cmake
+++ cmake/importSDL2.cmake
@@ -1,20 +1,20 @@
-find_package(SDL2)
+function(find_system_SDL2)
+    message(STATUS "Looking for SDL2 in the system")
+    find_package(SDL2)
+    if(NOT SDL2_FOUND)
+      message(STATUS "SDL2 not found.")
+    endif()
+    set(SDL2_FOUND ${SDL2_FOUND} PARENT_SCOPE)
+endfunction()
 
-if (SDL2_FOUND)
-	# Do nothing! We have it!
-elseif (NOT WITHOUT_DEPENDENCIES)
+function(download_SDL2)
+  message(STATUS "Downloading SDL2 binary")
+  add_third_party_dependency("SDL2-2.0.5" "build/vm")
+endfunction()
 
-	include(cmake/DownloadProject.cmake)
-
-	if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-  	set(SHOULD_BUILD_SDL FALSE)
-	else()
-  	set(SHOULD_BUILD_SDL TRUE)
-	endif()
-
-	if (SHOULD_BUILD_SDL)
-    message(STATUS "SDL2 not found.")
+function(build_SDL2)
     message(STATUS "Building SDL2")
+  	include(cmake/DownloadProject.cmake)
     download_project(PROJ SDL2
         URL         https://libsdl.org/release/SDL2-2.0.12.tar.gz
         URL_HASH    MD5=783b6f2df8ff02b19bb5ce492b99c8ff
@@ -23,9 +23,31 @@ elseif (NOT WITHOUT_DEPENDENCIES)
     add_subdirectory(${SDL2_SOURCE_DIR} ${SDL2_BINARY_DIR} EXCLUDE_FROM_ALL)
 
     set_target_properties(SDL2 PROPERTIES LIBRARY_OUTPUT_DIRECTORY ${EXECUTABLE_OUTPUT_PATH})
-	endif()
+    add_dependencies(${VM_LIBRARY_NAME} SDL2)
+    set(SDL2_FOUND "From build_SDL2" PARENT_SCOPE)
+endfunction()
 
-	add_dependencies(${VM_LIBRARY_NAME} SDL2)
+if (NOT WITHOUT_DEPENDENCIES)
+  #Only get SDL2 if required
+  if(PHARO_DEPENDENCIES_PREFER_DOWNLOAD_BINARIES)
+    #Download SDL2 binaries directly
+    download_SDL2()
+  else()
+    #Look for SDL2 in the system, then build or download if possible
+    find_system_SDL2()
+
+    if(NOT SDL2_FOUND)
+      # Only build SDL if we are in MSVC, otherwise, download
+      if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+        set(SHOULD_BUILD_SDL FALSE)
+      else()
+        set(SHOULD_BUILD_SDL TRUE)
+      endif()
+      if (SHOULD_BUILD_SDL)
+        build_SDL2()
+      else()
+        download_SDL2()
+      endif()
+    endif()
+  endif()
 endif()
-
-#add_third_party_dependency("SDL2-2.0.5" "build/vm")
