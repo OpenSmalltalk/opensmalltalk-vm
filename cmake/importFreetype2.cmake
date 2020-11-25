56208529d60c6b09ba15c500873e8406e18264d5
Guille Polito <guillermopolito@gmail.com>
2020-11-25 09:33:29 +0100

    Same build pattern for freetype

--- cmake/importFreetype2.cmake
+++ cmake/importFreetype2.cmake
@@ -1,39 +1,59 @@
-find_package(Freetype)
-
-if (Freetype_FOUND)
+function(find_system_Freetype)
+  message(STATUS "Looking for Freetype in the system")
+  find_package(Freetype)
+  if(NOT Freetype_FOUND)
+    message(STATUS "Freetype not found.")
+  endif()
+  set(Freetype_FOUND ${Freetype_FOUND} PARENT_SCOPE)
 	add_dependencies(${VM_LIBRARY_NAME} Freetype::Freetype)
-elseif (NOT WITHOUT_DEPENDENCIES)
-	message(STATUS "Freetype not found in the system")
+endfunction()
 
-    message(STATUS "Building Freetype")
+function(download_Freetype)
+  if (NOT UNIX)
+    # Download it for now, except for linuxes
+    add_third_party_dependency("freetype-2.9.1" "build/vm")
+  endif()
+endfunction()
 
-    include(cmake/DownloadProject.cmake)
+function(build_Freetype)
+  message(STATUS "Building Freetype")
 
-    download_project(PROJ                freetype
-                 URL      https://download.savannah.gnu.org/releases/freetype/freetype-2.10.0.tar.gz
-                 ${UPDATE_DISCONNECTED_IF_AVAILABLE}
-    )
+  include(cmake/DownloadProject.cmake)
 
-    set(DISABLE_FORCE_DEBUG_POSTFIX ON)
+  download_project(PROJ                freetype
+               URL      https://download.savannah.gnu.org/releases/freetype/freetype-2.10.0.tar.gz
+               ${UPDATE_DISCONNECTED_IF_AVAILABLE}
+  )
 
-    # Store the old value of the 'BUILD_SHARED_LIBS'
-    set(BUILD_SHARED_LIBS_OLD ${BUILD_SHARED_LIBS})
-    # Make subproject to use 'BUILD_SHARED_LIBS=ON' setting.
-    set(BUILD_SHARED_LIBS ON CACHE INTERNAL "Build SHARED libraries")
+  set(DISABLE_FORCE_DEBUG_POSTFIX ON)
 
-    add_subdirectory(${freetype_SOURCE_DIR} ${freetype_BINARY_DIR} EXCLUDE_FROM_ALL)
+  # Store the old value of the 'BUILD_SHARED_LIBS'
+  set(BUILD_SHARED_LIBS_OLD ${BUILD_SHARED_LIBS})
+  # Make subproject to use 'BUILD_SHARED_LIBS=ON' setting.
+  set(BUILD_SHARED_LIBS ON CACHE INTERNAL "Build SHARED libraries")
 
-    # Restore the old value of the parameter
-    set(BUILD_SHARED_LIBS ${BUILD_SHARED_LIBS_OLD} CACHE BOOL "Type of libraries to build" FORCE)
-    
-    #set_target_properties(${NAME} PROPERTIES MACOSX_RPATH ON)
-    set_target_properties(freetype PROPERTIES LIBRARY_OUTPUT_DIRECTORY ${EXECUTABLE_OUTPUT_PATH})
-    #set_target_properties(${NAME} PROPERTIES INSTALL_NAME_DIR "@executable_path/Plugins")
+  add_subdirectory(${freetype_SOURCE_DIR} ${freetype_BINARY_DIR} EXCLUDE_FROM_ALL)
 
-	add_dependencies(${VM_LIBRARY_NAME} freetype)
-else()
- 	message(FATAL_ERROR "Freetype not found")
+  # Restore the old value of the parameter
+  set(BUILD_SHARED_LIBS ${BUILD_SHARED_LIBS_OLD} CACHE BOOL "Type of libraries to build" FORCE)
+  
+  #set_target_properties(${NAME} PROPERTIES MACOSX_RPATH ON)
+  set_target_properties(freetype PROPERTIES LIBRARY_OUTPUT_DIRECTORY ${EXECUTABLE_OUTPUT_PATH})
+  #set_target_properties(${NAME} PROPERTIES INSTALL_NAME_DIR "@executable_path/Plugins")
+
+  add_dependencies(${VM_LIBRARY_NAME} freetype)
+endfunction()
+
+if (NOT WITHOUT_DEPENDENCIES)
+  #Only get Freetype if required
+  if(PHARO_DEPENDENCIES_PREFER_DOWNLOAD_BINARIES)
+    #Download Freetype binaries directly
+    download_Freetype()
+  else()
+    #Look for SDL2 in the system, then build or download if possible
+    find_system_Freetype()
+    if(NOT Freetype_FOUND)
+        build_Freetype_FOUND()
+    endif()
+  endif()
 endif()
-
-
-# add_third_party_dependency("freetype-2.9.1" "build/vm")
