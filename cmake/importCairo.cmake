2943bec25b090d187fece62f794d215d553240fe
Guille Polito <guillermopolito@gmail.com>
2020-11-25 10:15:10 +0100

    use different version of pixman

--- cmake/importCairo.cmake
+++ cmake/importCairo.cmake
@@ -4,14 +4,15 @@ if (Cairo_FOUND)
 
 elseif (NOT WITHOUT_DEPENDENCIES)
 
-  if (NOT UNIX)
-    # Cairo does not support building on CMake
-    # Download it for now, except for linuxes
-    add_third_party_dependency("cairo-1.15.4" "build/vm")
+  # Cairo does not support building on CMake
+  # Download it for now, except for linuxes
+  if (OSX)
     add_third_party_dependency("libpixman-0.38.4" "build/vm")
-    add_third_party_dependency("libpng-1.6.34" "build/vm")
+  elseif(WIN)
+    add_third_party_dependency("pixman-0.34.0" "build/vm")
   endif()
-
+  add_third_party_dependency("cairo-1.15.4" "build/vm")
+  add_third_party_dependency("libpng-1.6.34" "build/vm")
 else()
 	message(FATAL_ERROR "Could not find Cairo")
 endif()
