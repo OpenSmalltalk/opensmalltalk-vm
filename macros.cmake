3f6e2caf7ba12cc1c3a4ca6879b426e1c764b3a5
Guille Polito <guillermopolito@gmail.com>
2020-12-15 15:51:33 +0100

    Cut addLibraryWithRPATH with addIndependentLibraryWithRPATH

--- macros.cmake
+++ macros.cmake
@@ -1,4 +1,18 @@
+# Include a library in the project, linking it to the main library
 macro(addLibraryWithRPATH NAME)
+    addIndependentLibraryWithRPATH(${NAME} ${ARGN})
+
+    # Declare the main executable depends on the plugin so it gets built with it
+    add_dependencies(${VM_EXECUTABLE_NAME} ${NAME})
+
+    #Declare the plugin depends on the VM core library
+    if(NOT "${NAME}" STREQUAL "${VM_LIBRARY_NAME}")
+        target_link_libraries(${NAME} ${VM_LIBRARY_NAME})
+    endif()
+endmacro()
+
+# Include a loose-dependency library in the project, but do not link it to the main library
+macro(addIndependentLibraryWithRPATH NAME)
     SET(CMAKE_BUILD_WITH_INSTALL_RPATH TRUE)
     set(CMAKE_INSTALL_RPATH "@executable_path/Plugins")
 
@@ -6,16 +20,7 @@ macro(addLibraryWithRPATH NAME)
     set_target_properties(${NAME} PROPERTIES MACOSX_RPATH ON)
     set_target_properties(${NAME} PROPERTIES LIBRARY_OUTPUT_DIRECTORY ${LIBRARY_OUTPUT_DIRECTORY})
     set_target_properties(${NAME} PROPERTIES INSTALL_NAME_DIR "@executable_path/Plugins")
-
-    # Declare the main executable depends on the plugin so it gets built with it
-    add_dependencies(${VM_EXECUTABLE_NAME} ${NAME})
-    #target_link_libraries(${VM_EXECUTABLE_NAME} ${NAME})
-    #Declare the plugin depends on the VM core library
-    if(NOT "${NAME}" STREQUAL "${VM_LIBRARY_NAME}")
-        target_link_libraries(${NAME} ${VM_LIBRARY_NAME})
-    endif()
 endmacro()
-
 
 macro(get_platform_name VARNAME)
     set(${VARNAME} ${CMAKE_SYSTEM_NAME}-${CMAKE_SYSTEM_PROCESSOR})
@@ -55,7 +60,7 @@ macro(do_decompress_thirdparty NAME TARGETPATH)
 endmacro()
 
 # Add a third party dependency taken from the given URL
-macro(add_third_party_dependency_with_baseurl NAME TARGETPATH BASEURL)
+macro(add_third_party_dependency_with_baseurl NAME BASEURL)
 
     get_platform_name(PLATNAME)
     message("Adding third-party libraries for ${PLATNAME}: ${NAME}")
@@ -68,14 +73,14 @@ macro(add_third_party_dependency_with_baseurl NAME TARGETPATH BASEURL)
     file(GLOB DOWNLOADED_THIRD_PARTY_LIBRARIES
         "${${NAME}_SOURCE_DIR}/*"
     )
+
     add_custom_target(${NAME}
-        COMMAND ${CMAKE_COMMAND} -E copy ${DOWNLOADED_THIRD_PARTY_LIBRARIES} "${LIBRARY_OUTPUT_PATH}/"
-        COMMENT "Copying ${NAME} binaries from '${${NAME}_SOURCE_DIR}' to '${LIBRARY_OUTPUT_PATH}/'" VERBATIM)
-    add_dependencies(${VM_LIBRARY_NAME} ${NAME})
+        COMMAND ${CMAKE_COMMAND} -E copy ${DOWNLOADED_THIRD_PARTY_LIBRARIES} "$<TARGET_FILE_DIR:${VM_LIBRARY_NAME}>"
+    )
 endmacro()
 
 # Add a third party dependency taken from the files.pharo.org repository
-macro(add_third_party_dependency NAME TARGETPATH)
+macro(add_third_party_dependency NAME)
     if(SIZEOF_VOID_P EQUAL 8)
         set(ARCH 64)
     else()
@@ -83,23 +88,7 @@ macro(add_third_party_dependency NAME TARGETPATH)
     endif()
     get_platform_name(PLATNAME)
     set(BASE_URL "https://files.pharo.org/vm/pharo-spur${ARCH}/${PLATNAME}/third-party/")
-    add_third_party_dependency_with_baseurl(${NAME} ${TARGETPATH} ${BASE_URL})
-endmacro()
-
-macro(add_third_party_dependency_from_jenkins LIBNAME TARGETPATH JOB BRANCH VERSION)
-    get_full_platform_name_with_osx(PLATNAME)
-
-	set(NAME ${LIBNAME}-${VERSION}-${PLATNAME})
-	set(URL "https://ci.inria.fr/pharo-ci-jenkins2/job/${JOB}/job/${BRANCH}/lastSuccessfulBuild/artifact/build/packages/${NAME}.zip")
-
-    message("Adding third-party libraries for ${PLATNAME} from Jenkins: ${NAME} (at ${URL})")
-    
-    add_custom_command(OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/build/third-party/${NAME}.zip"
-        COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_CURRENT_BINARY_DIR}/build/third-party
-        COMMAND ${CMAKE_COMMAND} -E chdir ${CMAKE_CURRENT_BINARY_DIR}/build/third-party wget --no-check-certificate "${URL}"
-        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
-
-	do_decompress_thirdparty(${NAME} ${TARGETPATH})
+    add_third_party_dependency_with_baseurl(${NAME} ${BASE_URL})
 endmacro()
 
 macro(get_commit_hash VARNAME)
