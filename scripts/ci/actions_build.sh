ffb2ac63276cf817f5726660ba501ab821cdf4cd
Marcel Taeumel <marcel.taeumel@hpi.de>
2021-07-27 09:34:40 +0200

    Adds latest work on actions for continuous integration.

---
+++ scripts/ci/actions_build.sh
@@ -0,0 +1,164 @@
+#!/bin/bash
+# Uses:
+# - ARCH (e.g., "macos64x64")
+# - FLAVOR (e.g., "squeak.cog.spur")
+# - RUNNER_OS (i.e., "Linux", "macOS", "Windows")
+# - HEARTBEAT (i.e., "threaded" or "itimer"; !! Linux only !!)
+# Provides:
+# - ASSET_REVISION (e.g., "202107261048")
+# - ASSET_NAME (e.g., "squeak.cog.spur_macos64x64")
+# - BUILD_PATH (e.g., "build.macos64x64/squeak.cog.spur")
+# - PRODUCTS_PATH (e.g., "products")
+# - APP_NAME (e.g., "vm" or "sqcogspur64linuxht" or "Squeak.app")
+
+set -e
+
+if [[ "${RUNNER_OS}" == "Windows" ]]; then
+    source ./scripts/ci/actions_prepare_msys.sh
+fi
+
+echo "$(cat platforms/Cross/vm/sqSCCSVersion.h | .git_filters/RevDateURL.smudge)" > platforms/Cross/vm/sqSCCSVersion.h
+echo "$(cat platforms/Cross/plugins/sqPluginsSCCSVersion.h | .git_filters/RevDateURL.smudge)" > platforms/Cross/plugins/sqPluginsSCCSVersion.h
+
+[[ -z "${ARCH}" ]] && exit 2
+[[ -z "${FLAVOR}" ]] && exit 3
+
+readonly ASSET_REVISION=$(grep -m1 "SvnRawRevisionString" "platforms/Cross/vm/sqSCCSVersion.h" | sed 's/[^0-9.]*\([0-9.]*\).*/\1/')
+
+ASSET_NAME="${FLAVOR}_${ARCH}"
+BUILD_PATH="$(pwd)/build.${ARCH}/${FLAVOR}"
+
+PRODUCTS_PATH="$(pwd)/products"
+mkdir "${PRODUCTS_PATH}" || true # ensure PRODUCTS_PATH exists
+
+check_buildPath() {
+    if [[ ! -d "${BUILD_PATH}" ]]; then
+        echo "Build path does not exist: ${BUILD_PATH}"
+        exit 11
+    fi 
+}
+
+skip_BochsPlugins() {
+    echo "Skipping Bochs plugins..."
+    sed -i 's/Bochs.* //g' plugins.ext
+    sed -i 's/Bochs.* //g' plugins.int
+}
+
+# export COGVREV="$(git describe --tags --always)"
+# export COGVDATE="$(git show -s --format=%cd HEAD)"
+# export COGVURL="$(git config --get remote.origin.url)"
+# export COGVOPTS="-DCOGVREV=\"${COGVREV}\" -DCOGVDATE=\"${COGVDATE// /_}\" -DCOGVURL=\"${COGVURL//\//\\\/}\""
+
+build_Linux() {
+    echo '::group::Running "make configure" in platforms/unix/config ...'
+    (cd platforms/unix/config/ && make configure)
+    echo '::endgroup::'
+
+    BUILD_PATH="${BUILD_PATH}/build"
+    if [[ "${MODE}" == "debug" ]]; then
+        BUILD_PATH="${BUILD_PATH}.debug"
+        PRODUCTS_PATH="${PRODUCTS_PATH}/debug"
+    elif [[ "${MODE}" == "assert" ]]; then
+        BUILD_PATH="${BUILD_PATH}.assert"
+        PRODUCTS_PATH="${PRODUCTS_PATH}/assert"
+    fi
+
+    if [[ ! -z "$HEARTBEAT" ]] && [[ "${HEARTBEAT}" != "threaded" ]]; then
+        BUILD_PATH="${BUILD_PATH}.itimerheartbeat"
+        ASSET_NAME="${ASSET_NAME}_itimer"
+    fi
+
+    pushd "${BUILD_PATH}"
+
+    echo "::group::Building ${BUILD_PATH}..."
+    echo n | bash -e ./mvm || exit 1
+    echo "::endgroup::"
+
+    cd "${PRODUCTS_PATH}"
+    readonly APP_NAME=$(find * -type d | head -n 1)
+
+    popd
+}
+
+build_macOS() {
+    check_buildPath
+
+    pushd "${BUILD_PATH}"
+
+    echo "::group::Building ${BUILD_PATH}..."
+    if [[ "${MODE}" == "debug" ]]; then
+        bash -e ./mvm -d || exit 1
+    elif [[ "${MODE}" == "assert" ]]; then
+        bash -e ./mvm -a || exit 1
+    else
+        bash -e ./mvm -f || exit 1
+    fi
+    echo "::endgroup::"
+
+    echo "::group::Moving build artifacts to ${PRODUCTS_PATH}..."
+    if [[ "${FLAVOR}" == "newspeak"* ]]; then
+        # TODO: Why does the Newspeak flavor have to wrong name? Should be set in its build scripts (or Makefile ...)
+        mv ./*.app "${PRODUCTS_PATH}/Newspeak.app"
+    else
+        mv ./*.app "${PRODUCTS_PATH}/"
+    fi
+    echo "::endgroup::"
+
+    cd "${PRODUCTS_PATH}"
+    readonly APP_NAME=$(find * -type d -path "*.app" | head -n 1)
+
+    popd
+}
+
+build_Windows() {
+    check_buildPath
+    pushd "${BUILD_PATH}"
+
+    echo "::group::Building ${BUILD_PATH}..."
+    skip_BochsPlugins
+    if [[ "${MODE}" == "debug" ]]; then
+        bash -e ./mvm -d || exit 1
+    elif [[ "${MODE}" == "assert" ]]; then
+        bash -e ./mvm -a || exit 1
+    else
+        bash -e ./mvm -f || exit 1
+    fi
+    echo "::endgroup::"
+
+    echo "::group::Moving build artifacts to ${PRODUCTS_PATH}..."
+    if [[ "${MODE}" == "debug" ]]; then
+        mv "./builddbg/vm" "${PRODUCTS_PATH}/"
+    elif [[ "${MODE}" == "assert" ]]; then
+        mv "./buildast/vm" "${PRODUCTS_PATH}/"
+    else
+        mv "./build/vm" "${PRODUCTS_PATH}/"
+    fi
+    echo "::endgroup::"
+
+    readonly APP_NAME="vm"
+
+    popd
+}
+
+if [[ ! $(type -t build_$RUNNER_OS) ]]; then
+    echo "Unsupported runner OS ${RUNNER_OS}."
+    exit 99
+fi
+
+build_$RUNNER_OS
+
+if [[ "${MODE}" == "debug" ]]; then
+    ASSET_NAME="${ASSET_NAME}_debug"
+elif [[ "${MODE}" == "assert" ]]; then
+    ASSET_NAME="${ASSET_NAME}_assert"
+fi
+
+echo "ASSET_REVISION=${ASSET_REVISION}" >> $GITHUB_ENV
+echo "ASSET_NAME=${ASSET_NAME}" >> $GITHUB_ENV
+
+[[ ! -d "${BUILD_PATH}" ]] && exit 12
+echo "BUILD_PATH=${BUILD_PATH}" >> $GITHUB_ENV
+[[ ! -d "${PRODUCTS_PATH}" ]] && exit 13
+echo "PRODUCTS_PATH=${PRODUCTS_PATH}" >> $GITHUB_ENV
+[[ ! -d "${PRODUCTS_PATH}/${APP_NAME}" ]] && exit 14
+echo "APP_NAME=${APP_NAME}" >> $GITHUB_ENV
---
+++ scripts/ci/actions_prepare_msys.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+#   Prepare MSYS environment so that all build tools have the $(TOOLPREFIX) as
+#   expected in Makefile.tools:
+#      - win64x64\common\Makefile.tools
+#      - win32x86\common\Makefile.tools
+#   Also, extend $PATH since MSYS is installed but not conveniently accessible
+#   in a GitHub-Actions Windows environment.
+
+[[ -z "${MSYS_SYS}" ]] && exit 2
+[[ -z "${MSYS_ENV}" ]] && exit 2
+
+ln -f -s /c/msys64/${MSYS_SYS}/bin/clang /c/msys64/${MSYS_SYS}/bin/${MSYS_ENV}-w64-mingw32-clang
+ln -f -s /c/msys64/${MSYS_SYS}/bin/clang++ /c/msys64/${MSYS_SYS}/bin/${MSYS_ENV}-w64-mingw32-clang++
+ln -f -s /c/msys64/${MSYS_SYS}/bin/ar /c/msys64/${MSYS_SYS}/bin/${MSYS_ENV}-w64-mingw32-ar
+ln -f -s /c/msys64/${MSYS_SYS}/bin/dlltool /c/msys64/${MSYS_SYS}/bin/${MSYS_ENV}-w64-mingw32-dlltool
+ln -f -s /c/msys64/${MSYS_SYS}/bin/as /c/msys64/${MSYS_SYS}/bin/${MSYS_ENV}-w64-mingw32-as
+ln -f -s /c/msys64/${MSYS_SYS}/bin/windres /c/msys64/${MSYS_SYS}/bin/${MSYS_ENV}-w64-mingw32-windres
+ln -f -s /c/msys64/${MSYS_SYS}/bin/nm /c/msys64/${MSYS_SYS}/bin/${MSYS_ENV}-w64-mingw32-nm
+ln -f -s /c/msys64/${MSYS_SYS}/bin/dllwrap /c/msys64/${MSYS_SYS}/bin/${MSYS_ENV}-w64-mingw32-dllwrap
+ln -f -s /c/msys64/${MSYS_SYS}/bin/strip /c/msys64/${MSYS_SYS}/bin/${MSYS_ENV}-w64-mingw32-strip
+ln -f -s /c/msys64/${MSYS_SYS}/bin/objcopy /c/msys64/${MSYS_SYS}/bin/${MSYS_ENV}-w64-mingw32-objcopy
+
+# Add MinGW tools for build scripts
+PATH=$PATH:/c/msys64/${MSYS_SYS}/bin
+
+# Add other GNU tools (e.g., wget) for third-party build scripts
+PATH=$PATH:/c/msys64/usr/bin
