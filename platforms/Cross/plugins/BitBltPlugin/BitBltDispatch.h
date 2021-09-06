70cd4bd06d8eb27c66946abf13e72586399eb83a
Tim Rowledge <tim@rowledge.org>
2021-09-06 11:51:47 -0700

    ARMv8 BitBLT enhancements from Ben Avison, plus include order fixes
    Add in Ben's BitBLT improvements for AMRv8, plus some more generic BLT improvements for ARMv6 , re-worked a little to handle the include-order changes
    
    Co-Authored-By: bavison <3324657+bavison@users.noreply.github.com>

--- platforms/Cross/plugins/BitBltPlugin/BitBltDispatch.h
+++ platforms/Cross/plugins/BitBltPlugin/BitBltDispatch.h
@@ -26,8 +26,8 @@
 #ifndef BITBLTDISPATCH_H_
 #define BITBLTDISPATCH_H_
 
-#include <stdbool.h>
 #include "sqVirtualMachine.h"
+#include <stdbool.h>
 
 #ifndef ColorMapPresent
 #define ColorMapPresent     1
@@ -116,7 +116,7 @@ typedef struct {
 	unsigned int          (*cmLookupTable)[];
 	bool                    noHalftone;
 	usqInt                  halftoneHeight;
-	sqInt                 (*halftoneBase)[];
+	unsigned int          (*halftoneBase)[];
 	union {
 		sqInt               sourceAlpha;
 		struct {
