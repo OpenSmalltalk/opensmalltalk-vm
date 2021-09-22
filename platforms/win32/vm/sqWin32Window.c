bdb66451e05399b9d81c8333ec4935463de7c668
Eliot Miranda <eliot.miranda@gmail.com>
2021-09-22 11:26:03 -0700

    Don't include system headers in Windows msvc makefile dependency generation. Fix a typo. [ci skip]

--- platforms/win32/vm/sqWin32Window.c
+++ platforms/win32/vm/sqWin32Window.c
@@ -765,7 +765,7 @@ void SetupPixmaps(void)
 	for (b= 0; b < 6; b++)
 	  {
 	    int i= 40 + ((36 * r) + (6 * b) + g);
-	    if (i > 255) error("index out of range in color table compuation");
+	    if (i > 255) error("index out of range in color table computation");
 	    SetColorEntry(i, (r * 65535) / 5, (g * 65535) / 5, (b * 65535) / 5);
 	  }
   }
