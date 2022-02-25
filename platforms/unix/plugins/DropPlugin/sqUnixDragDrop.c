OpenSmalltalk/opensmalltalk-vm

commit facfd9012e9f95679dc054fde69c13d5d4b514ca

CogVM source as per VMMaker.oscog-eem.2923
DropPlugin:
Add primitiveDropRequestURI to support launch apple events as drop events
on MacOS.  Nuke the unused setFileAccessCallback:. Delete unused functions from
DropPlugin.h and implementors.
Currently the only full implememntation (unu=tested) is on X11/Unix.  The Mac
implementation is skeletal; it should be filled in real soon now.

platforms/unix/plugins/DropPlugin/sqUnixDragDrop.c
@@ -52,8 +52,7 @@ extern char		     **uxDropFileNames;
 extern SQFile * fileValueOf(sqInt objectPointer);
 #else
 /*	Return a pointer to the first byte of of the SQFile data structure file
-	record within
-	anSQFileRecord, which is expected to be a ByteArray of size
+	record within anSQFileRecord, which is expected to be a ByteArray of size
 	self>>fileRecordSize. 
  */
 
@@ -68,17 +67,53 @@ fileValueOf(sqInt anSQFileRecord)
 sqInt dropInit(void)     { return 1; }
 sqInt dropShutdown(void) { return 1; }
 
-char *dropRequestFileName(sqInt dropIndex)	// in st coordinates
+/* We now set USE_FILE_URIs to 1 in platforms/unix//vm-display-X11/sqUnixXdnd.c
+ * hence dropRequestFileName skips the URI prefix and dropRequestURI includes it
+ */
+char *
+dropRequestFileName(sqInt dropIndex)	// in st coordinates
 {
-	if ((dropIndex > 0) && (dropIndex <= uxDropFileCount)) {
-		assert(uxDropFileNames);
-		dndReceived(uxDropFileNames[dropIndex - 1]);
-		return uxDropFileNames[dropIndex - 1];
-	}
-	return 0;
+	char *fileURIPrefix = "file:///";
+	int prefixLength = 0;
+	char *dropFileName;
+
+	if (dropIndex <= 0 || dropIndex > uxDropFileCount)
+		return 0;
+
+	assert(uxDropFileNames);
+	dndReceived(uxDropFileNames[dropIndex - 1]);
+
+	// The three valid schemes are
+	// file://host/path (prefix length 7)
+	// file:///path     (prefix length 8)
+	// file:/path 		(prefix length 6)
+	// see https://en.wikipedia.org/wiki/File_URI_scheme
+
+	// Compute the length of the prefix...
+	if (!(dropFileName = uxDropFileNames[dropIndex - 1]))
+		return 0;
+	while (*fileURIPrefix && *fileURIPrefix++ == *dropFileName++)
+		++prefixLength;
+
+	// file:///path & file:/path => path; anything else answered verbatim
+	return prefixLength == 8 || prefixLength == 6
+		? uxDropFileNames[dropIndex - 1] + fileURILength
+		: uxDropFileNames[dropIndex - 1];
 }
 
-sqInt dropRequestFileHandle(sqInt dropIndex)
+char *
+dropRequestURI(sqInt dropIndex)	// in st coordinates
+{
+	if (dropIndex <= 0 || dropIndex > uxDropFileCount)
+		return 0;
+
+	assert(uxDropFileNames);
+	dndReceived(uxDropFileNames[dropIndex - 1]);
+	return uxDropFileNames[dropIndex - 1];
+}
+
+sqInt
+dropRequestFileHandle(sqInt dropIndex)
 {
 	char *path= dropRequestFileName(dropIndex);
 	if (path) {
@@ -89,7 +124,3 @@ sqInt dropRequestFileHandle(sqInt dropIndex)
 	}  
 	return interpreterProxy->nilObject();
 }
-
-sqInt	sqSecFileAccessCallback(void *callback)               { return 0; }
-void	sqSetNumberOfDropFiles(sqInt numberOfFiles)           { }
-void	sqSetFileInformation(sqInt dropIndex, void *dropFile) { }

platforms/Cross/plugins/DropPlugin/DropPlugin.h
@@ -4,10 +4,7 @@
 sqInt dropInit(void);
 sqInt dropShutdown(void);
 
-char* dropRequestFileName(sqInt dropIndex); /* return name of file or NULL if error */
+char *dropRequestFileName(sqInt dropIndex); /* return name of file or NULL if error */
+char *dropRequestURI(sqInt dropIndex); /* return uri (if supported) or NULL if error */
 /* note: dropRequestFileHandle needs to bypass plugin security checks when implemented */
 sqInt dropRequestFileHandle(sqInt dropIndex); /* return READ-ONLY file handle OOP or nilObject if error */
-sqInt sqSecFileAccessCallback(void *);
-void sqSetNumberOfDropFiles(sqInt numberOfFiles);
-void sqSetFileInformation(sqInt dropIndex, void *dropFile);
-void sqDragTriggerData(char *aByteArray, sqInt dataLength, char *aFormat, sqInt formatLength);

platforms/iOS/vm/OSX/sqSqueakOSXDropAPI.m
@@ -50,12 +50,14 @@ sqInt dropShutdown(void) {
 	return 1;
 };
 
-char* dropRequestFileName(sqInt dropIndex) {
-	/* return name of file or NULL if error */
+char *dropRequestFileName(sqInt dropIndex) {
+	/* return file name or NULL if error */
 	NSView <sqSqueakOSXView> *view = [((sqSqueakOSXScreenAndWindow*)((__bridge NSWindow *)windowHandleFromIndex(1)).delegate) getMainViewOnWindow];
 	NSString *fileNameString = [view dragFileNameStringAtIndex: dropIndex];
 	return (char *) [fileNameString UTF8String];
 }
+
+char *dropRequestURI(sqInt dropIndex) { return NULL; }
 
 /* note: dropRequestFileHandle needs to bypass plugin security checks when implemented */
 sqInt dropRequestFileHandle(sqInt dropIndex) {
@@ -74,8 +76,4 @@ sqInt dropRequestFileHandle(sqInt dropIndex) {
 	}
 	sqInt result = ((sqInt (*) (char * nameIndex, sqInt nameSize, sqInt writeFlag, sqInt secureFlag)) fn)(fileName,(sqInt) strlen(fileName), 0,0);
 	return result;
-}
-
-sqInt sqSecFileAccessCallback(void *ptr) { 
-	return 0; 
 }

platforms/iOS/vm/OSX/sqSqueakOSXMetalView.m
@@ -861,12 +861,10 @@ lastSeenKeyBoardModifierDetails,dragInProgress,dragCount,dragItems,windowLogic,l
 }
 
 - (NSString*) dragFileNameStringAtIndex: (sqInt) index {
-	if (!self.dragItems)
+	if (!self.dragItems
+	 || index < 1 || index > [self.dragItems count])
 		return NULL;
-	if (index < 1 || index > [self.dragItems count])
-		return NULL;
-	NSString *filePath = (self.dragItems)[(NSUInteger) index - 1];
-	return filePath;
+	return (self.dragItems)[(NSUInteger) index - 1];
 }
 
 

platforms/unix/vm-display-X11/sqUnixXdnd.c
@@ -163,7 +163,8 @@ static void *xmalloc(size_t size)
 }
 
 
-static void *xcalloc(size_t nmemb, size_t size)
+static void *
+xcalloc(size_t nmemb, size_t size)
 {
   void *ptr= calloc(nmemb, size);
   if (!ptr)
@@ -175,7 +176,8 @@ static void *xcalloc(size_t nmemb, size_t size)
 }
 
 
-static void *xrealloc(void *ptr, size_t size)
+static void *
+xrealloc(void *ptr, size_t size)
 {
   ptr= realloc(ptr, size);
   if (!ptr)
@@ -187,7 +189,8 @@ static void *xrealloc(void *ptr, size_t size)
 }
 
 
-static int hexValue(const int c)
+static int
+hexValue(const int c)
 {
   if (c <  '0') return 0;
   if (c <= '9') return c - '0';
@@ -203,9 +206,10 @@ static int hexValue(const int c)
  * Neither does unix/plugins/DropPlugin/sqUnixDragDrop.c::dropRequestFileHandle.
  * Simplest thing is to convert file: URIs to file names at source.
  */
-#define USE_FILE_URIs 0
+#define USE_FILE_URIs 1
 #if !USE_FILE_URIs
-static char *uri2string(const char *uri)
+static char *
+uri2string(const char *uri)
 {
   size_t len= strlen(uri);
   char *string= (char *)xmalloc(len + 3);
@@ -243,7 +247,8 @@ static char *uri2string(const char *uri)
 
 /* Answer dndAware window under the cursor, or None if not found.
  */
-static Window dndAwareWindow(Window root, Window child, int *versionReturn)
+static Window
+dndAwareWindow(Window root, Window child, int *versionReturn)
 {
   Atom actualType;
   int actualFormat;
@@ -266,7 +271,8 @@ static Window dndAwareWindow(Window root, Window child, int *versionReturn)
   
   XQueryPointer(stDisplay, child, &rootReturn, &childReturn, &rootX, &rootY, &winX, &winY, &mask);
 
-  if (childReturn == None) return None;
+  if (childReturn == None)
+	return None;
 
   return dndAwareWindow(root, childReturn, versionReturn);
 }
@@ -279,7 +285,8 @@ static Window dndAwareWindow(Window root, Window child, int *versionReturn)
  * Window target : target window
  * char * type : event type name
  */
-static void sendClientMessage(long *data, Window source, Window target, Atom type)
+static void
+sendClientMessage(long *data, Window source, Window target, Atom type)
 {
   XEvent e;
   XClientMessageEvent *evt= &e.xclient;
@@ -300,7 +307,8 @@ static void sendClientMessage(long *data, Window source, Window target, Atom typ
 /*fdebugf((stderr, "Send %s to: 0x%lx\n", type, target));*/
 }
 
-static void sendEnter(Window target, Window source)
+static void
+sendEnter(Window target, Window source)
 {
   long data[5]= { 0, 0, 0, 0, 0 };
   data[1] |= 0x0UL; /* just three data types */
@@ -323,7 +331,8 @@ static void sendEnter(Window target, Window source)
 }
 
 
-static void sendPosition(Window target, Window source, int rootX, int rootY, Time timestamp)
+static void
+sendPosition(Window target, Window source, int rootX, int rootY, Time timestamp)
 {
   long data[5]= { 0, 0, 0, 0, 0 };
   data[2]= (rootX << 16) | rootY;
@@ -333,7 +342,8 @@ static void sendPosition(Window target, Window source, int rootX, int rootY, Tim
 }
 
 
-static void sendDrop(Window target, Window source, Time timestamp)
+static void
+sendDrop(Window target, Window source, Time timestamp)
 {
   long data[5]= { 0, 0, 0, 0, 0 };
   data[2]= timestamp;
@@ -343,7 +353,8 @@ static void sendDrop(Window target, Window source, Time timestamp)
 }
 
 
-static void sendLeave(Window target, Window source)
+static void
+sendLeave(Window target, Window source)
 {
   long data[5]= { 0, 0, 0, 0, 0 };
   fdebugf((stderr, "Send XdndLeave (output) source: 0x%lx target: 0x%lx\n", source, target));
@@ -351,7 +362,8 @@ static void sendLeave(Window target, Window source)
 }
 
 
-static enum XdndState dndOutInitialize(enum XdndState state)
+static enum XdndState
+dndOutInitialize(enum XdndState state)
 {
   fdebugf((stderr, "Internal signal DndOutStart (output)\n"));
   memset(&xdndOutRequestEvent, 0, sizeof(xdndOutRequestEvent));
@@ -363,7 +375,8 @@ static enum XdndState dndOutInitialize(enum XdndState state)
 
 /* Track the current mouse position.
  */
-static enum XdndState dndOutMotion(enum XdndState state, XMotionEvent *evt)
+static enum XdndState
+dndOutMotion(enum XdndState state, XMotionEvent *evt)
 {
   Window currentWindow= None;
   int versionReturn= 0;
@@ -402,7 +415,8 @@ static enum XdndState dndOutMotion(enum XdndState state, XMotionEvent *evt)
 
 /* A status message to know accept or not is received.
  */
-static enum XdndState dndOutStatus(enum XdndState state, XClientMessageEvent *evt)
+static enum XdndState
+dndOutStatus(enum XdndState state, XClientMessageEvent *evt)
 {
   long *ldata= evt->data.l;
   fdebugf((stderr, "Receive XdndStatus (output) status: 0x%lx target: 0x%lx\n", ldata[1], ldata[0]));
@@ -425,7 +439,8 @@ static enum XdndState dndOutStatus(enum XdndState state, XClientMessageEvent *ev
 
 /* The mouse button was released.
 */
-static enum XdndState dndOutRelease(enum XdndState state, XButtonEvent *evt)
+static enum XdndState
+dndOutRelease(enum XdndState state, XButtonEvent *evt)
 {
   if (XdndStateIdle == state) return XdndStateIdle;
   fdebugf((stderr, "Receive ButtonRelease (output) window: 0x%lx\n", evt->window));
@@ -442,7 +457,8 @@ static enum XdndState dndOutRelease(enum XdndState state, XButtonEvent *evt)
 
 /* Another application is requesting the selection.
 */
-static enum XdndState dndOutSelectionRequest(enum XdndState state, XSelectionRequestEvent *req)
+static enum XdndState
+dndOutSelectionRequest(enum XdndState state, XSelectionRequestEvent *req)
 {
   fdebugf((stderr, "Receive SelectionRequest for %s (output) owner: 0x%lx : requestor: 0x%lx\n",
            XGetAtomName(stDisplay, req->target), req->owner, req->requestor));
@@ -459,7 +475,8 @@ static enum XdndState dndOutSelectionRequest(enum XdndState state, XSelectionReq
 
 /* A finished message is received.
  */
-static enum XdndState dndOutFinished(enum XdndState state, XClientMessageEvent *evt)
+static enum XdndState
+dndOutFinished(enum XdndState state, XClientMessageEvent *evt)
 {
   fdebugf((stderr, "Receive XdndFinished (output) source: 0x%lx target: 0x%lx\n",
            DndWindow, xdndFinished_targetWindow(evt)));
@@ -475,7 +492,8 @@ static enum XdndState dndOutFinished(enum XdndState state, XClientMessageEvent *
  * state =  0 : Target window doesn't accept.
  * state =  1 : Target window accepts.
  */
-static void updateCursor(int state)
+static void
+updateCursor(int state)
 {
   static int lastCursor= -1;
 
@@ -494,7 +512,8 @@ static void updateCursor(int state)
 }
 
 
-static void dndInDestroyTypes(void)
+static void
+dndInDestroyTypes(void)
 {
   if (xdndInTypes == NULL)
     return;
@@ -503,7 +522,8 @@ static void dndInDestroyTypes(void)
 }
 
 
-static void updateInTypes(Atom *newTargets, int targetSize)
+static void
+updateInTypes(Atom *newTargets, int targetSize)
 {
   int i;
   dndInDestroyTypes();
@@ -516,7 +536,8 @@ static void updateInTypes(Atom *newTargets, int targetSize)
 
 /* Answer non-zero if dnd input object is available.
 */
-static int dndAvailable(void)
+static int
+dndAvailable(void)
 {
   return useXdnd && xdndInTypes;
 }
@@ -526,7 +547,8 @@ static int dndAvailable(void)
  * types - returned types (it should be copied by client), or NULL if unavailable.
  * count - number of types.
  */
-static void dndGetTargets(Atom **types, int *count)
+static void
+dndGetTargets(Atom **types, int *count)
 {
   int i;
   *types= 0;
@@ -538,7 +560,8 @@ static void dndGetTargets(Atom **types, int *count)
 }
 
 
-static void dndGetTypeList(XClientMessageEvent *evt)
+static void
+dndGetTypeList(XClientMessageEvent *evt)
 {
   xdndWillAccept= 0;
   isUrlList= 0;
@@ -586,7 +609,8 @@ static void dndGetTypeList(XClientMessageEvent *evt)
   xdndWillAccept= 1;
 }
 
-static void dndSendStatus(int willAccept, Atom action)
+static void
+dndSendStatus(int willAccept, Atom action)
 {
   XClientMessageEvent evt;
   memset(&evt, 0, sizeof(evt));
@@ -608,7 +632,8 @@ static void dndSendStatus(int willAccept, Atom action)
              xdndSourceWindow, willAccept, evt.data.l[1], XGetAtomName(stDisplay, action), action)); */
 }
 
-static void dndSendFinished(void)
+static void
+dndSendFinished(void)
 {
     XClientMessageEvent evt;
     memset(&evt, 0, sizeof(evt));
@@ -626,7 +651,8 @@ static void dndSendFinished(void)
 }
 
 
-static enum XdndState dndInEnter(enum XdndState state, XClientMessageEvent *evt)
+static enum XdndState
+dndInEnter(enum XdndState state, XClientMessageEvent *evt)
 {
   fdebugf((stderr, "Receive XdndEnter (input)\n"));
   if (xdndEnter_version(evt) < 3)
@@ -642,7 +668,8 @@ static enum XdndState dndInEnter(enum XdndState state, XClientMessageEvent *evt)
 }
 
 
-static enum XdndState dndInLeave(enum XdndState state)
+static enum XdndState
+dndInLeave(enum XdndState state)
 {
   fdebugf((stderr, "Receive XdndLeave (input)\n"));
   recordDragEvent(SQDragLeave, 0);
@@ -650,7 +677,8 @@ static enum XdndState dndInLeave(enum XdndState state)
 }
 
 
-static enum XdndState dndInPosition(enum XdndState state, XClientMessageEvent *evt)
+static enum XdndState
+dndInPosition(enum XdndState state, XClientMessageEvent *evt)
 {
   /*fdebugf((stderr, "Receive XdndPosition (input)\n"));*/
 
@@ -707,7 +735,8 @@ initDropFileNames()
   }
 }
 
-enum XdndState dndInDrop(enum XdndState state, XClientMessageEvent *evt)
+enum XdndState
+dndInDrop(enum XdndState state, XClientMessageEvent *evt)
 {
   fdebugf((stderr, "Receive XdndDrop (input)\n"));
 
@@ -843,13 +872,14 @@ addDropFile(char *fileName)
   else
     uxDropFileNames= (char **)xcalloc(1, sizeof(char *));
 #if USE_FILE_URIs
-    uxDropFileNames[uxDropFileCount++]= strdup(fileName);
+  uxDropFileNames[uxDropFileCount++]= strdup(fileName);
 #else
-    uxDropFileNames[uxDropFileCount++]= uri2string(fileName);
+  uxDropFileNames[uxDropFileCount++]= uri2string(fileName);
 #endif
 }
 
-static void dndGetSelection(Window owner, Atom property)
+static void
+dndGetSelection(Window owner, Atom property)
 {
   unsigned long remaining;
   unsigned char *data= 0;
@@ -881,7 +911,8 @@ static void dndGetSelection(Window owner, Atom property)
 
 
 /* SelectionNotify will be received as an answer of an XConvertSelection request sent by us. */
-static enum XdndState dndInSelectionNotify(enum XdndState state, XSelectionEvent *evt)
+static enum XdndState
+dndInSelectionNotify(enum XdndState state, XSelectionEvent *evt)
 {
   fdebugf((stderr, "Receive SelectionNotify (input)\n"));
   if (evt->property != XdndSelectionAtom) return state;
@@ -897,7 +928,8 @@ static enum XdndState dndInSelectionNotify(enum XdndState state, XSelectionEvent
 }
 
 
-static enum XdndState dndInFinished(enum XdndState state)
+static enum XdndState
+dndInFinished(enum XdndState state)
 {
   fdebugf((stderr, "Internal signal DndInFinished (input)\n"));
   dndSendFinished();
@@ -909,7 +941,8 @@ static enum XdndState dndInFinished(enum XdndState state)
 
 /* DnD client event handler */
 
-static enum XdndState dndHandleClientMessage(enum XdndState state, XClientMessageEvent *evt)
+static enum XdndState
+dndHandleClientMessage(enum XdndState state, XClientMessageEvent *evt)
 {
   Atom type= evt->message_type;
   if (type == XdndStatus)
@@ -932,7 +965,8 @@ static enum XdndState dndHandleClientMessage(enum XdndState state, XClientMessag
 
 /* DnD event handler */
 
-static void dndHandleEvent(int type, XEvent *evt)
+static void
+dndHandleEvent(int type, XEvent *evt)
 {
   static enum XdndState state= XdndStateIdle;
 
@@ -1180,7 +1214,8 @@ dndLaunchFile(char *filename)
 	return 0;
 }
 
-static void dndInitialise(void)
+static void
+dndInitialise(void)
 {
   XdndAware=		 XInternAtom(stDisplay, "XdndAware", False);
   XdndSelection=	 XInternAtom(stDisplay, "XdndSelection", False);
@@ -1211,7 +1246,8 @@ static void dndInitialise(void)
 #define fail(why)	do { fprintf(stderr, "%s\n", why);  exit(1); } while (0)
 
 
-static void run(void)
+static void
+run(void)
 {
   for (;;)
     {
@@ -1242,7 +1278,8 @@ static void run(void)
 }
 
 
-int main(int argc, char **argv)
+int
+main(int argc, char **argv)
 {
   stDisplay= XOpenDisplay(0);
   if (!stDisplay) fail("cannot open display");

platforms/win32/plugins/DropPlugin/sqWin32Drop.c
@@ -32,17 +32,6 @@ extern HWND stWindow;
 #define DPRINTF(x)
 #endif
 
-int sqSecFileAccessCallback(void *function) {
-#ifndef _MSC_VER
-#warning "REMOVE THIS NONSENSE"
-#warning "REMOVE THIS NONSENSE"
-#warning "REMOVE THIS NONSENSE"
-#warning "REMOVE THIS NONSENSE"
-#warning "REMOVE THIS NONSENSE"
-#endif
-  return 0;
-}
-
 
 /***************************************************************************/
 /***************************************************************************/
@@ -280,9 +269,8 @@ static char** dropFiles = NULL;
 static void freeDropFiles(void) {
   int i;
   if(dropFiles) {
-    for(i=0; i < numDropFiles; i++) {
+    for(i=0; i < numDropFiles; i++)
       free(dropFiles[i]);
-    }
     free(dropFiles);
     dropFiles = NULL;
     numDropFiles = 0;
@@ -768,11 +756,16 @@ int dropShutdown(void) {
   return 1;
 }
 
-char *dropRequestFileName(int dropIndex) {
+char *
+dropRequestFileName(int dropIndex) {
   DPRINTF(("dropRequestFileName(%d)\n", dropIndex));
   if(dropIndex < 1 || dropIndex > numDropFiles) return NULL;
   return dropFiles[dropIndex-1];
 }
+
+// So far win32 supports only files.
+char *
+dropRequestURI(int dropIndex) { return NULL; }
 
 sqInt dropRequestFileHandle(sqInt dropIndex) {
   sqInt fileHandle;

src/plugins/DropPlugin/DropPlugin.c
@@ -1,9 +1,9 @@
 /* Automatically generated by
-	VMPluginCodeGenerator VMMaker.oscog-eem.2744 uuid: 0273a0b2-9293-4e5f-b9ff-546a7d6a8270
+	VMPluginCodeGenerator VMMaker.oscog-eem.2923 uuid: 32f0b442-c9d6-4135-8ca0-2393e253109c
    from
-	DropPlugin VMMaker.oscog-eem.2744 uuid: 0273a0b2-9293-4e5f-b9ff-546a7d6a8270
+	DropPlugin VMMaker.oscog-eem.2923 uuid: 32f0b442-c9d6-4135-8ca0-2393e253109c
  */
-static char __buildInfo[] = "DropPlugin VMMaker.oscog-eem.2744 uuid: 0273a0b2-9293-4e5f-b9ff-546a7d6a8270 " __DATE__ ;
+static char __buildInfo[] = "DropPlugin VMMaker.oscog-eem.2923 uuid: 32f0b442-c9d6-4135-8ca0-2393e253109c " __DATE__ ;
 
 
 
@@ -43,7 +43,7 @@ EXPORT(const char*) getModuleName(void);
 EXPORT(sqInt) initialiseModule(void);
 EXPORT(sqInt) primitiveDropRequestFileHandle(void);
 EXPORT(sqInt) primitiveDropRequestFileName(void);
-EXPORT(sqInt) setFileAccessCallback(sqInt address);
+EXPORT(sqInt) primitiveDropRequestURI(void);
 EXPORT(sqInt) setInterpreter(struct VirtualMachine *anInterpreter);
 EXPORT(sqInt) shutdownModule(void);
 
@@ -51,29 +51,23 @@ EXPORT(sqInt) shutdownModule(void);
 /*** Variables ***/
 
 #if !defined(SQUEAK_BUILTIN_PLUGIN)
-static sqInt (*classString)(void);
 static sqInt (*failed)(void);
-static void * (*firstIndexableField)(sqInt oop);
-static sqInt (*instantiateClassindexableSize)(sqInt classPointer, sqInt size);
 static sqInt (*methodArgumentCount)(void);
-static sqInt (*pop)(sqInt nItems);
+static sqInt (*methodReturnString)(char *aCString);
+static sqInt (*methodReturnValue)(sqInt oop);
 static sqInt (*primitiveFail)(void);
-static sqInt (*push)(sqInt object);
 static sqInt (*stackIntegerValue)(sqInt offset);
 #else /* !defined(SQUEAK_BUILTIN_PLUGIN) */
-extern sqInt classString(void);
 extern sqInt failed(void);
-extern void * firstIndexableField(sqInt oop);
-extern sqInt instantiateClassindexableSize(sqInt classPointer, sqInt size);
 extern sqInt methodArgumentCount(void);
-extern sqInt pop(sqInt nItems);
+extern sqInt methodReturnString(char *aCString);
+extern sqInt methodReturnValue(sqInt oop);
 extern sqInt primitiveFail(void);
-extern sqInt push(sqInt object);
 extern sqInt stackIntegerValue(sqInt offset);
 extern
 #endif
 struct VirtualMachine* interpreterProxy;
-static const char *moduleName = "DropPlugin VMMaker.oscog-eem.2744 " INT_EXT;
+static const char *moduleName = "DropPlugin VMMaker.oscog-eem.2923 " INT_EXT;
 
 
 
@@ -112,12 +106,11 @@ primitiveDropRequestFileHandle(void)
 		return primitiveFail();
 	}
 	dropIndex = stackIntegerValue(0);
-
-	/* dropRequestFileHandle needs to return the actual oop returned */
-	handleOop = dropRequestFileHandle(dropIndex);
 	if (!(failed())) {
-		pop(2);
-		push(handleOop);
+		handleOop = dropRequestFileHandle(dropIndex);
+		if (!(failed())) {
+			methodReturnValue(handleOop);
+		}
 	}
 	return 0;
 }
@@ -132,38 +125,40 @@ EXPORT(sqInt)
 primitiveDropRequestFileName(void)
 {
     sqInt dropIndex;
-    char *dropName;
-    sqInt i;
-    sqInt nameLength;
-    sqInt nameOop;
-    char *namePtr;
 
 	if (!((methodArgumentCount()) == 1)) {
 		return primitiveFail();
 	}
-	dropIndex = stackIntegerValue(0);
 
-	/* dropRequestFileName returns name or NULL on error */
-	dropName = dropRequestFileName(dropIndex);
-	if (dropName == null) {
-		return primitiveFail();
+	/* dropRequestFileName returns name or NULL on error; methodReturnString: fails for nil */
+	dropIndex = stackIntegerValue(0);
+	if (!(failed())) {
+		methodReturnString(dropRequestFileName(dropIndex));
 	}
-	nameLength = strlen(dropName);
-	nameOop = instantiateClassindexableSize(classString(), nameLength);
-	namePtr = firstIndexableField(nameOop);
-	for (i = 0; i < nameLength; i += 1) {
-		namePtr[i] = (dropName[i]);
-	}
-	pop(2);
-	push(nameOop);
 	return 0;
 }
 
-	/* DropPlugin>>#setFileAccessCallback: */
+
+/*	On platforms that don't support URIs (currently Windows) the idea is to
+	use primitiveDropRequestURI,
+	which will fail, and then fall back on primitiveDropRequestFileName. */
+
+	/* DropPlugin>>#primitiveDropRequestURI */
 EXPORT(sqInt)
-setFileAccessCallback(sqInt address)
+primitiveDropRequestURI(void)
 {
-	return sqSecFileAccessCallback(((void *)address));
+    sqInt dropIndex;
+
+	if (!((methodArgumentCount()) == 1)) {
+		return primitiveFail();
+	}
+
+	/* dropRequestURI returns name or NULL on error; methodReturnString: fails for nil */
+	dropIndex = stackIntegerValue(0);
+	if (!(failed())) {
+		methodReturnString(dropRequestURI(dropIndex));
+	}
+	return 0;
 }
 
 
@@ -184,14 +179,11 @@ setInterpreter(struct VirtualMachine *anInterpreter)
 	if (ok) {
 		
 #if !defined(SQUEAK_BUILTIN_PLUGIN)
-		classString = interpreterProxy->classString;
 		failed = interpreterProxy->failed;
-		firstIndexableField = interpreterProxy->firstIndexableField;
-		instantiateClassindexableSize = interpreterProxy->instantiateClassindexableSize;
 		methodArgumentCount = interpreterProxy->methodArgumentCount;
-		pop = interpreterProxy->pop;
+		methodReturnString = interpreterProxy->methodReturnString;
+		methodReturnValue = interpreterProxy->methodReturnValue;
 		primitiveFail = interpreterProxy->primitiveFail;
-		push = interpreterProxy->push;
 		stackIntegerValue = interpreterProxy->stackIntegerValue;
 #endif /* !defined(SQUEAK_BUILTIN_PLUGIN) */
 	}
@@ -214,7 +206,7 @@ void* DropPlugin_exports[][3] = {
 	{(void*)_m, "initialiseModule", (void*)initialiseModule},
 	{(void*)_m, "primitiveDropRequestFileHandle\000\000", (void*)primitiveDropRequestFileHandle},
 	{(void*)_m, "primitiveDropRequestFileName\000\000", (void*)primitiveDropRequestFileName},
-	{(void*)_m, "setFileAccessCallback", (void*)setFileAccessCallback},
+	{(void*)_m, "primitiveDropRequestURI\000\000", (void*)primitiveDropRequestURI},
 	{(void*)_m, "setInterpreter", (void*)setInterpreter},
 	{(void*)_m, "shutdownModule\000\377", (void*)shutdownModule},
 	{NULL, NULL, NULL}
@@ -224,5 +216,6 @@ void* DropPlugin_exports[][3] = {
 
 signed char primitiveDropRequestFileHandleAccessorDepth = 0;
 signed char primitiveDropRequestFileNameAccessorDepth = 0;
+signed char primitiveDropRequestURIAccessorDepth = 0;
 
 #endif /* ifdef SQ_BUILTIN_PLUGIN */

commit e51c41fd40c139470e82530e7c0f3bc5e86b3a99

DnD/Unix: Fix dropRequestFileName which removed the trailing slash from the path
That was a regression since facfd9012e9f95679dc054fde69c13d5d4b514ca.

platforms/unix/plugins/DropPlugin/sqUnixDragDrop.c
@@ -1,23 +1,23 @@
 /* sqUnixDragDrop.c -- support for drag and drop, for those UIs that have it
- * 
+ *
  * Author: Ian Piumarta <ian.piumarta@inria.fr>
- * 
+ *
  *   Copyright (C) 1996-2004 by Ian Piumarta and other authors/contributors
  *                              listed elsewhere in this file.
  *   All rights reserved.
- *   
+ *
  *   This file is part of Unix Squeak.
- * 
+ *
  *   Permission is hereby granted, free of charge, to any person obtaining a
  *   copy of this software and associated documentation files (the "Software"),
  *   to deal in the Software without restriction, including without limitation
  *   the rights to use, copy, modify, merge, publish, distribute, sublicense,
  *   and/or sell copies of the Software, and to permit persons to whom the
  *   Software is furnished to do so, subject to the following conditions:
- * 
+ *
  *   The above copyright notice and this permission notice shall be included in
  *   all copies or substantial portions of the Software.
- * 
+ *
  *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -52,7 +52,7 @@ extern SQFile * fileValueOf(sqInt objectPointer);
 #else
 /*	Return a pointer to the first byte of of the SQFile data structure file
 	record within anSQFileRecord, which is expected to be a ByteArray of size
-	self>>fileRecordSize. 
+	self>>fileRecordSize.
  */
 
 	/* OSProcessPlugin>>#fileValueOf: */
@@ -94,9 +94,9 @@ dropRequestFileName(sqInt dropIndex)	// in st coordinates
 	while (*fileURIPrefix && *fileURIPrefix++ == *dropFileName++)
 		++prefixLength;
 
-	// file:///path & file:/path => path; anything else answered verbatim
+	// file:///path & file:/path => /path; anything else answered verbatim
 	return prefixLength == 8 || prefixLength == 6
-		? uxDropFileNames[dropIndex - 1] + prefixLength
+		? uxDropFileNames[dropIndex - 1] + prefixLength - 1
 		: uxDropFileNames[dropIndex - 1];
 }
 
@@ -120,6 +120,6 @@ dropRequestFileHandle(sqInt dropIndex)
 		sqInt handle = instantiateClassindexableSize(classByteArray(), fileRecordSize());
 		sqFileOpen(fileValueOf(handle), path, strlen(path), 0);
 		return handle;
-	}  
+	}
 	return interpreterProxy->nilObject();
 }