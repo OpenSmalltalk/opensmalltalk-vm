c5dad2567c3a8971676a606e2b93783c9f07a602
Tobias Pape <tobias@netshed.de>
2023-04-13 14:12:07 +0200

    [ci] simplify now that it is working

--- scripts/ci/actions_prepare_linux_x86.sh
+++ scripts/ci/actions_prepare_linux_x86.sh
@@ -8,6 +8,7 @@ set -ex
 # has to support the older i386 packages to make 32-bit compile.
 
 DEV_PKGS=(
+    libc6-dev
     libasound2-dev
     libssl-dev
     libfreetype6-dev
@@ -24,53 +25,21 @@ DEV_PKGS=(
     libglu1-mesa-dev
 )
 
-if [[ "${ARCH}" = "linux64x64" ]]; then
-    sudo apt-get update -y
-    sudo apt-get install -yq --no-install-suggests --no-install-recommends --allow-unauthenticated \
-            debhelper \
-            devscripts \
-            gcc-multilib \
-            automake \
-            autoconf \
-            libtool \
-            curl \
-            cmake \
-            libc6-dev \
-            "${DEV_PKGS[@]}"
-
-elif [[ "${ARCH}" = "linux32x86" ]]; then
-    # These Packages must go in amd64, because we cannot install the
-    # :i386 package otherwise.
-    POISON_PKGS=(
-        "${DEV_PKGS[@]}"
-        libcairo2
-        libcairo-gobject2
-        libglib2.0-0
-        libmount-dev
-        libselinux1-dev
-        gir1.2-pango
-        libharfbuzz-dev
-        libpango-1.0-0
-        libpangocairo-1.0-0
-        libpangoft2-1.0-0
-        libpangoxft-1.0-0
-    )
+# Per default, let apt decide
+ARCHCODE=""
+if [[ "${ARCH}" = "linux32x86" ]]; then
     sudo dpkg --add-architecture i386
-    sudo add-apt-repository ppa:ondrej/php
-    sudo apt-get update -y
-    # apt-mark showhold
-    # head -n100 /etc/apt/sources.list /etc/apt/sources.list.d/* /etc/apt/apt.conf.d/*
-    apt-cache policy libc6 libc6:i386 libc6-dev libc6-dev:amd64 libc6-dev:i386
-    echo ======
-    apt-cache policy libpcre2-8-0:amd64  libpcre2-8-0:i386 
-    
-    # make sure no conflicting x86_64 packages remain
-    # sudo apt-get purge "${POISON_PKGS[@]/%/:amd64}"
-    # install i386-version of packages
-    sudo apt-get install   -o Debug::pkgProblemResolver=yes --ignore-hold -o Dpkg::Options='--force-confdef --force-confold --force-overwrite' -y --no-install-suggests --no-install-recommends --allow-unauthenticated \
-         libpcre2-8-0:amd64  libpcre2-8-0:i386 \
-            devscripts \
-            gcc-multilib \
-            libc6-dev:i386 \
-            "${DEV_PKGS[@]/%/:i386}"
+    ARCHCODE=":i386"
 fi
+
+sudo apt-get update -y
+sudo apt-get install -yq --no-install-suggests --no-install-recommends --allow-unauthenticated \
+     debhelper \
+     devscripts \
+     gcc-multilib \
+     automake \
+     autoconf \
+     libtool \
+     curl \
+     cmake \
+     "${DEV_PKGS[@]/%/$ARCHCODE}"
