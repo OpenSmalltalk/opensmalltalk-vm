OpenSmalltalk/opensmalltalk-vm

commit 220f91c6de598f069d1070f6408f721526298d3f

For macOS platform, fixes high-resolution scaling when (1) reading prior display extent from image and (2) setting new extent via HostWindowPlugin.

platforms/iOS/plugins/HostWindowPlugin/sqMacHostWindow.m
@@ -138,10 +138,10 @@ ioSizeOfWindowSetxy(wIndexType windowIndex, sqInt x, sqInt y)
 		return 0;
 
 	NSWindow *window = nsWindowFromIndex(windowIndex);
-    NSRect frame = [window frame];
+    NSRect frame = [window convertRectToBacking: [window frame]];
     frame.size.width = x;
     frame.size.height = y;
-    [window setFrame: frame display: YES];    
+    [window setFrame: [window convertRectFromBacking: frame] display:YES];  
 
 	return 0;
 }

platforms/iOS/vm/OSX/plugins/HostWindowPlugin/sqMacHostWindow.m
@@ -93,10 +93,10 @@ sqInt ioPositionOfNativeDisplay(unsigned long windowHandle)
 sqInt ioSizeOfWindowSetxy(wIndexType windowIndex, sqInt x, sqInt y)
 {
     NSWindow *window = [[NSApplication sharedApplication] mainWindow];
-    NSRect rect = [window frame];
+    NSRect rect = [window convertRectToBacking: [window frame]];
     rect.size.width = x;
     rect.size.height = y;
-    [window setFrame:rect display:YES];    
+    [window setFrame: [window convertRectFromBacking: rect] display:YES];
 	return (0);  /* w is high 16 bits; h is low 16 bits */
 }
 

platforms/iOS/vm/OSX/SqueakOSXAppDelegate.m
@@ -163,16 +163,17 @@ SqueakOSXAppDelegate *gDelegateApp;
 	width  = ((unsigned) getSavedWindowSize()) >> 16;
 	height = getSavedWindowSize() & 0xFFFF;
 	width = (sqInt) ((width*4)/32.0f+0.5)*8.0;  //JMM OPEN/GL THOUGHTS FOR PERFORMANCE
-	NSSize sizeOfWindowContent;
-	sizeOfWindowContent.width = width;
-	sizeOfWindowContent.height = height;
 	
-	[gDelegateApp.window setContentSize: sizeOfWindowContent];
 	NSRect resetFrame;
 	resetFrame.origin.x = 0.0f;
 	resetFrame.origin.y	= 0.0f;
 	resetFrame.size.width = width;
 	resetFrame.size.height = height;
+	resetFrame = [[self mainView] convertRectFromBacking: resetFrame];
+
+	NSSize sizeOfWindowContent = resetFrame.size;
+	[gDelegateApp.window setContentSize: sizeOfWindowContent];
+
     [self.window setAcceptsMouseMovedEvents: YES];
 #if MAC_OS_X_VERSION_MIN_REQUIRED <= MAC_OS_X_VERSION_10_9
 	[self.window useOptimizedDrawing: YES];

commit 21a2fc963e2deb2071d1a89eb8594b6d7e7b09f3

Fix the regression in the Mac HostWindowPlugin caused by 220f91c6de598f069d1070f6408f721526298d3f. To fix that issue correctly the plugin must be aware of the image's lack of awareness of window scaling. The current code causes a reasonable loop that grabs the native window rectangle, modifies it slightly, and installs the new rectangle to shrink the native window rectangle to zero in a few iterations if scaling is on.
Fix a few comments, variable names, etc.

platforms/Cross/plugins/HostWindowPlugin/HostWindowPlugin.h
@@ -10,7 +10,7 @@
  * goes wrong - typically the windowIndex invalid or similar */
 extern sqInt closeWindow(sqIntptr_t index);
 
-/* createWindow: takes sqInt width, height and origin x/y plus a char* list of
+/* createWindow: takes sqInt width, height and origin x/y plus a char * list of
  * as yet undefined attributes. Returns an sqInt window index or 0 for failure
  * Failure may occur because of an inability to add the window, too many
  * windows already extant (platform dependant), the specified size being
@@ -32,7 +32,7 @@ sqIntptr_t windowIndex);
  * I hope is unlikely for some time to come */
 extern sqInt ioSizeOfWindow(sqIntptr_t windowIndex);
 
-/* ioSizeOfNativeWindow: arg is void* windowHandle, defined as usqIntptr_t
+/* ioSizeOfNativeWindow: arg is void * windowHandle, defined as usqIntptr_t
  * for convenience. Return the size of the specified native window in
  * (width<<16 || height) format like ioScreenSize. The size includes
  * the window's title bar, and borders, if any.
@@ -41,7 +41,7 @@ extern sqInt ioSizeOfWindow(sqIntptr_t windowIndex);
  * which it is hoped is unlikely for some time to come */
 extern sqInt ioSizeOfNativeWindow(usqIntptr_t windowHandle);
 
-/* as per ioSizeOfNativeWindow, but answers the size of the drawing
+/* as per ioSizeOfNativeWindow, but returns the size of the drawing
  * surface (the inside) of the window.
  */
 extern sqInt ioSizeOfNativeDisplay(usqIntptr_t windowHandle);
@@ -58,7 +58,7 @@ extern sqInt ioSizeOfWindowSetxy(sqIntptr_t windowIndex, sqInt w, sqInt h);
  * Return -1 (as above) for failure - typically invalid windowIndex */
 extern sqInt ioPositionOfWindow(sqIntptr_t windowIndex);
 
-/* ioPositionOfNativeWindow: arg is void* windowHandle, defined as usqIntptr_t
+/* ioPositionOfNativeWindow: arg is void * windowHandle, defined as usqIntptr_t
  * for convenience. Return the pos of the specified native window in
  * (left<<16 || top) format like ioScreenSize. The position is the origin of the
  * host window and not its content, so it does not offset the position by the
@@ -66,7 +66,7 @@ extern sqInt ioPositionOfWindow(sqIntptr_t windowIndex);
  * Return -1 (as above) for failure - typically invalid windowHandle */
 extern sqInt ioPositionOfNativeWindow(usqIntptr_t windowHandle);
 
-/* as per ioPositionOfNativeWindow, but answers the position of the drawing
+/* as per ioPositionOfNativeWindow, but returns the position of the drawing
  * surface (the inside) of the window.
  */
 extern sqInt ioPositionOfNativeDisplay(usqIntptr_t windowHandle);
@@ -78,12 +78,12 @@ extern sqInt ioPositionOfNativeDisplay(usqIntptr_t windowHandle);
  * format or -1 for failure, as above */
 extern sqInt ioPositionOfWindowSetxy(sqIntptr_t windowIndex, sqInt x, sqInt y);
 
-/* ioSetTitleOfWindow: args are sqIntptr_t windowIndex, char* newTitle and
+/* ioSetTitleOfWindow: args are sqIntptr_t windowIndex, char * newTitle and
  * sqInt size of new title. Fail with -1 if windowIndex is invalid, string is too
 long for platform etc. Leave previous title in place on failure */
 sqInt ioSetTitleOfWindow(sqIntptr_t windowIndex, char *newTitle, sqInt sizeOfTitle);
 
-/* ioSetIconOfWindow: args are int windowIndex windowIndex, char* iconPath and
+/* ioSetIconOfWindow: args are int windowIndex windowIndex, char * iconPath and
  * int size of new logo path. If one of the function is failing, the logo is not set.
  */
 extern sqInt ioSetIconOfWindow(sqIntptr_t windowIndex, char *iconPath, sqInt sizeOfPath);
@@ -106,8 +106,8 @@ extern sqInt ioPositionOfScreenWorkArea(sqIntptr_t windowIndex);
  */
 extern sqInt ioSizeOfScreenWorkArea(sqIntptr_t windowIndex);
 
-/* ioScreenRectangles answer an Array of screen coordinates as pairs of packed
- * points, origin, extent for each screen.
+/* ioScreenRectangles returns an Array of screen coordinates as pairs of packed
+ * points, origin, extent for each screen, or 0 if none can be found.
  */
 extern sqInt ioScreenRectangles(void);
 

platforms/iOS/plugins/HostWindowPlugin/sqMacHostWindow.m
@@ -35,10 +35,10 @@
  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  OTHER DEALINGS IN THE SOFTWARE.
 
- The end-user documentation included with the redistribution, if any, must include the following acknowledgment: 
- "This product includes software developed by Corporate Smalltalk Consulting Ltd (http://www.smalltalkconsulting.com) 
- and its contributors", in the same place and form as other third-party acknowledgments. 
- Alternately, this acknowledgment may appear in the software itself, in the same form and location as other 
+ The end-user documentation included with the redistribution, if any, must include the following acknowledgment:
+ "This product includes software developed by Corporate Smalltalk Consulting Ltd (http://www.smalltalkconsulting.com)
+ and its contributors", in the same place and form as other third-party acknowledgments.
+ Alternately, this acknowledgment may appear in the software itself, in the same form and location as other
  such third-party acknowledgments.
  */
 
@@ -56,7 +56,7 @@
 extern struct VirtualMachine *interpreterProxy;
 
 /* WARNING, WARNING, WARNING, Will Robertson!! This is a partial implementation
- * that cannot create new windows.  It simply answers or operates on the main
+ * that cannot create new windows.  It simply returns or operates on the main
  * Squeak window, which is all that Terf needs.
  *
  * Regarding coordinate system transformations use recordWindowEvent:window: in
@@ -83,10 +83,10 @@ closeWindow(wIndexType windowIndex)
 {
 	NSWindow *window = nsWindowFromIndex(windowIndex);
 
-	if (!window) 
+	if (!window)
 		return 0;
 	windowBlockFromIndex(windowIndex)->context = NULL;
-	RemoveWindowBlock(windowBlockFromIndex(windowIndex));	
+	RemoveWindowBlock(windowBlockFromIndex(windowIndex));
 	[window close];
 	return 1;
 }
@@ -99,9 +99,9 @@ ioPositionOfWindow(wIndexType windowIndex)
 	if (!window)
 		return -1;
 
-    NSRect win = [window frame];
-	return packedDoubleXY(win.origin.x,
-						  yZero() - (win.origin.y + win.size.height));
+	NSRect frame = [window frame];
+	return packedDoubleXY(frame.origin.x,
+						  yZero() - (frame.origin.y + frame.size.height));
 }
 
 sqInt
@@ -110,10 +110,10 @@ ioPositionOfWindowSetxy(wIndexType windowIndex, sqInt x, sqInt y)
 	if (windowIndex != 1)
 		return 0;
 	NSWindow *window = nsWindowFromIndex(windowIndex);
-    NSRect frame = [window frame];
-    frame.origin.x = x;
-    frame.origin.y = yZero() - (y + frame.size.height);
-    [window setFrame: frame display: YES];    
+	NSRect frame = [window frame];
+	frame.origin.x = x;
+	frame.origin.y = yZero() - (y + frame.size.height);
+	[window setFrame: frame display: YES];
 
 	return 0;
 }
@@ -126,9 +126,9 @@ ioSizeOfWindow(wIndexType windowIndex)
 	if (!window)
 		return -1;
 
-	NSRect win = [window frame];
+	NSRect frame = [window frame];
 
-	return packedDoubleXY(win.size.width,win.size.height);
+	return packedDoubleXY(frame.size.width,frame.size.height);
 }
 
 sqInt
@@ -138,10 +138,10 @@ ioSizeOfWindowSetxy(wIndexType windowIndex, sqInt x, sqInt y)
 		return 0;
 
 	NSWindow *window = nsWindowFromIndex(windowIndex);
-    NSRect frame = [window convertRectToBacking: [window frame]];
-    frame.size.width = x;
-    frame.size.height = y;
-    [window setFrame: [window convertRectFromBacking: frame] display:YES];  
+	NSRect frame = [window frame];
+	frame.size.width = x;
+	frame.size.height = y;
+	[window setFrame: frame display: YES];
 
 	return 0;
 }
@@ -154,10 +154,10 @@ ioSetTitleOfWindow(wIndexType windowIndex, char *newTitle, sqInt sizeOfTitle)
 	if (!window)
 		return 0;
 
-    NSString *title = [[NSString alloc] initWithBytes:newTitle length:sizeOfTitle encoding:NSUTF8StringEncoding];
+	NSString *title = [[NSString alloc] initWithBytes:newTitle length:sizeOfTitle encoding:NSUTF8StringEncoding];
 
-    [window setTitle: title];
-    RELEASEOBJ(title);
+	[window setTitle: title];
+	RELEASEOBJ(title);
 
 	return 1;
 }
@@ -215,7 +215,7 @@ windowDescriptorBlock *entry;
 
 
 wHandleType
-windowHandleFromIndex(wIndexType windowIndex) 
+windowHandleFromIndex(wIndexType windowIndex)
 {
 windowDescriptorBlock *entry;
 	entry = windowListRoot;
@@ -253,7 +253,7 @@ windowDescriptorBlock *entry;
 	return 0;
 }
 
-static sqInt nextIndex = 1; 
+static sqInt nextIndex = 1;
 
 /* Create a new entry in the linkedlist of windows.
  * If the calloc fails, return NULL which will then go back to the
@@ -316,10 +316,10 @@ ioPositionOfNativeWindow(usqIntptr_t windowHandle)
 		return -1;
 
 	NSWindow *window = (__bridge NSWindow *)(void *)windowHandle;
-    NSRect win = [window frame];
+	NSRect frame = [window frame];
 
-	return packedDoubleXY(win.origin.x,
-						  yZero() - (win.origin.y + win.size.height));
+	return packedDoubleXY(frame.origin.x,
+						  yZero() - (frame.origin.y + frame.size.height));
 }
 
 sqInt
@@ -329,20 +329,20 @@ ioSizeOfNativeWindow(usqIntptr_t windowHandle)
 		return -1;
 
 	NSWindow *window = (__bridge NSWindow *)(void *)windowHandle;
-	NSRect win = [window frame];
+	NSRect frame = [window frame];
 
-	return packedDoubleXY(win.size.width,win.size.height);
+	return packedDoubleXY(frame.size.width,frame.size.height);
 }
 
 static int
 titlebarHeight(NSWindow *window,NSRect win)
 {
-    NSSize contentSize = [window contentRectForFrameRect: win].size;
+	NSSize contentSize = [window contentRectForFrameRect: win].size;
 
-    return win.size.height - contentSize.height;
+	return win.size.height - contentSize.height;
 }
 
-/* ioPositionOfNativeDisplay answers the origin of the client rectangle,
+/* ioPositionOfNativeDisplay returns the origin of the client rectangle,
  * in screen coordinates of the specified window handle, i.e. of the
  * rectangle below the title bar.
  */
@@ -353,14 +353,14 @@ ioPositionOfNativeDisplay(usqIntptr_t windowHandle)
 		return -1;
 
 	NSWindow *window = (__bridge NSWindow *)(void *)windowHandle;
-    NSRect win = [window frame];
+	NSRect frame = [window frame];
 
-	return packedDoubleXY(win.origin.x,
-						  yZero() + titlebarHeight(window,win)
-						  - (win.origin.y + win.size.height));
+	return packedDoubleXY(frame.origin.x,
+						  yZero() + titlebarHeight(window,frame)
+						  - (frame.origin.y + frame.size.height));
 }
 
-/* ioSizeOfNativeDisplay answers the extent of the client rectangle,
+/* ioSizeOfNativeDisplay returns the extent of the client rectangle,
  * in screen coordinates of the specified window handle, i.e. of the
  * rectangle below the title bar.
  */
@@ -371,36 +371,38 @@ ioSizeOfNativeDisplay(usqIntptr_t windowHandle)
 		return -1;
 
 	NSWindow *window = (__bridge NSWindow *)(void *)windowHandle;
-    NSRect win = [window frame];
+	NSRect frame = [window frame];
 
-	return packedDoubleXY(win.size.width,
-						  win.size.height-titlebarHeight(window,win));
+	return packedDoubleXY(frame.size.width,
+						  frame.size.height-titlebarHeight(window,frame));
 }
 
 /* Return the pixel origin (topleft) of the platform-defined working area
-   for the screen containing the given window. */
-/* copied from platforms/unix/vm-display-X11/sqUnixX11.c */
+ * for the screen containing the given window.
+ * copied from platforms/unix/vm-display-X11/sqUnixX11.c
+ */
 sqInt
 ioPositionOfScreenWorkArea(sqIntptr_t windowIndex)
 {
-    NSScreen *screen = [[[NSApplication sharedApplication] mainWindow] screen];
-    NSRect frame = [screen visibleFrame];
+	NSScreen *screen = [[[NSApplication sharedApplication] mainWindow] screen];
+	NSRect frame = [screen visibleFrame];
 	// 0@36 last we looked...
 	return packedDoubleXY(frame.origin.x,
 						  yZero() - (frame.origin.y + frame.size.height));
 }
 
 /* Return the pixel extent of the platform-defined working area
-   for the screen containing the given window. */
+ * for the screen containing the given window.
+ */
 sqInt
 ioSizeOfScreenWorkArea(sqIntptr_t windowIndex)
 {
-    NSScreen *screen = [[[NSApplication sharedApplication] mainWindow] screen];
-    NSRect frame = [screen visibleFrame];
+	NSScreen *screen = [[[NSApplication sharedApplication] mainWindow] screen];
+	NSRect frame = [screen visibleFrame];
 	return packedDoubleXY(frame.size.width,frame.size.height);
 }
 
-/* Answer an Array of screen coordinates as pairs of packed points, origin,
+/* Return an Array of screen coordinates as pairs of packed points, origin,
  * extent for each screen. So if there is one monitor the array has two entries.
  */
 sqInt
@@ -419,7 +421,7 @@ ioScreenRectangles(void)
 			(void)storeIntegerofObjectwithValue
 					(i * 2 + 1, a, packedDoubleXY(f.size.width,f.size.height));
 		}
-	return  a;
+	return a;
 }
 
 /* What happens with multiple monitors? There's a unified address space that