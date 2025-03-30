4787c7f6482ff956444a9f2d13019867b3858c35
Eliot Miranda <eliot.miranda@gmail.com>
2025-03-29 21:28:22 -0700

    CogVM source as per VMMaker.oscog-eem.3531
    Improve the assert in SpurMemoryManager>>storePointerUnchecked:ofObject:withValue:
    to cope with cloneObject: on CompiledCode, and restoring saved first fields of
    empty objects. Fixes several bogus assert fails in running the full test suite.
    
    Cogit: fix genPrimitiveAt for V3 on ARM32. The call of the
    cePositive32BitIntegerTrampoline trampoline must save and restore the link reg.
    
    Interpreter:
    Rename storePointerNoAssert:ofFreeChunk:withValue: to
    storeUncheckedNoAssert:ofAny:withValue: and use it in the segment storage
    primitive on Spur, to avoid bogus assert failure messages
    (old code used storePointerUnchecked:...).
    
    Add NewObjectMemory>>numPointerSlotsOf: and use it in longPrintOop:
    instead of the older code that didn't handle contexts correctly.
    
    Add an option to compile asserts to use warningat and pass the numeric line number,
    convenient for debugging specific assert fails with conditional breakpoints.

--- platforms/Cross/vm/sqAssert.h
+++ platforms/Cross/vm/sqAssert.h
@@ -23,21 +23,19 @@ IMPORT(void) error(const char *);
 IMPORT(void) warning(const char *);
 IMPORT(void) warningat(const char *,int);
 IMPORT(void) warninginat(const char *, const char *,int);
-#else
-# if defined(SQUEAK_BUILTIN_PLUGIN)
+#elif defined(SQUEAK_BUILTIN_PLUGIN)
 void error(const char *);
 void warning(const char *);
 void warningat(const char *,int);
 void warninginat(const char *, const char *,int);
-# else
-#   if !defined(EXPORT)
-#	  define EXPORT(returnType) returnType
-#   endif
+#else
+# if !defined(EXPORT)
+#	define EXPORT(returnType) returnType
+# endif
 EXPORT(void) error(const char *);
 EXPORT(void) warning(const char *);
 EXPORT(void) warningat(const char *,int);
 EXPORT(void) warninginat(const char *, const char *,int);
-#  endif
 #endif
 #pragma auto_inline(on)
 
@@ -88,24 +86,36 @@ warningIfNotInAt(__word condition, const char *msg, const char *fn, int line)
 }
 # undef __word
 
-# define assert(expr)  warningIfNot(expr, #expr " " __stringifyNum(__LINE__))
-# define assertfnl(expr,fn,l)  warningIfNotInAt(expr, #expr,fn,l)
-# define asserta(expr) warningIfNot(expr, #expr " " __stringifyNum(__LINE__))
-# define assertf(msg)  (warning(#msg " " __stringifyNum(__LINE__)),0)
+# if ASSERTLN // use warning*At to pass line as number for breakpointing
+#	define assert(expr)  warningIfNotAt(expr, #expr, __LINE__)
+#	define asserta(expr) warningIfNot(expr, #expr, __LINE__)
+#	define assertf(msg)  (warningat(#msg, __LINE__),0)
+# else
+#	define assert(expr)  warningIfNot(expr, #expr " " __stringifyNum(__LINE__))
+#	define asserta(expr) warningIfNot(expr, #expr " " __stringifyNum(__LINE__))
+#	define assertf(msg)  (warning(#msg " " __stringifyNum(__LINE__)),0)
+# endif
 # define assertl(expr,line)  warningIfNotAt(expr, #expr, line)
 # define assertal(expr,line) warningIfNotAt(expr, #expr, line)
 # define assertfl(msg,line)  (warningat(#msg,line),0)
+# define assertfnl(expr,fn,l)  warningIfNotInAt(expr, #expr,fn,l)
 extern char expensiveAsserts;
 # define eassert(expr)  warningIfNot(!expensiveAsserts || !(expr), #expr " " __stringifyNum(__LINE__))
 # define PRODUCTION 0
 #else
-# define assert(expr)  ((expr)||(warning(#expr " " __stringifyNum(__LINE__)),0))
-# define assertfnl(expr,fn,l)  ((expr)||(warninginat(#expr,fn,l),0))
-# define asserta(expr) ((expr)||(warning(#expr " " __stringifyNum(__LINE__)),0))
-# define assertf(msg)  (warning(#msg " " __stringifyNum(__LINE__)),0)
+# if ASSERTLN // use warning*At to pass line as number for breakpointing
+#	define assert(expr)  ((expr)||(warningat(#expr, __LINE__),0))
+#	define asserta(expr) ((expr)||(warningat(#expr, __LINE__),0))
+#	define assertf(msg)  (warningat(#msg, __LINE__),0)
+# else
+#	define assert(expr)  ((expr)||(warning(#expr " " __stringifyNum(__LINE__)),0))
+#	define asserta(expr) ((expr)||(warning(#expr " " __stringifyNum(__LINE__)),0))
+#	define assertf(msg)  (warning(#msg " " __stringifyNum(__LINE__)),0)
+# endif
 # define assertl(expr,line)  ((expr)||(warningat(#expr,line),0))
 # define assertal(expr,line) ((expr)||(warningat(#expr,line),0))
 # define assertfl(msg,line)  (warningat(#msg,line),0)
+# define assertfnl(expr,fn,l)  ((expr)||(warninginat(#expr,fn,l),0))
 extern char expensiveAsserts;
 # define eassert(expr)  (!expensiveAsserts||(expr) \
 						 ||(warning(#expr " " __stringifyNum(__LINE__)),0))
