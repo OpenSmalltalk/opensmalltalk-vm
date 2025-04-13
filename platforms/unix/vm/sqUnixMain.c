OpenSmalltalk/opensmalltalk-vm

commit e6108a0cd83b5cfde484d41e8a4e289b4da3a02d

CogVM source as per VMMaker.oscog-eem.3533
Provide an array of leakcheck flag explanations for the vm's help
(given event and send trace meanings are nice to have).

Win32: Move sqWin32Directory.c to the FilePlugin where it belongs.

Git: include renames in the output of scripts/modified

platforms/unix/vm/sqUnixMain.c
@@ -522,8 +522,23 @@ getAttributeString(sqInt id)
 		return imageName;
 	case 1001: // OS type: "unix", "win32", "mac", ...
 		return OS_TYPE;
-	case 1002: // OS name: e.g. "solaris2.5" on unix, "win95" on win32, ...
+	case 1002: { // OS name: e.g. "solaris2.5" on unix, "win95" on win32, ...
+#if OLD_1002_ATTRIBUTE
 		return VM_TARGET_OS;
+#else
+		static char *osName = 0;
+		struct utsname info;
+		if (osName)
+			return osName;
+		if (uname(&info) < 0)
+			return VM_TARGET_OS;
+		osName = malloc(strlen(info.sysname) + strlen(info.release) + 2);
+		strcpy(osName,info.sysname);
+		osName[strlen(info.sysname)] = ' ';
+		strcat(osName,info.release);
+		return osName;
+#endif
+	}
 	case 1003: // processor architecture: e.g. "68k", "x86", "PowerPC", ...
 		return VM_TARGET_CPU;
 	case 1004: // Interpreter version string
@@ -1823,7 +1838,15 @@ vm_printUsage(void)
 	option("  "VMOPTION("failonffiexception")"   when in an FFI callout primitive catch exceptions and fail the primitive\n");
 	option("  "VMOPTION("breakmnu")" selector    set breakpoint on MNU of selector\n");
 	option("  "VMOPTION("eden")" <size>[kmg]     use given eden size\n");
-	option("  "VMOPTION("leakcheck")" num        check for leaks in the heap\n");
+	option("  "VMOPTION("leakcheck")"=flags      check for leaks in the heap\n");
+	{ extern const char *leakCheckFlagsMeanings[];
+		i = 0;
+		while (leakCheckFlagsMeanings[i]) {
+			extendOption("    ");
+			extendOption(leakCheckFlagsMeanings[i++]);
+			extendOption("\n");
+		}
+	}
 	option("  "VMOPTION("stackpages")" <num>     use given number of stack pages\n");
 #endif
 	option("  "VMOPTION("noevents")"             disable event-driven input support\n");

platforms/iOS/vm/Common/Classes/sqSqueakMainApplication.m
@@ -141,9 +141,9 @@ extern sqInt interpret(void);  //This is a VM Callback
 - (void) setupAIO {
 	void aioInit(void);
 	aioInit();
-	#if STACKVM || COGVM
+#if STACKVM || COGVM
 	ioInitThreads();
-	#endif
+#endif
 }
 
 - (void) setupEventQueue {

platforms/iOS/vm/OSX/sqSqueakOSXApplication.m
@@ -577,7 +577,14 @@ printOptionStrings()
 	option("  "VMOPTION("failonffiexception")"   when in an FFI callout primitive catch exceptions and fail the primitive\n");
 	option("  "VMOPTION("breakmnu")" selector    set breakpoint on MNU of selector\n");
 	option("  "VMOPTION("eden")" <size>[kmg]     set eden memory to bytes\n");
-	option("  "VMOPTION("leakcheck")" num        check for leaks in the heap\n");
+	option("  "VMOPTION("leakcheck")"=flags      check for leaks in the heap\n");
+	extern const char *leakCheckFlagsMeanings[];
+	i = 0;
+	while (leakCheckFlagsMeanings[i]) {
+		extendOption("    ");
+		extendOption(leakCheckFlagsMeanings[i++]);
+		extendOption("\n");
+	}
 	option("  "VMOPTION("stackpages")" num       use n stack pages\n");
 	option("  "VMOPTION("numextsems")" num       make the external semaphore table num in size\n");
 	option("  "VMOPTION("noheartbeat")"          disable the heartbeat for VM debugging. disables input\n");

platforms/win32/plugins/FilePlugin/sqWin32Directory.c
@@ -10,12 +10,13 @@
 *   UPDATES:
 *     1) Support for long path names added by using UNC prefix in that case
 *        (Marcel Taeumel, Hasso Plattner Institute, Postdam, Germany)
+*     2) Moved to platforms/win32/plugins/FilePlugin (eem)
 *
 *****************************************************************************/
 #include <Windows.h>
 
 #include "sq.h"
-#include "../plugins/FilePlugin/sqWin32File.h"
+#include "sqWin32File.h"
 # include <sys/types.h>
 # include <sys/stat.h>
 
@@ -31,6 +32,7 @@
 #endif
 
 extern struct VirtualMachine *interpreterProxy;
+IMPORT(DWORD) convertToSqueakTime(SYSTEMTIME st);
 
 #define FAIL() { return interpreterProxy->primitiveFail(); }
 
@@ -50,8 +52,9 @@ extern struct VirtualMachine *interpreterProxy;
 #define DEFAULT_DRIVE_PERMISSIONS (S_IRUSR | (S_IRUSR>>3) | (S_IRUSR>>6) | \
                                    S_IWUSR | (S_IWUSR>>3) | (S_IWUSR>>6) | \
                                    S_IXUSR | (S_IXUSR>>3) | (S_IXUSR>>6))
-  
-static void read_permissions(sqInt *posixPermissions, WCHAR* path, sqInt pathLength, sqInt attr)
+
+static void
+read_permissions(sqInt *posixPermissions, WCHAR* path, sqInt pathLength, sqInt attr)
 {
   *posixPermissions |= S_IRUSR | (S_IRUSR>>3) | (S_IRUSR>>6);
   if (!(attr & FILE_ATTRIBUTE_READONLY)) {
@@ -77,7 +80,8 @@ static void read_permissions(sqInt *posixPermissions, WCHAR* path, sqInt pathLen
   }
 }
 
-static int findFileFallbackOnSharingViolation(WCHAR *win32Path, WIN32_FILE_ATTRIBUTE_DATA* winAttrs) {
+static int
+findFileFallbackOnSharingViolation(WCHAR *win32Path, WIN32_FILE_ATTRIBUTE_DATA* winAttrs) {
   WIN32_FIND_DATAW findData;
   HANDLE findHandle = FindFirstFileW(win32Path,&findData);
 
@@ -97,7 +101,8 @@ static int findFileFallbackOnSharingViolation(WCHAR *win32Path, WIN32_FILE_ATTRI
    useful for trying to stay in sync with case-sensitive platforms. */
 int caseSensitiveFileMode = 0;
 
-int hasCaseSensitiveDuplicate(WCHAR *path) {
+int
+hasCaseSensitiveDuplicate(WCHAR *path) {
   WCHAR *src, *dst, *prev;
   WCHAR* findPath = NULL;
   WIN32_FIND_DATAW findData; /* cached find data */
@@ -157,27 +162,8 @@ typedef union {
   squeakFileOffsetType offset;
 } win32FileOffset;
 
-DWORD convertToSqueakTime(SYSTEMTIME st)
-{ DWORD secs;
-  DWORD dy;
-  static DWORD nDaysPerMonth[14] = { 
-    0,  0,  31,  59,  90, 120, 151,
-      181, 212, 243, 273, 304, 334, 365 };
-  /* Squeak epoch is Jan 1, 1901 */
-  dy = st.wYear - 1901; /* compute delta year */
-  secs = dy * 365 * 24 * 60 * 60       /* base seconds */
-         + (dy >> 2) * 24 * 60 * 60;   /* seconds of leap years */
-  /* check if month > 2 and current year is a leap year */
-  if (st.wMonth > 2 && (dy & 0x0003) == 0x0003)
-    secs += 24 * 60 * 60; /* add one day */
-  /* add the days from the beginning of the year */
-  secs += (nDaysPerMonth[st.wMonth] + st.wDay - 1) * 24 * 60 * 60;
-  /* add the hours, minutes, and seconds */
-  secs += st.wSecond + 60*(st.wMinute + 60*st.wHour);
-  return secs;
-}
-
-sqInt dir_Create(char *pathString, sqInt pathLength)
+sqInt
+dir_Create(char *pathString, sqInt pathLength)
 {
   WCHAR *win32Path = NULL;
 
@@ -187,9 +173,11 @@ sqInt dir_Create(char *pathString, sqInt pathLength)
   return CreateDirectoryW(win32Path,NULL);
 }
 
-sqInt dir_Delimitor(void) { return '\\'; }
+sqInt
+dir_Delimitor(void) { return '\\'; }
 
-sqInt dir_Lookup(char *pathString, sqInt pathLength, sqInt index,
+sqInt
+dir_Lookup(char *pathString, sqInt pathLength, sqInt index,
 /* outputs: */ char *name, sqInt *nameLength, sqInt *creationDate, sqInt *modificationDate,
                sqInt *isDirectory, squeakFileOffsetType *sizeIfFile, sqInt *posixPermissions, sqInt *isSymlink)
 {
@@ -201,11 +189,11 @@ sqInt dir_Lookup(char *pathString, sqInt pathLength, sqInt index,
      has limited support for paths longer than 260 characters. In the case of
      longer paths, they must not contain syntactic sugar such as ".", "..", or
      "a/b"*:
-     
+
      * DON'T: dir_Lookup("<very long path>\foo\.", ...)
      * DON'T: dir_Lookup("<very long path>\foo\..", ...)
      * DON'T: dir_Lookup("<very long path>\foo/bar", ...)
-     
+
      For more details, see:
      https://learn.microsoft.com/en-us/windows/win32/fileio/naming-a-file#paths
      *) Long paths are automatically converted into UNC paths, which only
@@ -263,18 +251,17 @@ sqInt dir_Lookup(char *pathString, sqInt pathLength, sqInt index,
     int mask;
 
     mask = GetLogicalDrives();
-    for(i=0;i<26; i++)
+    for(i=0; i<26; i++)
       if (mask & (1 << i))
-	if (--index == 0) {
-	  /* found the drive ! */
+	    if (--index == 0) { // found the drive !
           name[0]           = 'A'+i;
-          name[1]	          = ':';
+          name[1]           = ':';
           *nameLength       = 2;
           *creationDate     = 0;
           *modificationDate = 0;
           *isDirectory      = true;
           *sizeIfFile       = 0;
-	  *posixPermissions |= DEFAULT_DRIVE_PERMISSIONS;
+          *posixPermissions |= DEFAULT_DRIVE_PERMISSIONS;
           return ENTRY_FOUND;
         }
     return NO_MORE_ENTRIES;
@@ -301,7 +288,7 @@ sqInt dir_Lookup(char *pathString, sqInt pathLength, sqInt index,
   } else {
     pattern[patternLength-1] = '*';
   }
-  
+
   /* convert the path name into a null-terminated C string */
   ALLOC_WIN32_PATH(win32Path, pattern, patternLength);
 
@@ -309,7 +296,7 @@ sqInt dir_Lookup(char *pathString, sqInt pathLength, sqInt index,
     lastStringLength = 0;
     return BAD_PATH;
   }
-  
+
   /* and go looking for entries */
   findHandle = FindFirstFileW(win32Path,&findData);
   if (findHandle == INVALID_HANDLE_VALUE) {
@@ -362,7 +349,8 @@ sqInt dir_Lookup(char *pathString, sqInt pathLength, sqInt index,
   return ENTRY_FOUND;
 }
 
-sqInt dir_EntryLookup(char *pathString, sqInt pathLength, char* nameString, sqInt nameStringLength,
+sqInt
+dir_EntryLookup(char *pathString, sqInt pathLength, char* nameString, sqInt nameStringLength,
 /* outputs: */ char *name, sqInt *nameLength, sqInt *creationDate, sqInt *modificationDate,
                     sqInt *isDirectory, squeakFileOffsetType *sizeIfFile, sqInt *posixPermissions, sqInt *isSymlink)
 {
@@ -379,12 +367,12 @@ sqInt dir_EntryLookup(char *pathString, sqInt pathLength, char* nameString, sqIn
      ".", which is also supported for long paths. This convention is provided
      for an efficient existence check of the directory (e.g.,
      FileDirectory>>#exists):
-     
+
      * DON'T: dir_EntryLookup("<very long path>", .., "foo\.", ...)
      * DON'T: dir_EntryLookup("<very long path>", .., "foo\..", ...)
      * DON'T: dir_EntryLookup("<very long path>", .., "foo/bar", ...)
      * DO:    dir_EntryLookup("<very long path>", .., ".", ...)
-     
+
      For more details, see:
      https://learn.microsoft.com/en-us/windows/win32/fileio/naming-a-file#paths
      *) Long paths are automatically converted into UNC paths, which only
@@ -459,10 +447,10 @@ sqInt dir_EntryLookup(char *pathString, sqInt pathLength, char* nameString, sqIn
     nameStringLength = 0;
   }
   memcpy(fullPath+fullPathLength-nameStringLength,nameString,nameStringLength);
-  
+
   /* convert the path name into a null-terminated C string */
   ALLOC_WIN32_PATH(win32Path, fullPath, fullPathLength);
-  
+
   if (!GetFileAttributesExW(win32Path, 0, &winAttrs)) {
 #ifdef PharoVM
     if (GetLastError() == ERROR_SHARING_VIOLATION) {
@@ -531,21 +519,24 @@ dir_SetPathToWorkingDirAsUTF8(char *pathName)
 }
 
 // Compatibility with the Mac
-sqInt dir_SetMacFileTypeAndCreator(char *filename, sqInt filenameSize,
+sqInt
+dir_SetMacFileTypeAndCreator(char *filename, sqInt filenameSize,
 			     char *fType, char *fCreator)
 {
   /* Win32 files are untyped, and the creator is correct by default */
   return true;
 }
 
-sqInt dir_GetMacFileTypeAndCreator(char *filename, sqInt filenameSize,
+sqInt
+dir_GetMacFileTypeAndCreator(char *filename, sqInt filenameSize,
 			     char *fType, char *fCreator)
 {
   /* Win32 files are untyped, and the creator is correct by default */
   FAIL();
 }
 
-sqInt dir_Delete(char *pathString, sqInt pathLength) {
+sqInt
+dir_Delete(char *pathString, sqInt pathLength) {
   /* Delete the existing directory with the given path. */
   WCHAR *win32Path = NULL;
 

platforms/win32/vm/sqWin32Time.c
@@ -8,6 +8,27 @@
 
 #include "sq.h"
 
+EXPORT(DWORD)
+convertToSqueakTime(SYSTEMTIME st)
+{ DWORD secs;
+  DWORD dy;
+  static DWORD nDaysPerMonth[14] = { 
+    0,  0,  31,  59,  90, 120, 151,
+      181, 212, 243, 273, 304, 334, 365 };
+  /* Squeak epoch is Jan 1, 1901 */
+  dy = st.wYear - 1901; /* compute delta year */
+  secs = dy * 365 * 24 * 60 * 60       /* base seconds */
+         + (dy >> 2) * 24 * 60 * 60;   /* seconds of leap years */
+  /* check if month > 2 and current year is a leap year */
+  if (st.wMonth > 2 && (dy & 0x0003) == 0x0003)
+    secs += 24 * 60 * 60; /* add one day */
+  /* add the days from the beginning of the year */
+  secs += (nDaysPerMonth[st.wMonth] + st.wDay - 1) * 24 * 60 * 60;
+  /* add the hours, minutes, and seconds */
+  secs += st.wSecond + 60 * (st.wMinute + 60 * st.wHour);
+  return secs;
+}
+
 #if !STACKVM
 /*
  * Win32 FILETIMEs are 10th's of microseconds since 1601.  Smalltalk times

platforms/win32/vm/sqWin32Utils.c
@@ -49,18 +49,20 @@ abortMessage(TCHAR *fmt, ...)
 { TCHAR *buf;
 	va_list args;
 
-	va_start(args, fmt);
 	if (fIsConsole) {
+		va_start(args, fmt);
 #if _UNICODE
 		vfwprintf(stderr, fmt, args);
 #else
 		vfprintf(stderr, fmt, args);
 #endif
+		va_end(args);
 		exit(-1);
 	}
-	buf = (TCHAR*) calloc(sizeof(TCHAR), 4096);
+	buf = (TCHAR *)calloc(sizeof(TCHAR), _tcslen(fmt) + 1024);
 	if (fmt[_tcslen(fmt)-1] == '\n')
 		fmt[_tcslen(fmt)-1] = 0;
+	va_start(args, fmt);
 #if _UNICODE
 	wvsprintf(buf, fmt, args);
 #else

platforms/win32/vm/sqWin32Window.c
@@ -192,7 +192,6 @@ int ioDrainEventQueue(void);
 #endif
 
 extern sqInt byteSwapped(sqInt);
-extern int convertToSqueakTime(SYSTEMTIME);
 static int recordMouseDown(WPARAM, LPARAM);
 static int recordModifierButtons();
 static int recordKeystroke(UINT,WPARAM,LPARAM);
@@ -3186,19 +3185,16 @@ HideSplashScreen(void)
 # define VMOPTION(arg) "-"arg
 # define TVMOPTION(arg) TEXT("-") TEXT(arg)
 # if _UNICODE
-static TCHAR *
-asTCharString(char *charString)
-{
-	int len = MultiByteToWideChar(CP_UTF8, 0, charString, -1, NULL, 0);
-	if (len <= 0)
-		return 0; /* invalid UTF8 ? */
-	LPWSTR tcharString = malloc(len*sizeof(WCHAR));
-	if (MultiByteToWideChar(CP_UTF8, 0, charString, -1, tcharString, len) == 0)
-		return 0;
-	return tcharString;
-}
+# define toTCharString(charString,tcharString)								\
+	len = MultiByteToWideChar(CP_UTF8, 0, charString, -1, NULL, 0);			\
+	if (len <= 0)															\
+		tcharString = 0; /* invalid UTF8 ? */								\
+	else {																	\
+		tcharString = alloca(len*sizeof(WCHAR));							\
+		MultiByteToWideChar(CP_UTF8, 0, charString, -1, tcharString, len);	\
+	}
 # else
-#	define asTCharString(charString) charString
+#	define toTCharString(charString,tcharString) tcharString = charString
 # endif
 
 /* print usage with different output levels */
@@ -3210,16 +3206,34 @@ printUsage(int level)
       abortMessage(TEXT("Usage: ") TEXT(VM_NAME) TEXT(" [options] <imageFile>\n"));
       break;
     case 1: { // full usage
-#if COGVM
-	  char traceFlags[1024];
-	  extern const char *traceFlagsMeanings[];
-	  bzero(traceFlags,1024);
-	  int i = 0;
-	  while (traceFlagsMeanings[i]) {
-		strcat(traceFlags, "\n\t\t");
-		strcat(traceFlags, traceFlagsMeanings[i]);
+#if STACKVM
+# define N 512
+	  int i = 0, len;
+	  char concatenation[N];
+
+	  TCHAR *leakCheckFlags;
+	  extern const char *leakCheckFlagsMeanings[];
+	  bzero(concatenation,N);
+	  i = 0;
+	  while (leakCheckFlagsMeanings[i]) {
+		strcat(concatenation, "\n\t\t");
+		strcat(concatenation, leakCheckFlagsMeanings[i]);
 		++i;
 	  }
+	  toTCharString(concatenation,leakCheckFlags);
+# if COGVM
+	  TCHAR *traceFlags;
+	  extern const char *traceFlagsMeanings[];
+	  bzero(concatenation,N);
+	  i = 0;
+	  while (traceFlagsMeanings[i]) {
+		strcat(concatenation, "\n\t\t");
+		strcat(concatenation, traceFlagsMeanings[i]);
+		++i;
+	  }
+	  toTCharString(concatenation,traceFlags);
+# endif
+# undef N
 #endif
       abortMessage(TEXT("Usage: ") TEXT(VM_NAME) TEXT(" [vmOptions] imageFile [imageOptions]\n\n")
                    TEXT("vmOptions:")
@@ -3232,9 +3246,8 @@ printUsage(int level)
                    TEXT("\n\t") TVMOPTION("breaksel:") TEXT(" string \t(call warning on send of sel for debug)")
 #endif /* STACKVM || NewspeakVM */
 #if STACKVM
-                   TEXT("\n\t") TVMOPTION("failonffiexception") TEXT("\t(when in an FFI callout primitive catch exceptions and fail the primitive)")
                    TEXT("\n\t") TVMOPTION("breakmnu:") TEXT(" string \t(call warning on MNU of sel for debug)")
-                   TEXT("\n\t") TVMOPTION("leakcheck:") TEXT(" n \t\t(leak check on GC (1=full,2=incr,3=both))")
+                   TEXT("\n\t") TVMOPTION("leakcheck:") TEXT(" flags \t(leak check on GC etc)%s")
                    TEXT("\n\t") TVMOPTION("eden:") TEXT(" bytes \t\t(set eden memory size to bytes)")
                    TEXT("\n\t") TVMOPTION("stackpages:") TEXT(" n \t\t(use n stack pages)")
                    TEXT("\n\t") TVMOPTION("numextsems:") TEXT(" n \t\t(allow up to n external semaphores)")
@@ -3248,7 +3261,7 @@ printUsage(int level)
                    TEXT("\n\t") TVMOPTION("sendtrace") TEXT(" \t\t(trace sends to stdout for debug)")
 # endif
                    TEXT("\n\t") TVMOPTION("warnpid") TEXT("   \t\t(print pid in warnings)")
-                   TEXT("\n\t") TVMOPTION("[no]failonffiexception") TEXT("   \t\t([never]always catch exceptions in FFI calls)")
+                   TEXT("\n\t") TVMOPTION("[no]failonffiexception") TEXT("   ([never]always catch exceptions in FFI calls)")
 #endif
 #if COGVM
                    TEXT("\n\t") TVMOPTION("codesize:") TEXT(" bytes \t(set machine-code memory size to bytes)")
@@ -3263,8 +3276,11 @@ printUsage(int level)
 #endif
                    TEXT("\n") TEXT("Options begin with single -, but -- prefix is silently accepted")
                    TEXT("\n") TEXT("Options with arguments -opt:n are also accepted with separators -opt n")
-#if COGVM
-					, asTCharString(traceFlags)
+#if STACKVM
+					, leakCheckFlags
+# if COGVM
+					, traceFlags
+# endif
 #endif
                    );
 	  }

scripts/modified
@@ -1,3 +1,3 @@
 #!/bin/bash
 # echo modified files to stdout
-git status --porcelain | egrep "^ M|^M |^AM" | sed 's/^...//'
+git status --porcelain | egrep "^[ AR]M|^M " | sed 's/^...//'

commit 28712a36aa2a818cd932701d21cd4a60f4d10b4e

Fix the regression in e6108a0cd83b5cfde484d41e8a4e289b4da3a02d. I stupidly write a string concatenation incorrectly.

platforms/unix/vm/sqUnixMain.c
@@ -535,7 +535,7 @@ getAttributeString(sqInt id)
 		osName = malloc(strlen(info.sysname) + strlen(info.release) + 2);
 		strcpy(osName,info.sysname);
 		osName[strlen(info.sysname)] = ' ';
-		strcat(osName,info.release);
+		strcpy(osName + strlen(info.sysname) + 1,info.release);
 		return osName;
 #endif
 	}