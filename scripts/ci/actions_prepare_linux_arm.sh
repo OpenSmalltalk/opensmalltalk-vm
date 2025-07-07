e59c3800b9c2b4d36fc34bde56d367650f284710
Marcel Taeumel <marcel.taeumel@hpi.de>
2025-07-07 14:05:21 +0200

    Linux/ARM CI: Replace dependency libgl1-mesa-glx with libgl1 and libglx-mesa0. See https://packages.debian.org/bookworm/libgl1-mesa-glx

--- scripts/ci/actions_prepare_linux_arm.sh
+++ scripts/ci/actions_prepare_linux_arm.sh
@@ -11,7 +11,10 @@ set -e
 # let the configure script pass.
 
 sudo apt-get update -y
-sudo apt-get install -yq --no-install-suggests --no-install-recommends   build-essential git devscripts   uuid-dev libcairo2-dev libpango1.0-dev libgl1-mesa-dev libgl1-mesa-glx libssl-dev libevdev-dev m4 libpulse-dev   libasound2-dev libfreetype6-dev libx11-dev libxrender-dev libxrandr-dev  libtool automake autoconf libltdl-dev
+sudo apt-get install -yq --no-install-suggests --no-install-recommends   build-essential git devscripts   uuid-dev libcairo2-dev libpango1.0-dev libgl1-mesa-dev libgl1 libglx-mesa0 libssl-dev libevdev-dev m4 libpulse-dev   libasound2-dev libfreetype6-dev libx11-dev libxrender-dev libxrandr-dev  libtool automake autoconf libltdl-dev
+
+
+
 
 # Other packages found in older build scripts, ignored for now:
 #   uuid-runtime libsm-dev libice-dev cmake
