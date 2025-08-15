aac688f62ec58bcb0b7cebff2a161dc145314e50
Marcel Taeumel <marcel.taeumel@hpi.de>
2025-08-15 17:24:19 +0200

    win64ARMv8 CI | fix MSYS2 path since C: cannot be used in windows-11-arm runner

--- scripts/ci/actions_prepare_msys.sh
+++ scripts/ci/actions_prepare_msys.sh
@@ -11,19 +11,21 @@
 [[ -z "${MSYS_SYS}" ]] && exit 2
 [[ -z "${MSYS_ENV}" ]] && exit 2
 
-ln -f -s /c/msys64/${MSYS_SYS}/bin/clang /c/msys64/${MSYS_SYS}/bin/${MSYS_ENV}-w64-mingw32-clang
-ln -f -s /c/msys64/${MSYS_SYS}/bin/clang++ /c/msys64/${MSYS_SYS}/bin/${MSYS_ENV}-w64-mingw32-clang++
-ln -f -s /c/msys64/${MSYS_SYS}/bin/ar /c/msys64/${MSYS_SYS}/bin/${MSYS_ENV}-w64-mingw32-ar
-ln -f -s /c/msys64/${MSYS_SYS}/bin/dlltool /c/msys64/${MSYS_SYS}/bin/${MSYS_ENV}-w64-mingw32-dlltool
-ln -f -s /c/msys64/${MSYS_SYS}/bin/as /c/msys64/${MSYS_SYS}/bin/${MSYS_ENV}-w64-mingw32-as
-ln -f -s /c/msys64/${MSYS_SYS}/bin/windres /c/msys64/${MSYS_SYS}/bin/${MSYS_ENV}-w64-mingw32-windres
-ln -f -s /c/msys64/${MSYS_SYS}/bin/nm /c/msys64/${MSYS_SYS}/bin/${MSYS_ENV}-w64-mingw32-nm
-ln -f -s /c/msys64/${MSYS_SYS}/bin/dllwrap /c/msys64/${MSYS_SYS}/bin/${MSYS_ENV}-w64-mingw32-dllwrap
-ln -f -s /c/msys64/${MSYS_SYS}/bin/strip /c/msys64/${MSYS_SYS}/bin/${MSYS_ENV}-w64-mingw32-strip
-ln -f -s /c/msys64/${MSYS_SYS}/bin/objcopy /c/msys64/${MSYS_SYS}/bin/${MSYS_ENV}-w64-mingw32-objcopy
+if [ -z "${MSYS_PATH}" ]; then MSYS_PATH="/c/msys64"; fi
+
+ln -f -s ${MSYS_PATH}/${MSYS_SYS}/bin/clang ${MSYS_PATH}/${MSYS_SYS}/bin/${MSYS_ENV}-w64-mingw32-clang
+ln -f -s ${MSYS_PATH}/${MSYS_SYS}/bin/clang++ ${MSYS_PATH}/${MSYS_SYS}/bin/${MSYS_ENV}-w64-mingw32-clang++
+ln -f -s ${MSYS_PATH}/${MSYS_SYS}/bin/ar ${MSYS_PATH}/${MSYS_SYS}/bin/${MSYS_ENV}-w64-mingw32-ar
+ln -f -s ${MSYS_PATH}/${MSYS_SYS}/bin/dlltool ${MSYS_PATH}/${MSYS_SYS}/bin/${MSYS_ENV}-w64-mingw32-dlltool
+ln -f -s ${MSYS_PATH}/${MSYS_SYS}/bin/as ${MSYS_PATH}/${MSYS_SYS}/bin/${MSYS_ENV}-w64-mingw32-as
+ln -f -s ${MSYS_PATH}/${MSYS_SYS}/bin/windres ${MSYS_PATH}/${MSYS_SYS}/bin/${MSYS_ENV}-w64-mingw32-windres
+ln -f -s ${MSYS_PATH}/${MSYS_SYS}/bin/nm ${MSYS_PATH}/${MSYS_SYS}/bin/${MSYS_ENV}-w64-mingw32-nm
+ln -f -s ${MSYS_PATH}/${MSYS_SYS}/bin/dllwrap ${MSYS_PATH}/${MSYS_SYS}/bin/${MSYS_ENV}-w64-mingw32-dllwrap
+ln -f -s ${MSYS_PATH}/${MSYS_SYS}/bin/strip ${MSYS_PATH}/${MSYS_SYS}/bin/${MSYS_ENV}-w64-mingw32-strip
+ln -f -s ${MSYS_PATH}/${MSYS_SYS}/bin/objcopy ${MSYS_PATH}/${MSYS_SYS}/bin/${MSYS_ENV}-w64-mingw32-objcopy
 
 # Add MinGW tools for build scripts
-PATH=$PATH:/c/msys64/${MSYS_SYS}/bin
+PATH=$PATH:${MSYS_PATH}/${MSYS_SYS}/bin
 
 # Add other GNU tools (e.g., wget) for third-party build scripts
-PATH=$PATH:/c/msys64/usr/bin
+PATH=$PATH:${MSYS_PATH}/usr/bin
