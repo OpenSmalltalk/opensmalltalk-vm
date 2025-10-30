d9a4f87416d9bde7e41be26f0abc4df388e4c354
David T Lewis <lewis@mail.msen.com>
2025-10-30 10:50:00 -0400

    Let ptyForkAndExec return the pid of the child process used for tty_login such that primitives can inform the image of the child pid. This is to support coordination of SIGCHLD handling between PseudoTTYPlugin and OSProcess. Also let "declining responsibility for child processes" be a debug message rather than stderr message in anticipation of better OSProcess integration. This update based on the more recent version in squeakvm.org SVN with cosmetic updates from original author (to keep code bases in sync).

--- platforms/unix/plugins/PseudoTTYPlugin/sqUnixPseudoTTYPlugin.c
+++ platforms/unix/plugins/PseudoTTYPlugin/sqUnixPseudoTTYPlugin.c
@@ -2,7 +2,7 @@
  * 
  * Author:	Ian Piumarta <ian.piumarta@inria.fr>
  * Version:	1.1
- * Last edited:	2003-09-03 18:06:13 by piumarta on emilia.inria.fr
+ * Last edited:	2009-08-19 04:25:56 by piumarta on emilia-2.local
  * 
  * This plugin extends AsynchFilePlugin with support for Unix98-style
  * pseudo ttys.  See the PseudoTTY and PseudoTTYPlugin class comments
@@ -31,12 +31,12 @@
  *   
  *   This file is part of Unix Squeak.
  * 
- *   Permission is hereby granted, free of charge, to any person obtaining a
- *   copy of this software and associated documentation files (the "Software"),
- *   to deal in the Software without restriction, including without limitation
- *   the rights to use, copy, modify, merge, publish, distribute, sublicense,
- *   and/or sell copies of the Software, and to permit persons to whom the
- *   Software is furnished to do so, subject to the following conditions:
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
  * 
  *   The above copyright notice and this permission notice shall be included in
  *   all copies or substantial portions of the Software.
@@ -45,11 +45,12 @@
  *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- *   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- *   DEALINGS IN THE SOFTWARE.
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *   SOFTWARE.
  */
 
+#include "config.h"
 #include "sq.h"
 #include "PseudoTTYPlugin.h"
 
@@ -69,9 +70,9 @@
 #include "openpty.h"		/* hide the gory details ;) */
 
 #if 0
-# define DPRINTF(ARGS) printf ARGS
+# define debugf(ARGS) printf ARGS
 #else
-# define DPRINTF(ARGS)
+# define debugf(ARGS)
 #endif
 
 
@@ -123,20 +124,20 @@ static void sigchld(int signum)
       /* close(zombie->pty->fd); */
       zombie->pty->rd.status= -2;
       signalSemaphoreWithIndex(zombie->pty->sema);
-      DPRINTF(("closed pty for pid %d\n", pid));
+      debugf(("closed pty for pid %d\n", pid));
     }
 }
 
 
 int ptyInit(void)
 {
-  DPRINTF(("ptyInit: AsyncFileSession is %d\n", sqUnixAsyncFileSessionID));
+  debugf(("ptyInit: AsyncFileSession is %d\n", sqUnixAsyncFileSessionID));
   vm= sqGetInterpreterProxy();
   slaves= 0;
   prevchld= signal(SIGCHLD, sigchld);
   if ((prevchld != SIG_DFL) && (prevchld != SIG_IGN))
     {
-      fprintf(stderr, "declining responsibility for child processes!\n");
+      debugf(("ptyInit: declining responsibility for child processes!\n"));
       signal(SIGCHLD, prevchld);
       reaping= 0;
     }
@@ -178,30 +179,30 @@ int ptyShutdown(void)
 #include <time.h>
 
 
-int ptyForkAndExec(AsyncFile *f, int semaIndex,
+pid_t ptyForkAndExec(AsyncFile *f, int semaIndex,
 		   char *cmdIndex, int cmdLen, sqInt *argIndex, int argLen)
 {
   int ptm= -1, pts= -1;
   char tty[32];
   FilePtr fp= 0;
 
-  DPRINTF(("ptyForkAndExec\n"));
+  debugf(("ptyForkAndExec\n"));
 
   /* Module init must succeed in loading the AsyncFile plugin */
   if (sqUnixAsyncFileSessionID == 0)
     {
       vm->primitiveFail();
-      return 0;
+      return -1;
     }
 
-  DPRINTF(("AsyncFileSession is %d\n", sqUnixAsyncFileSessionID));
+  debugf(("AsyncFileSession is %d\n", sqUnixAsyncFileSessionID));
 
   if (openpty(&ptm, &pts, tty, 0, 0) == -1)
     {
       perror("pty: openpty");
       goto failDetached;
     }
-  DPRINTF(("pty: using %s (ptm %d pts %d)\n", tty, ptm, pts));
+  debugf(("pty: using %s (ptm %d pts %d)\n", tty, ptm, pts));
 
   if ((fp= asyncFileAttach(f, ptm, semaIndex)) == 0)
     goto failDetached;
@@ -213,9 +214,11 @@ int ptyForkAndExec(AsyncFile *f, int semaIndex,
     char   **argv= (char **)alloca(sizeof(char *) * (argLen + 2));
     int      i= 0;
     SlavePtr slave= 0;
+    pid_t    childPid;
+
     memcpy((void *)cmd, cmdIndex, cmdLen);
     cmd[cmdLen]= '\0';
-    DPRINTF(("pty: command: %s\n", cmd));
+    debugf(("pty: command: %s\n", cmd));
     argv[0]= cmd;
     for (i= 1;  i <= argLen;  ++i)
       {
@@ -224,12 +227,12 @@ int ptyForkAndExec(AsyncFile *f, int semaIndex,
 	int   len= 0;
 	if (!vm->isBytes(argOop)) goto fail;
 	len= vm->stSizeOf(argOop);
-	DPRINTF(("pty: arg %d len %d\n", i, len));
+	debugf(("pty: arg %d len %d\n", i, len));
 	arg= (char *)alloca(len + 1);
 	memcpy((void *)arg, (void *)vm->firstIndexableField(argOop), len);
 	arg[len]= '\0';
 	argv[i]= arg;
-	DPRINTF(("pty: argv[%d]: %s\n", i, argv[i]));
+	debugf(("pty: argv[%d]: %s\n", i, argv[i]));
       }
     argv[argLen+1]= 0;	/* argv terminator */
 
@@ -239,9 +242,9 @@ int ptyForkAndExec(AsyncFile *f, int semaIndex,
     slaves= slave;
     slave->pts= pts;
     slave->pty= fp;
-    slave->pid= fork();
-
-    switch (slave->pid)
+    childPid= fork();
+    slave->pid= childPid;
+    switch (childPid)
       {
       case -1:			/* error */
 	slaves= slaves->next;
@@ -263,7 +266,7 @@ int ptyForkAndExec(AsyncFile *f, int semaIndex,
 	close(pts);
 	break;
       }
-    return 0;
+    return childPid;
   }
 
  fail:
@@ -274,7 +277,7 @@ int ptyForkAndExec(AsyncFile *f, int semaIndex,
   if (ptm >= 0) close(ptm);
   if (pts >= 0) close(pts);
   vm->primitiveFail();
-  return 0;
+  return -1;
 }
 
 
@@ -283,14 +286,14 @@ int ptyClose(AsyncFile *f)
   SlavePtr slave= 0, prev= 0;
   FilePtr  pty= (FilePtr)f->state;
   validate(f);
-  DPRINTF(("pty: close %d\n", pty->fd));
+  debugf(("pty: close %d\n", pty->fd));
   if (pty->fd >= 0)
     {
       for (prev= 0, slave= slaves;  slave;  prev= slave, slave= slave->next)
 	if (slave->pty == pty)
 	  {
 	    int pid= slave->pid;
-	    DPRINTF(("killing pid %d connected to pts %d\n", pid, slave->pts));
+	    debugf(("killing pid %d connected to pts %d\n", pid, slave->pts));
 	    /* terminate with increasing degrees of violence... */
 	    kill(pid, SIGTERM);
 	    usleep(200*1000);
@@ -318,7 +321,7 @@ int ptyWindowSize(AsyncFile *f, int cols, int rows)
   struct winsize sz;
   FilePtr pty= (FilePtr)f->state;
   validate(f);
-  DPRINTF(("pty %d size %d %d\n", pty->fd, cols, rows));
+  debugf(("pty %d size %d %d\n", pty->fd, cols, rows));
   sz.ws_col= cols;
   sz.ws_row= rows;
   sz.ws_xpixel= sz.ws_ypixel= 0;
