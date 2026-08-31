b0a5f72ae526d08d46064f7d8fffa36d769e1720
Eliot Miranda <eliot.miranda@gmail.com>
2026-08-30 17:46:44 -0700

    CogVM source as per VMMaker.oscog-eem.3787
    Merge VMMaker.oscog-ct.3784. (VMMaker.oscog-eem.3787 inherits from
    VMMaker.oscog-ct.3786).
    
    Discard VMMaker.oscog-eem.3785, VMMaker.oscog-eem.3785 &
    VMMaker.oscog-eem.37864
    
    Fix regression in FFI's handling of returning nil results of
    string return type in VMMaker.oscog-eem.3477.
    
    Fix bad bug in binary dispatch in
    SpurMemoryManager>>fetchInteger:ofObject:/storeInteger:ofObject:withValue:.
    Explicitly type requiredStackSize in ffiCall:ArgsOnStack:ArgArray:NumArgs:
    as sqInt now fetchInteger:ofObject: has return type sqLong.
    
    Allow SpurMemoryManager>>indexOf:in: to function on CompiledCode,
    searching for literals.
    
    Slang:
    make sure that ifNil: does not emit the expression twice.
    
    Avoid overwriting previous declarations of toDoLimit with a smaller type when
    inlining loops. Fixes https://github.com/OpenSmalltalk/opensmalltalk-vm/issues/784.
    
    Comment typos

--- platforms/unix/vm/sqUnixMain.c
+++ platforms/unix/vm/sqUnixMain.c
@@ -1829,7 +1829,7 @@ vm_printUsage(void)
 	option("  "VMOPTION("failonffiexception")"   when in an FFI callout primitive catch exceptions and fail the primitive\n");
 	option("  "VMOPTION("breakmnu")" selector    set breakpoint on MNU of selector\n");
 	option("  "VMOPTION("eden")" <size>[kmg]     use given eden size\n");
-	option("  "VMOPTION("leakcheck")"=flags      check for leaks in the heap\n");
+	option("  "VMOPTION("leakcheck")" flags      check for leaks in the heap\n");
 	{ extern const char *leakCheckFlagsMeanings[];
 		i = 0;
 		while (leakCheckFlagsMeanings[i]) {
